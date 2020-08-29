8efe8ac4c8d9c5c8b28bee38dc0c02b7554cd2b1
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-08-29 15:59:03 +0530

    Password encoder

--- SpringBoot/SpringSecurity/src/main/java/com/anilt/spring/security/basicauth/StudentSecurityConfig.java
+++ SpringBoot/SpringSecurity/src/main/java/com/anilt/spring/security/basicauth/StudentSecurityConfig.java
@@ -2,14 +2,29 @@ package com.anilt.spring.security.basicauth;
 
 import javax.annotation.security.PermitAll;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class StudentSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private final PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	public StudentSecurityConfig(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -20,6 +35,17 @@ public class StudentSecurityConfig extends WebSecurityConfigurerAdapter {
 		.authenticated()
 		.and()
 		.httpBasic();
+	}
+	
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		UserDetails anilUser =  User.builder()
+		.username("anil")
+		.password(passwordEncoder.encode("password"))
+		.roles("TEACHER")
+		.build();
+		return new InMemoryUserDetailsManager(anilUser);
 	}
 
 }
