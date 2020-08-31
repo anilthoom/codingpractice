bdd7bf60dae79bf80117c7cfd4f6e507f8aac9c2
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-08-31 18:15:14 +0530

    Internationalization Project Creation

---
+++ SpringBoot/InternationalizationTest/src/test/java/com/anilt/spring/i18n/InternationalizationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anilt.spring.i18n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternationalizationTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
