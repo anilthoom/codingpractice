e7b8652a1b3642fa33df39232b26076d701d2fa3
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-01 17:49:12 +0530

    Roles enum addition

--- SpringBoot/SpringSecurity/src/main/java/com/anilt/spring/security/AppUserPermissions.java
+++ SpringBoot/SpringSecurity/src/main/java/com/anilt/spring/security/AppUserPermissions.java
@@ -7,11 +7,20 @@ package com.anilt.spring.security;
  * @author anilt
  *
  */
-public enum AppUserPermissions {
-	STUDENT_READ,
-	STUDENT_WRITE,
-	COURSE_READ,
-	COURSE_WRITE
+enum AppUserPermissions {
+	STUDENT_READ, STUDENT_WRITE, COURSE_READ, COURSE_WRITE;
 	
+	private final String permission;
 	
+
+	private AppUserPermissions() {
+	}
+
+	AppUserPermissions(String permission) {
+		this.permission = permission;
+	}
+	
+	public String getPermission() {
+		return permission;
+	}
 }
