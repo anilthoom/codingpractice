090d281004df5237a301cdf694d6a6fb6caa596a
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-01 17:35:52 +0530

    Permissions ENUM added

---
+++ SpringBoot/SpringSecurity/src/main/java/com/anilt/spring/security/AppUserPermissions.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.anilt.spring.security;
+
+/**
+ * @author anilt
+ *
+ */
+public enum AppUserPermissions {
+	STUDENT_READ("student:read"),
+	STUDENT_WRITE("student:write"),
+	COURSE_READ("course:read"),
+	COURSE_WRITE("course:write")
+}
