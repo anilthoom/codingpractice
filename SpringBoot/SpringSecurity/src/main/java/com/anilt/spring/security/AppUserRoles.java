06722cb5be1ab7660f066bfc6da4afdc39fc90ad
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-01 18:17:00 +0530

    Role permission mapping

--- SpringBoot/SpringSecurity/src/main/java/com/anilt/spring/security/AppUserRoles.java
+++ SpringBoot/SpringSecurity/src/main/java/com/anilt/spring/security/AppUserRoles.java
@@ -3,13 +3,26 @@
  */
 package com.anilt.spring.security;
 
+import java.util.Set;
+
+import com.google.common.collect.Sets;
+
 /**
  * @author anilt
  *
  */
 public enum AppUserRoles {
 
-	STUDENT,
-	TEACHER,
-	ADMIN
+	STUDENT(Sets.newHashSet()),
+	TEACHER(Sets.newHashSet());
+	
+	private final Set<AppUserPermissions> permissios;
+
+	/**
+	 * @param permissios
+	 */
+	private AppUserRoles(Set<AppUserPermissions> permissios) {
+		this.permissios = permissios;
+	}
+	
 }
