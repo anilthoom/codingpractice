2e9521cf209dc77a443e5e662fe996e6f2c8c2d9
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-02 17:48:23 +0530

    New user role added

--- SpringBoot/SpringSecurity/src/main/java/com/anilt/spring/security/student/controller/StudentManagementController.java
+++ SpringBoot/SpringSecurity/src/main/java/com/anilt/spring/security/student/controller/StudentManagementController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,4 +41,11 @@ public class StudentManagementController {
 	{
 		System.out.println(studentId);
 	}
+	
+	@PutMapping(path ="{studentId}")
+	public void updateStudent(@PathVariable("studentId") Integer studentId, Student student) {
+		
+		System.out.println(student);
+	}
+	
 }
