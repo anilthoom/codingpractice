ce1530c4c1fc336775829866058cd7097adccb6f
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-02 18:59:26 +0530

    New user role added

--- SpringBoot/SpringSecurity/src/main/java/com/anilt/spring/security/basicauth/StudentSecurityConfig.java
+++ SpringBoot/SpringSecurity/src/main/java/com/anilt/spring/security/basicauth/StudentSecurityConfig.java
@@ -30,6 +30,7 @@ public class StudentSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
+		.csrf().disable()
 		.authorizeRequests()
 		.antMatchers("/","index","css/*", "/js/*").permitAll()
 		.antMatchers("/api/**").hasRole(AppUserRoles.STUDENT.name())
