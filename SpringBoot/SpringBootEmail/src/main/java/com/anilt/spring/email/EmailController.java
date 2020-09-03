b141cc954e2490d652b03acda8e150dec97b774d
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-03 11:58:56 +0530

    email details

--- SpringBoot/SpringBootEmail/src/main/java/com/anilt/spring/email/EmailController.java
+++ SpringBoot/SpringBootEmail/src/main/java/com/anilt/spring/email/EmailController.java
@@ -1,5 +1,6 @@
 package com.anilt.spring.email;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sendmail")
 public class EmailController {
 
+	private EmailConfiguration emailConfig;
+
+	public EmailController(EmailConfiguration emailConfig) {
+		this.emailConfig = emailConfig;
+	}
+	
+	@PostMapping
+	public void sendMail() {
+		
+	}
 }
