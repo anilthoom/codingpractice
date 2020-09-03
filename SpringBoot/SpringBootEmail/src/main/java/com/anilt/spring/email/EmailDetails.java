6ef5b6252ae7be69bd34df3c4817493e6b0c7e8b
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-03 11:50:08 +0530

    email details

---
+++ SpringBoot/SpringBootEmail/src/main/java/com/anilt/spring/email/EmailDetails.java
@@ -0,0 +1,36 @@
+package com.anilt.spring.email;
+
+public class EmailDetails {
+
+	private String name;
+	
+	private String email;
+	
+	private String emailContent;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmailContent() {
+		return emailContent;
+	}
+
+	public void setEmailContent(String emailContent) {
+		this.emailContent = emailContent;
+	}
+	
+	
+}
