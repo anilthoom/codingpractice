e1c2ec27cbb1dc945243948818028a5165f4a704
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-03 11:34:27 +0530

    Getters Setters

--- SpringBoot/SpringBootEmail/src/main/java/com/anilt/spring/email/EmailConfiguration.java
+++ SpringBoot/SpringBootEmail/src/main/java/com/anilt/spring/email/EmailConfiguration.java
@@ -9,4 +9,29 @@ public class EmailConfiguration {
 	private String port;
 	private String username;
 	private String password;
+	
+	public String getHost() {
+		return host;
+	}
+	public void setHost(String host) {
+		this.host = host;
+	}
+	public String getPort() {
+		return port;
+	}
+	public void setPort(String port) {
+		this.port = port;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
