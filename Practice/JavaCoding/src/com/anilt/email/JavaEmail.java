54b59786c4b6fa9cf2e247f134298f12285d072a
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-03 13:35:08 +0530

    Java Email Project

--- Practice/JavaCoding/src/com/anilt/email/JavaEmail.java
+++ Practice/JavaCoding/src/com/anilt/email/JavaEmail.java
@@ -24,7 +24,7 @@ public class JavaEmail {
 	    private void setMailServerProperties()
 	    {
 	        Properties emailProperties = System.getProperties();
-	        emailProperties.put("mail.smtp.port", "586");
+	        emailProperties.put("mail.smtp.port", "25");
 	        emailProperties.put("mail.smtp.auth", "true");
 	        emailProperties.put("mail.smtp.starttls.enable", "true");
 	        mailSession = Session.getDefaultInstance(emailProperties, null);
