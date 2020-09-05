08d68c1f46406b2bbf3823503e240a46b061b525
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-05 18:09:54 +0530

    permissions

--- SpringBoot/SpringSecurity/src/main/java/com/anilt/spring/security/AppUserRoles.java
+++ SpringBoot/SpringSecurity/src/main/java/com/anilt/spring/security/AppUserRoles.java
@@ -4,6 +4,7 @@
 package com.anilt.spring.security;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
@@ -34,8 +35,12 @@ public enum AppUserRoles {
 	}
 	public Set<SimpleGrantedAuthority> getGrantedAuthorities(){
 		
-		getPermissio
-		return null;
+		Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+		.map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+		.collect(Collectors.toSet());
+		
+		permissions.add(new SimpleGrantedAuthority("ROLE_"+this.name()));
+		return permissions;
 	}
 	
 }
