7f0cc2920752d569a5aa48d956232f04a6939a0a
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-05 18:24:07 +0530

    Bug fix

--- SpringBoot/SpringSecurity/src/main/java/com/anilt/spring/security/AppUserRoles.java
+++ SpringBoot/SpringSecurity/src/main/java/com/anilt/spring/security/AppUserRoles.java
@@ -4,6 +4,7 @@
 package com.anilt.spring.security;
 
 import java.util.Set;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
