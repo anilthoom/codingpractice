70aeb645ae084df49ed068e1097105b2f454e7cf
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-07 11:01:32 +0530

    enum test

--- Practice/JavaCoding/src/com/anilt/enumtest/EnumTest.java
+++ Practice/JavaCoding/src/com/anilt/enumtest/EnumTest.java
@@ -1,5 +1,18 @@
 package com.anilt.enumtest;
 
 public enum EnumTest {
-
+	RED("stop"),
+	ORANGE("slowdown"),
+	GREEN("go");
+	
+	private String action;
+	
+	public String getAcction()
+	{
+		return this.action;
+	}
+	
+	private EnumTest(String action) {
+		this.action= action;
+	}
 }
