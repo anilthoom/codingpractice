b871cb4581c0ffb428ca3717a55b2f4ea6d94eae
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-07 11:15:47 +0530

    executor

--- Practice/JavaCoding/src/com/anilt/enumtest/EnumExe.java
+++ Practice/JavaCoding/src/com/anilt/enumtest/EnumExe.java
@@ -2,4 +2,13 @@ package com.anilt.enumtest;
 
 public class EnumExe {
 
+	public static void main(String[] args) {
+		EnumTest[] et = EnumTest.values();
+		for (EnumTest enumTest : et) {
+			System.out.println("Name : "+enumTest.name()
+			+" Action : "
+			+ enumTest.getAcction()
+			);
+		}
+	}
 }
