2863cab5922a8b72ee9afc7aae79296f11e6cacd
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-11 17:35:24 +0530

    python changes

--- Python/Classes/AccessSpecifiers.py
+++ Python/Classes/AccessSpecifiers.py
@@ -1,13 +1,17 @@
-class AccessSpecifiersDemo:
+class AccessSpecifiersDemo():
     def __init__(self):
         self.__pri = ("I am PRIVATE variable")
         self._pro = ("I am PROTECTED variable")
         self.pub = ("I am PUBLIC variable")
 
     def __privateMeth(self):
-        
+        print("In Private method")
 
-asd = AccessSpecifiersDemo()
-print(asd.pub)
-print(asd._pro)
-# print(asd.__pri)
+
+ob = AccessSpecifiersDemo()
+# print(ob.pub)
+# print(ob._pro)
+# print(ob.__pri)
+
+ob._AccessSpecifiersDemo__privateMeth() # Accessing private method
+print (ob._AccessSpecifiersDemo__pri) # Accessing private variable
