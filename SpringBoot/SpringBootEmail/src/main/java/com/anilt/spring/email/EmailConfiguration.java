d8f6d74c9cda0c95e7955b2faf0db1d500428682
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-11 15:41:16 +0530

    Getter annotation

--- SpringBoot/SpringBootEmail/src/main/java/com/anilt/spring/email/EmailConfiguration.java
+++ SpringBoot/SpringBootEmail/src/main/java/com/anilt/spring/email/EmailConfiguration.java
@@ -3,6 +3,9 @@ package com.anilt.spring.email;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import lombok.Getter;
+
+@Getter
 @Component
 public class EmailConfiguration {
 
