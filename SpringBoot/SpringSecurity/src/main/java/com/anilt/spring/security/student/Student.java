83b17d5fc1319489ad74e54946f70ef5cc7d0ce6
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-11 11:15:12 +0530

    Lombok Getters

--- SpringBoot/SpringSecurity/src/main/java/com/anilt/spring/security/student/Student.java
+++ SpringBoot/SpringSecurity/src/main/java/com/anilt/spring/security/student/Student.java
@@ -1,19 +1,11 @@
 package com.anilt.spring.security.student;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Student {
 	private final Integer studentId;
 	private final String studentName;
-	
-	public Student(Integer studentId, String studentName) {
-		super();
-		this.studentId = studentId;
-		this.studentName = studentName;
-	}
-	public Integer getStudentId() {
-		return studentId;
-	}
-	public String getStudentName() {
-		return studentName;
-	}
-	
 }
