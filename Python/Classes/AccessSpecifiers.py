d283cf2604817c00dcd88cf383cb620ae43db6bd
Thoom Anil Kumar <anilkumar.thoom@gmail.com>
2020-09-13 12:19:31 +0530

    fixed error

--- Python/Classes/AccessSpecifiers.py
+++ Python/Classes/AccessSpecifiers.py
@@ -16,7 +16,7 @@ ob = AccessSpecifiersDemo()
 # print(ob._pro)
 # print(ob.__pri)
 
-ob._AccessSpecifiersDemo__privateMeth() # Accessing private method
+# ob._AccessSpecifiersDemo__privateMeth() # Accessing private method
 print (ob._AccessSpecifiersDemo__pri) # Accessing private variable
 #print(ob._protectedmeth())
 # STOPPED AT 1 HOUR RESUME FROM CONSTRUCTEOR  https://learning.edureka.co/classroom/recording/777/7014/123449?tab=CourseContent
