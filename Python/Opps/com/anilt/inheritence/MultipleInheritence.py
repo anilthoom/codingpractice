44e39778936aaa2788264ea49126aa21bd4e8ad4
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-13 15:40:01 +0530

    Multiple Inheritence Demo

--- Python/Opps/com/anilt/inheritence/MultipleInheritence.py
+++ Python/Opps/com/anilt/inheritence/MultipleInheritence.py
@@ -2,3 +2,8 @@ class First(object):
     def __init__(self):
         super(First, self).__init__()
         print("First")
+
+class Second(object)
+    def __init__(self):
+        super(Second, self).__init__()
+        print("Second")
