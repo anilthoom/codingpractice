7761cd15ef8a8bd4baa4c2fad865caba42d296e1
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-14 15:10:16 +0530

    Output

---
+++ TypeScript/BasicsPractice.js
@@ -0,0 +1,18 @@
+console.log("Hello Brother....");
+/**
+ * Variables
+ */
+var found = true;
+var grade = 80;
+var cname = "ABC";
+var anyVar = "21nal";
+console.log(found, grade, cname, anyVar);
+/**
+ * Template Strings
+ */
+var firstName = "Anil";
+var secondName = "Kumar";
+console.log("Hi " + firstName + " " + secondName);
+/**
+ * For loop
+ */
