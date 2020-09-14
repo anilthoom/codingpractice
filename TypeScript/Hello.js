b7da1deb9831b8f3e4efebff52f1eae720fc13f4
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-14 14:55:02 +0530

    TypeScript : Variable demo

--- TypeScript/Hello.js
+++ TypeScript/Hello.js
@@ -1,1 +1,9 @@
-console.log("Hello Bhayya....");
+console.log("Hello Brother....");
+/**
+ * Variables
+ */
+var found = true;
+var grade = 80;
+var cname = "ABC";
+var anyVar = "21nal";
+console.log(found, grade, cname, anyVar);
--- TypeScript/Hello.ts
+++ TypeScript/Hello.ts
@@ -1,1 +1,15 @@
 console.log("Hello Brother....")
+
+/**
+ * Variables 
+ */
+let found: boolean = true;
+let grade: number = 80;
+let cname: string = "ABC";
+let anyVar: any = "21nal";
+
+console.log(found, grade, cname, anyVar);
+
+/**
+ * Template Strings 
+ */
