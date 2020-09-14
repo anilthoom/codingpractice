931f9377d8f0f318829650e2822379a9d5029e3e
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-14 15:45:20 +0530

    Class constructor

--- TypeScript/ClassDemo.ts
+++ TypeScript/ClassDemo.ts
@@ -1,4 +1,9 @@
 class Customer{
-
+    firstName: string;
+    lastName: string;
     
+    constructor(fName: string, lName: string){
+        this.firstName = fName;
+        this.lastName =  lName;
+    }
 }
