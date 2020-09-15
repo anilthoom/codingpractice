4a61b86e14283cd3cf422b3eaf671d1a076fb446
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-15 14:55:34 +0530

    New instance

--- TypeScript/ImportClassDemo.js
+++ TypeScript/ImportClassDemo.js
@@ -4,3 +4,6 @@ var Customers_1 = require("./Customers");
 var myObj = new Customers_1.Customers("Thoom", "Shri Tharunika");
 console.log(myObj.firstName);
 console.log(myObj.lastName);
+var newObj = new Customers_1.Customers("Thoom", "Shrihan Chandra");
+console.log(newObj.firstName);
+console.log(newObj.lastName);
--- TypeScript/ImportClassDemo.ts
+++ TypeScript/ImportClassDemo.ts
@@ -3,3 +3,7 @@ import {Customers} from './Customers'
 let myObj = new Customers("Thoom", "Shri Tharunika");
 console.log(myObj.firstName);
 console.log(myObj.lastName);
+
+let newObj = new Customers("Thoom", "Shrihan Chandra");
+console.log(newObj.firstName);
+console.log(newObj.lastName);
