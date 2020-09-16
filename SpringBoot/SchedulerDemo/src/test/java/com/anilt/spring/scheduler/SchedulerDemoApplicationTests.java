eacfae4aa8542f7d00d2886a80f9245d1fd2c6f8
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-16 14:32:47 +0530

    Scheduler Project Creation

---
+++ SpringBoot/SchedulerDemo/src/test/java/com/anilt/spring/scheduler/SchedulerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anilt.spring.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
