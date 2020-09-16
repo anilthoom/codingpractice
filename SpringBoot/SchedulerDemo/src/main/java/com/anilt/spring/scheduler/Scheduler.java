8214f5718916411de40cbadd54c9964e48add561
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-16 14:42:21 +0530

    Fixed rate scheduler

--- SpringBoot/SchedulerDemo/src/main/java/com/anilt/spring/scheduler/Scheduler.java
+++ SpringBoot/SchedulerDemo/src/main/java/com/anilt/spring/scheduler/Scheduler.java
@@ -8,11 +8,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Scheduler {
-	@Scheduled(cron = "0 * 9 * * ?")
+	@Scheduled(cron = "1 * 4 * * ?")
 	public void cronJobSch() {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 		Date now = new Date();
 		String startDate = sdf.format(now);
 		System.out.println("Java cron job expression :: "+ startDate);
 	}
+	
+	@Scheduled(fixedRate = 10000)
+	   public void fixedRateSch() {
+	      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+	      Date now = new Date();
+	      String strDate = sdf.format(now);
+	      System.out.println("Fixed Rate scheduler:: " + strDate);
+	   }
 }
