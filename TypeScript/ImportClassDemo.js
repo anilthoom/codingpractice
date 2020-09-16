70a9c9d2b341111a459607de22098801138930cb
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-16 16:56:49 +0530

    commit

--- TypeScript/ImportClassDemo.js
+++ TypeScript/ImportClassDemo.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Customers_1 = require("./Customers");
-var myObj = new Customers_1.Customers("Thoom", "Shri Tharunika");
+var myObj = new Customers_1.Customers("T", "Shri Tharunika");
 console.log(myObj.firstName);
 console.log(myObj.lastName);
-var newObj = new Customers_1.Customers("Thoom", "Shrihan Chandra");
+var newObj = new Customers_1.Customers("T", "Shrihan Chandra");
 console.log(newObj.firstName);
 console.log(newObj.lastName);
