791cacccd5d669f062a9b631514a3b018239ac7d
Thoom Anil Kumar <anilkumar.thoom@gmail.com>
2020-09-19 14:46:40 +0530

    Changes

--- TypeScript/ImportClassDemo.ts
+++ TypeScript/ImportClassDemo.ts
@@ -4,6 +4,6 @@ let myObj = new Customers("T", "Shri Tharunika");
 console.log(myObj.firstName);
 console.log(myObj.lastName);
 
-let newObj = new Customers("T", "Shrihan Chandra");
+let newObj = new Customers("T", "Shrihan");
 console.log(newObj.firstName);
 console.log(newObj.lastName);
