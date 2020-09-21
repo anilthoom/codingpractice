6125bd5d5f5b575654fbaf7e2e7e4235f3d2c778
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-21 16:20:57 +0530

    Dist function

--- Python/ModulesTest/MathTest.py
+++ Python/ModulesTest/MathTest.py
@@ -4,3 +4,9 @@ import math
 print(math.ceil(1.3))
 print(math.ceil(10.1))
 print((math.ceil(-5.9)))
+
+
+# Find the Euclidean distance between two single-dimensional points: math.dist(p,q)
+p = [3]
+q = [6]
+print(math.dist(p, q))
