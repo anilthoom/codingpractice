829b3774cdc89ca28cdc48c821ea2c6c4835cb47
Thoom Anil Kumar <anilkumar.thoom@gmail.com>
2020-09-21 20:04:55 +0530

    Class name changes

--- TypeScript/ImportClassDemo.ts
+++ TypeScript/ImportClassDemo.ts
@@ -1,9 +1,9 @@
-import {Customers} from './Customers'
+import {CustomersClass} from './Customers'
 
-let myObj = new Customers("T", "Shri Tharunika");
+let myObj = new CustomersClass("T", "Shri Tharunika");
 console.log(myObj.firstName);
 console.log(myObj.lastName);
 
-let newObj = new Customers("T", "Shrihan");
+let newObj = new CustomersClass("T", "Shrihan");
 console.log(newObj.firstName);
 console.log(newObj.lastName);
