171e8e1514dcfda698dbbdb45bb78a059910a15b
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-22 13:39:57 +0530

    Distance using Math

--- Python/EdurekaAssignments/com/anil/edureka/module3/casestudy1.py
+++ Python/EdurekaAssignments/com/anil/edureka/module3/casestudy1.py
@@ -34,10 +34,18 @@ else:
 #     Hint: Use time module.
 import datetime
 
-
 currentDateTime = datetime.datetime.now()
 print(currentDateTime.hour)
 if currentDateTime.hour >= 19 or currentDateTime.hour <= 6:
     print("It's dark outside!! Take torch light with you!!")
 else:
     print("It bright outside... enjoy D vitamin .... ")
+
+# 4. Write a program to find distance between two locations when their latitude and
+#     longitudes are given.
+from math import sin, cos, atan2, radians
+lat1 = radians(52.2296756)
+lon1 = radians(21.0122287)
+lat2 = radians(52.406374)
+lon2 = radians(16.9251681)
+print(lat1, lon1, lat2, lon2)
