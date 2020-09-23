dd250f6751f5506dec308d23d64810e395a334ad
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-23 14:30:27 +0530

    one, two D array

--- Python/NumpyPractice/NumpyPractice.py
+++ Python/NumpyPractice/NumpyPractice.py
@@ -1,11 +1,15 @@
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 7, 2, 3])
-print(arr)
-print(type(arr))
+# 0 - D array
+ar = np.array(64)
+print(ar)
 
-oneDarr = np.array([[1,2],
+# 1- D array
+oneDarr = np.array([1, 2, 3, 4, 7, 2, 3])
+print(oneDarr)
+
+# 2- D array
+twoDarr = np.array([[1,2],
                     [3,4]
                     ])
-print(type(oneDarr))
-print(oneDarr)
+print(twoDarr)
