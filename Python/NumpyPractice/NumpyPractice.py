9fdc9f6360644f4c0d3a0b4450619eeb4b01bab3
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-23 14:10:05 +0530

    one D array

--- Python/NumpyPractice/NumpyPractice.py
+++ Python/NumpyPractice/NumpyPractice.py
@@ -3,3 +3,9 @@ import numpy as np
 arr = np.array([1, 2, 3, 4, 7, 2, 3])
 print(arr)
 print(type(arr))
+
+oneDarr = np.array([[1,2],
+                    [3,4]
+                    ])
+print(type(oneDarr))
+print(oneDarr)
