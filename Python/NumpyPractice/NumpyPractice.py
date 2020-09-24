d762b4617eb71fd7196356b5c48c898e764a8b06
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-09-24 11:12:56 +0530

    Array indexes

--- Python/NumpyPractice/NumpyPractice.py
+++ Python/NumpyPractice/NumpyPractice.py
@@ -3,7 +3,7 @@ import numpy as np
 a = np.array(42)
 b = np.array([1, 2, 3, 4, 5])
 c = np.array([[1, 2, 3], [4, 5, 6]])
-d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
 
 print(a.ndim)
 print(b.ndim)
@@ -11,7 +11,7 @@ print(c.ndim)
 print(d.ndim)
 
 print(b[1] + b[4])
-
+print("Indexes example usage : ", d[0,1], d[1][1])
 arr = np.array([1, 2, 3, 4], ndmin=5)
 
 print(arr)
