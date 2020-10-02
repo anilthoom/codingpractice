2a43bec2c2028ff6ea170f4577100083363a2d60
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-02 19:11:58 +0530

    Added numpy

--- Python/NumpyPractice/TimeDemo.py
+++ Python/NumpyPractice/TimeDemo.py
@@ -1,4 +1,12 @@
 import datetime
-x=datetime.datetime.now()
+import numpy as np
+x = datetime.datetime.now()
 print(x.hour,"This is in 24 hour formate")
 print(x.minute,'This is minute')
+students2 = np.array(["Anil", "Shrihan", "Shritha", "Ramya", "Abc", "Bde"], dtype='S')
+print(students2.dtype)
+
+intArr = np.array([0, 1, 2])
+boolArr = intArr.astype(bool)
+print(boolArr.dtype)
+print(boolArr[2])
