723366f1391e8c069f44d4d8159dc6e7395e5333
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-03 14:36:51 +0530

    Empty array of type shape

--- TypeScript/02-Inheritence/DrawingUsingArray.ts
+++ TypeScript/02-Inheritence/DrawingUsingArray.ts
@@ -5,3 +5,6 @@ import { Rectangle } from './Rectangle';
 let myShape = new Shape(10, 20);
 let myCircle = new Circle(15, 25, 5);
 let myRectangle = new Rectangle(0, 0, 3, 4);
+
+//Empty array of type Shape
+let theShapes: Shape[] = [];
