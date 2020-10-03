8f958b9cc646df9ec6514025033af32977c943cf
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-03 15:21:25 +0530

    Coaching tips... implementation

---
+++ TypeScript/04-Interfaces/DriverClass.ts
@@ -0,0 +1,8 @@
+import { Coach } from "./Coach";
+import { CricketCoach } from "./CricketCoach";
+import { GolfCoach } from "./GolfCoach";
+
+let cricketPracticeTip = new CricketCoach();
+let golfPracticeTip = new GolfCoach();
+
+let coachingTips: Coach[] = [];
