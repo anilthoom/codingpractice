732b944602820c3a0602740532f11c91b19e0552
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-03 14:36:02 +0530

    Array Drawing

---
+++ TypeScript/02-Inheritence/DrawingUsingArray.ts
@@ -0,0 +1,7 @@
+import {Shape} from './Shape';
+import {Circle} from './Circle';
+import { Rectangle } from './Rectangle';
+
+let myShape = new Shape(10, 20);
+let myCircle = new Circle(15, 25, 5);
+let myRectangle = new Rectangle(0, 0, 3, 4);
