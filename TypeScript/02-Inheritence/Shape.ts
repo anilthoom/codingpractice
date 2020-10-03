c7a9d431b90d7379b9120a3a54f8f3c4b4765334
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-03 13:28:20 +0530

    Variables

--- TypeScript/02-Inheritence/Shape.ts
+++ TypeScript/02-Inheritence/Shape.ts
@@ -1,5 +1,11 @@
 export class Shape{
     private _x: number;
+    public get x(): number {
+        return this._x;
+    }
+    public set x(value: number) {
+        this._x = value;
+    }
     private _y: number;
     
 }
