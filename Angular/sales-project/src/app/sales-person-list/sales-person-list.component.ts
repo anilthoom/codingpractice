8432b2b520265491e078d684df8546301a7aa3f8
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-03 17:05:13 +0530

    Records added

--- Angular/sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ Angular/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sales-person-list.component.css']
 })
 export class SalesPersonListComponent implements OnInit {
+
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Anil", "Kumar", "anil@abc.com", 50000),
+    new SalesPerson("Ramya", "V", "ramya@abc.com", 60000),
+    new SalesPerson("Shrihan", "Chandra", "shrihan@abc.com", 40000),
+    new SalesPerson("Shritha", "T", "shritha@abc.com", 90000)
+  ];
 
   constructor() { }
 
