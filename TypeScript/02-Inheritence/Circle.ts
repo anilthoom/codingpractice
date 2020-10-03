0dfa52843f5e482df21309f98934c21d09bd4359
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-03 13:40:37 +0530

    Super class constructor changes

--- TypeScript/02-Inheritence/Circle.ts
+++ TypeScript/02-Inheritence/Circle.ts
@@ -1,6 +1,15 @@
 import { Shape } from './Shape';
 
 export class Circle extends Shape{
-    private _radius: number;
+    
+    constructor(theX: number, theY: number, private _radius: number){
+        super(theX, theY);
+    }
+    public get radius(): number {
+        return this._radius;
+    }
+    public set radius(value: number) {
+        this._radius = value;
+    }
     
 }
