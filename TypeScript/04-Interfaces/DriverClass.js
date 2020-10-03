e1fd28488741149f6fcf49a41db69943982bd8c8
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-03 15:24:12 +0530

    Final Interface Changes

--- TypeScript/04-Interfaces/DriverClass.js
+++ TypeScript/04-Interfaces/DriverClass.js
@@ -5,6 +5,8 @@ var GolfCoach_1 = require("./GolfCoach");
 var cricketPracticeTip = new CricketCoach_1.CricketCoach();
 var golfPracticeTip = new GolfCoach_1.GolfCoach();
 var coachingTips = [];
+coachingTips.push(cricketPracticeTip);
+coachingTips.push(golfPracticeTip);
 coachingTips.forEach(function (tip) {
     console.log(tip.getDailyWorkout());
 });
