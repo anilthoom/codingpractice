9651ab853928802af8548e8ced7823f12c3a5802
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-12 16:37:33 +0530

    Student Service

---
+++ SpringBoot/StudentService/src/test/java/com/anilt/spring/student/StudentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anilt.spring.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
