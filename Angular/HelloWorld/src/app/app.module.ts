473989b1b4e0b320fd12c9ff67294c24a97ecc8a
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-10-25 14:58:50 +0530

    Angular.json

--- Angular/HelloWorld/src/app/app.module.ts
+++ Angular/HelloWorld/src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
