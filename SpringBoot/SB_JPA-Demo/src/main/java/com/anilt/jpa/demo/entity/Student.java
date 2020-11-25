78cae4bbb84190b1a8763ad82d3370d7c287f368
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-11-25 10:14:05 +0530

    mapping with table name

--- SpringBoot/SB_JPA-Demo/src/main/java/com/anilt/jpa/demo/entity/Student.java
+++ SpringBoot/SB_JPA-Demo/src/main/java/com/anilt/jpa/demo/entity/Student.java
@@ -1,9 +1,11 @@
 package com.anilt.jpa.demo.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.util.Date;
 
 @Entity
+@Table(name = "student")
 public class Student {
     private Integer student_id;
     private String student_name;
