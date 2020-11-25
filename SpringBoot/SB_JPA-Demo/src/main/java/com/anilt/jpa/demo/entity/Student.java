163b17a61025cd4d0fd6065664f842c70ebc980d
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-11-25 10:10:17 +0530

    Student properties

--- SpringBoot/SB_JPA-Demo/src/main/java/com/anilt/jpa/demo/entity/Student.java
+++ SpringBoot/SB_JPA-Demo/src/main/java/com/anilt/jpa/demo/entity/Student.java
@@ -1,9 +1,12 @@
 package com.anilt.jpa.demo.entity;
 
+import java.util.Date;
+
 public class Student {
     //TODO: Need to add attributes
     private Integer student_id;
     private String student_name;
-
+    private Date student_dob;
+    
 
 }
