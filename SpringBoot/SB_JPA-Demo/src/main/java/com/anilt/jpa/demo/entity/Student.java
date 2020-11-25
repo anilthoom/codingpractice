0b4c235e3c998e69fc710731bdd301d81ba202ca
anilkumar.thoom <anilkumar.thoom@gmail.com>
2020-11-25 10:14:33 +0530

    Getter Setter Lombok

--- SpringBoot/SB_JPA-Demo/src/main/java/com/anilt/jpa/demo/entity/Student.java
+++ SpringBoot/SB_JPA-Demo/src/main/java/com/anilt/jpa/demo/entity/Student.java
@@ -1,11 +1,16 @@
 package com.anilt.jpa.demo.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.util.Date;
 
 @Entity
 @Table(name = "student")
+@Getter
+@Setter
 public class Student {
     private Integer student_id;
     private String student_name;
