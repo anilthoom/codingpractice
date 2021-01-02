c5290ff799175ef5cb03d2b9c4058ce1795779d0
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-01-02 12:33:48 +0530

    duplicate files

--- Practice/JavaCoding/SpringBoot/SB_JPA-Demo/src/main/java/com/anilt/jpa/demo/dao/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.anilt.jpa.demo.dao;
-
-import com.anilt.jpa.demo.entity.Test;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TestRepository extends CrudRepository<Test, Integer> {
-}
--- Practice/JavaCoding/SpringBoot/SpringBatchDemo/src/main/java/com/anilt/spring/batch/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.anilt.spring.batch.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.anilt.spring.batch.model.User;;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-}
