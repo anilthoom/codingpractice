eea9bd9e5e56629694cfe85aaaee244c0bdb67a6
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-01-06 10:48:52 +0530

    Factorial Problem

--- JavaCoding/src/com/anilt/problems/FactorialNumber.java
+++ JavaCoding/src/com/anilt/problems/FactorialNumber.java
@@ -5,7 +5,6 @@ public class FactorialNumber {
     {
         printFact(5);
 //		System.out.println(printFactRecursive(5));
-        printStringComb("abc", "");
 //		printStringComb("abcdefghijklmnopqrstuvwxyz", "");
     }
     public static int printFactRecursive(int num)
@@ -28,20 +27,5 @@ public class FactorialNumber {
             result = result*i;
         }
         System.out.println(result);
-    }
-    public static void printStringComb(String str, String ans)
-    {
-        if(str.length() == 0)
-        {
-            System.out.print(ans + " ");
-            return;
-        }
-        //abc
-        for(int i = 0; i < str.length(); i++)
-        {
-            char ch = str.charAt(i);
-            String subStr = str.substring(0, i)+str.substring(i+1);
-            printStringComb(subStr, ans+ch);
-        }
     }
 }
