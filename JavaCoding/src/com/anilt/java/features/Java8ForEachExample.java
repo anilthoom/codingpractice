c8df6a20764aaecf2cc07d923566faa1bbf65e68
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-01-07 09:34:58 +0530

    Foreach Java8

--- JavaCoding/src/com/anilt/java/features/Java8ForEachExample.java
+++ JavaCoding/src/com/anilt/java/features/Java8ForEachExample.java
@@ -7,9 +7,7 @@ import java.util.function.Consumer;
 import java.lang.Integer;
 
 public class Java8ForEachExample {
-
     public static void main(String[] args) {
-
         //creating sample Collection
         List<Integer> myList = new ArrayList<Integer>();
         for(int i=0; i<10; i++) myList.add(i);
@@ -23,13 +21,11 @@ public class Java8ForEachExample {
 
         //traversing through forEach method of Iterable with anonymous class
         myList.forEach(new Consumer<Integer>() {
-
             public void accept(Integer t) {
                 System.out.println("forEach anonymous class Value::"+t);
             }
 
         });
-
         //traversing with Consumer interface implementation
         MyConsumer action = new MyConsumer();
         myList.forEach(action);
