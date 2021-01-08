8410fe324069dce9dea68046e28a10ba8913f294
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-01-08 10:42:54 +0530

    Enum Month Use

---
+++ JavaCoding/src/com/anilt/java/features/enumtest/EnumMonthUse.java
@@ -0,0 +1,12 @@
+package com.anilt.java.features.enumtest;
+
+public class EnumMonthUse
+{
+
+	public static void main(String[] args) 
+	{
+		if(Month.June.name().equalsIgnoreCase("June"))
+			System.out.println("Looks good");
+	}
+
+}
