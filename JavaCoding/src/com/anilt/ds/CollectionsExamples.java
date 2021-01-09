34b282bec1bc74b89653d6aefdb0e953149c2865
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-01-09 11:09:24 +0530

    Collections

--- JavaCoding/src/com/anilt/ds/CollectionsExamples.java
+++ JavaCoding/src/com/anilt/ds/CollectionsExamples.java
@@ -16,166 +16,163 @@ import java.util.Stack;
 import java.util.TreeSet;
 import java.util.Vector;
 
-public class CollectionsExamples 
-{
-	public static void main(String[] args) 
-	{
+public class CollectionsExamples {
+    public static void main(String[] args) {
 
-		List <String> list1= new ArrayList();  
-		List <String> list2 = new LinkedList();  
-		List <String> list3 = new Vector();  
-		List <String> list4 = new Stack();  
+        List<String> list1 = new ArrayList();
+        List<String> list2 = new LinkedList();
+        List<String> list3 = new Vector();
+        List<String> list4 = new Stack();
 //		arrayList();
 //		hashSet();
-		treeSet();
-	}
-	public static void arrayList()
-	{  
-		ArrayList<String> list=new ArrayList<String>();//Creating arraylist  
-		list.add("Ravi");//Adding object in arraylist  
-		list.add("Vijay");  
-		list.add("Ravi");  
-		list.add("Ajay"); 
-		boolean isContains = list.contains("Ajay");
-		System.out.println(isContains);
-		//Traversing list through Iterator  
-		Iterator<String> itr=list.iterator();  
-		while(itr.hasNext())
-		{  
-			System.out.println(itr.next());  
-		}  
-	} 
-	public static void linkedList()
-	{  
-		LinkedList<String> al=new LinkedList<String>();  
-		al.add("Ravi");  
-		al.add("Vijay");  
-		al.add("Ravi");  
-		al.add("Ajay");
-		System.out.println(al.peek());
-		Iterator<String> itr=al.iterator();  
-		while(itr.hasNext()){  
-		System.out.println(itr.next());  
-		}  
-	}  
-	public static void vector()
-	{  
-		Vector<String> v=new Vector<String>();  
-		v.add("Ayush");  
-		v.add("Amit");  
-		v.add("Ashish");  
-		v.add("Garima");  
-		Iterator<String> itr=v.iterator();  
-		while(itr.hasNext()){  
-		System.out.println(itr.next());  
-		}  
-	}
-	public static void stack()
-	{  
-		Stack<String> stack = new Stack<String>();  
-		stack.push("Ayush");  
-		stack.push("Garvit");  
-		stack.push("Amit");  
-		stack.push("Ashish");  
-		stack.push("Garima");  
-		stack.pop();  
-		Iterator<String> itr=stack.iterator();  
-		while(itr.hasNext()){  
-		System.out.println(itr.next());  
-		}  
-	}
-	public static void priorityQueue()
-	{  
-		PriorityQueue<String> queue=new PriorityQueue<String>();  
-		queue.add("Amit Sharma");  
-		queue.add("Vijay Raj");  
-		queue.add("JaiShankar");  
-		queue.add("Raj");  
-		System.out.println("head:"+queue.element());  
-		System.out.println("head:"+queue.peek());  
-		System.out.println("iterating the queue elements:");  
-		Iterator itr=queue.iterator();  
-		while(itr.hasNext()){  
-		System.out.println(itr.next());  
-		}  
-		queue.remove();  
-		queue.poll();  
-		System.out.println("after removing two elements:");  
-		Iterator<String> itr2=queue.iterator();  
-		while(itr2.hasNext()){  
-		System.out.println(itr2.next());  
-		}  
-	}
-	public static void arrayDeque()
-	{  
-		//Creating Deque and adding elements  
-		Deque<String> deque = new ArrayDeque<String>();  
-		deque.add("Gautam");  
-		deque.add("Karan");  
-		deque.add("Ajay");  
-		//Traversing elements  
-		for (String str : deque) {  
-		System.out.println(str);  
-		}  
-	}
-	public static void hashSet()
-	{  
-		//Creating HashSet and adding elements  
-		HashSet<String> set=new HashSet<String>();  
-		set.add("Ravi");  
-		set.add("Vijay");  
-		set.add("Ravi");  
-		set.add("Ajay");
-		set.add("Ravi");
-		//Traversing elements  
-		Iterator<String> itr=set.iterator();  
-		while(itr.hasNext()){  
-		System.out.println(itr.next());  
-		}  
-	}
-	public static void linkedHasSet()
-	{  
-		LinkedHashSet<String> set=new LinkedHashSet<String>();  
-		set.add("Ravi");  
-		set.add("Vijay");  
-		set.add("Ravi");  
-		set.add("Ajay");  
-		Iterator<String> itr=set.iterator();  
-		while(itr.hasNext()){  
-		System.out.println(itr.next());  
-		}  
-	}
-	public static void treeSet()
-	{  
-		//Creating and adding elements  
-		TreeSet<String> set=new TreeSet<String>();  
-		set.add("Z");  
-		set.add("X");  
-		set.add("G");
-		set.add("X");
-		set.add("K");
-		set.add("A");
-		//traversing elements  
-		Iterator<String> itr=set.iterator();  
-		while(itr.hasNext()){  
-		System.out.println(itr.next());  
-		}  
-	}
-	public static void hashMap()
-	{  
-	    Map map=new HashMap();  
-	    //Adding elements to map  
-	    map.put(1,"Amit");  
-	    map.put(5,"Rahul");  
-	    map.put(2,"Jai");  
-	    map.put(6,"Amit");  
-	    //Traversing Map  
-	    Set set=map.entrySet();//Converting to Set so that we can traverse  
-	    Iterator itr=set.iterator();  
-	    while(itr.hasNext()){  
-	        //Converting to Map.Entry so that we can get key and value separately  
-	        Map.Entry entry=(Map.Entry)itr.next();  
-	        System.out.println(entry.getKey()+" "+entry.getValue());  
-	    }  
-	}  
+        treeSet();
+    }
+
+    public static void arrayList() {
+        ArrayList<String> list = new ArrayList<String>();//Creating arraylist
+        list.add("Ravi");//Adding object in arraylist
+        list.add("Vijay");
+        list.add("Ravi");
+        list.add("Ajay");
+        boolean isContains = list.contains("Ajay");
+        System.out.println(isContains);
+        //Traversing list through Iterator
+        Iterator<String> itr = list.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+
+    public static void linkedList() {
+        LinkedList<String> al = new LinkedList<String>();
+        al.add("Ravi");
+        al.add("Vijay");
+        al.add("Ravi");
+        al.add("Ajay");
+        System.out.println(al.peek());
+        Iterator<String> itr = al.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+
+    public static void vector() {
+        Vector<String> v = new Vector<String>();
+        v.add("Ayush");
+        v.add("Amit");
+        v.add("Ashish");
+        v.add("Garima");
+        Iterator<String> itr = v.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+
+    public static void stack() {
+        Stack<String> stack = new Stack<String>();
+        stack.push("Ayush");
+        stack.push("Garvit");
+        stack.push("Amit");
+        stack.push("Ashish");
+        stack.push("Garima");
+        stack.pop();
+        Iterator<String> itr = stack.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+
+    public static void priorityQueue() {
+        PriorityQueue<String> queue = new PriorityQueue<String>();
+        queue.add("Amit Sharma");
+        queue.add("Vijay Raj");
+        queue.add("JaiShankar");
+        queue.add("Raj");
+        System.out.println("head:" + queue.element());
+        System.out.println("head:" + queue.peek());
+        System.out.println("iterating the queue elements:");
+        Iterator itr = queue.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+        queue.remove();
+        queue.poll();
+        System.out.println("after removing two elements:");
+        Iterator<String> itr2 = queue.iterator();
+        while (itr2.hasNext()) {
+            System.out.println(itr2.next());
+        }
+    }
+
+    public static void arrayDeque() {
+        //Creating Deque and adding elements
+        Deque<String> deque = new ArrayDeque<String>();
+        deque.add("Gautam");
+        deque.add("Karan");
+        deque.add("Ajay");
+        //Traversing elements
+        for (String str : deque) {
+            System.out.println(str);
+        }
+    }
+
+    public static void hashSet() {
+        //Creating HashSet and adding elements
+        HashSet<String> set = new HashSet<String>();
+        set.add("Ravi");
+        set.add("Vijay");
+        set.add("Ravi");
+        set.add("Ajay");
+        set.add("Ravi");
+        //Traversing elements
+        Iterator<String> itr = set.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+
+    public static void linkedHasSet() {
+        LinkedHashSet<String> set = new LinkedHashSet<String>();
+        set.add("Ravi");
+        set.add("Vijay");
+        set.add("Ravi");
+        set.add("Ajay");
+        Iterator<String> itr = set.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+
+    public static void treeSet() {
+        //Creating and adding elements
+        TreeSet<String> set = new TreeSet<String>();
+        set.add("Z");
+        set.add("X");
+        set.add("G");
+        set.add("X");
+        set.add("K");
+        set.add("A");
+        //traversing elements
+        Iterator<String> itr = set.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+
+    public static void hashMap() {
+        Map map = new HashMap();
+        //Adding elements to map
+        map.put(1, "Amit");
+        map.put(5, "Rahul");
+        map.put(2, "Jai");
+        map.put(6, "Amit");
+        //Traversing Map
+        Set set = map.entrySet();//Converting to Set so that we can traverse
+        Iterator itr = set.iterator();
+        while (itr.hasNext()) {
+            //Converting to Map.Entry so that we can get key and value separately
+            Map.Entry entry = (Map.Entry) itr.next();
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
 }
