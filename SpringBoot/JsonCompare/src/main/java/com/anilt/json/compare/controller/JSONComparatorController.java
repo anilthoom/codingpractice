b5c3d1abb20a141d0d0481fd5a0b0da26ef31826
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-02-01 16:29:08 +0530

    JSON parser

--- SpringBoot/JsonCompare/src/main/java/com/anilt/json/compare/controller/JSONComparatorController.java
+++ SpringBoot/JsonCompare/src/main/java/com/anilt/json/compare/controller/JSONComparatorController.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonParser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import static org.junit.Assert.*;
 
 @RestController
 @RequestMapping("/compare")
@@ -13,11 +14,25 @@ public class JSONComparatorController {
     public String isObjectSame(){
 
         JsonParser parser = new JsonParser();
-        String objectString = "{\"customer\": {\"fullName\": \"Emily Jenkins\", \"age\": 27 }}";
-        String arrayString = "[10, 20, 30]";
+        String objectString = "{\n" +
+                "    \"customer\": {\n" +
+                "        \"id\": 44521,\n" +
+                "        \"fullName\": \"Emily Jenkins\",\n" +
+                "        \"age\": 27\n" +
+                "    }\n" +
+                "}";
+        String arrayString = "{\n" +
+                "    \"customer\": {\n" +
+                "        \"id\": 44521,\n" +
+                "        \"age\": 27,\n" +
+                "        \"fullName\": \"Emily Jenkins\"\n" +
+                "    }\n" +
+                "}";
         JsonElement json1 = parser.parse(objectString);
         JsonElement json2 = parser.parse(arrayString);
 
+
+        assertEquals(json1, json2);
         return "OK : "+json2.isJsonObject();
     }
 }
