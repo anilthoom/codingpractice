deba7e31087fe5496fb894a86e34408880719104
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-02-01 16:11:14 +0530

    JSON parser

--- SpringBoot/JsonCompare/src/main/java/com/anilt/json/compare/controller/JSONComparatorController.java
+++ SpringBoot/JsonCompare/src/main/java/com/anilt/json/compare/controller/JSONComparatorController.java
@@ -1,5 +1,6 @@
 package com.anilt.json.compare.controller;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,8 @@ public class JSONComparatorController {
         JsonParser parser = new JsonParser();
         String objectString = "{\"customer\": {\"fullName\": \"Emily Jenkins\", \"age\": 27 }}";
         String arrayString = "[10, 20, 30]";
-
-        return "OK";
+        JsonElement json1 = parser.parse(objectString);
+        JsonElement json2 = parser.parse(arrayString);
+        return "OK : "+json1.isJsonObject();
     }
 }
