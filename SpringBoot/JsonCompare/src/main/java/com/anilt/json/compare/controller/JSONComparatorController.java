3afbe521ab0e4ead470d84a7ea2eb62e2ee17fc7
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-02-01 16:09:26 +0530

    JSON parser

--- SpringBoot/JsonCompare/src/main/java/com/anilt/json/compare/controller/JSONComparatorController.java
+++ SpringBoot/JsonCompare/src/main/java/com/anilt/json/compare/controller/JSONComparatorController.java
@@ -1,5 +1,6 @@
 package com.anilt.json.compare.controller;
 
+import com.google.gson.JsonParser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ public class JSONComparatorController {
     @GetMapping("/")
     public String isObjectSame(){
 
+        JsonParser parser = new JsonParser();
 
         return "OK";
     }
