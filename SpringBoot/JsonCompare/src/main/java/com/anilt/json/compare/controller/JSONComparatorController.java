8fabca814fc985d5b8877ad27e5b6cec0e3fbdd1
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-02-01 16:07:56 +0530

    Controller class

--- SpringBoot/JsonCompare/src/main/java/com/anilt/json/compare/controller/JSONComparatorController.java
+++ SpringBoot/JsonCompare/src/main/java/com/anilt/json/compare/controller/JSONComparatorController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/compare")
 public class JSONComparatorController {
     @GetMapping("/")
-    public String isObjectsSame(){
+    public String isObjectSame(){
 
 
         return "OK";
