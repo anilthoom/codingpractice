d4a034fd944c3f861876a1c512d1fd936cca6571
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-02-10 15:57:36 +0530

    Reverse Integer

--- JavaCoding/src/com/anilt/leetcode/ReverseInteger.java
+++ JavaCoding/src/com/anilt/leetcode/ReverseInteger.java
@@ -2,14 +2,27 @@ package com.anilt.leetcode;
 
 public class ReverseInteger {
     public static void main(String[] args) {
-        System.out.println(reverse(-12345600));
+        System.out.println(reverse(-0123456));
     }
     public static int reverse(int x) {
-        String xValue = ""+x;
+        boolean isNegative = false;
+        String xValue = "";
+        if(x < 0 ){
+            isNegative = true;
+            xValue = ""+x;
+            xValue = xValue.substring(1, xValue.length());
+        }
+        else {
+            xValue = ""+x;
+        }
+         String.valueOf(x);
+
         String reverseStr = "";
         for(int i=xValue.length()-1; i>=0; i--){
             reverseStr += xValue.charAt(i);
         }
+        if (isNegative)
+            reverseStr = "-"+reverseStr;
         return Integer.parseInt(reverseStr);
     }
 }
