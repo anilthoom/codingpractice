20e24595fcbd0f8d202040857af85ffec5a40ab5
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-02-10 13:01:11 +0530

    Two sum

--- JavaCoding/src/com/anilt/leetcode/TwoSum.java
+++ JavaCoding/src/com/anilt/leetcode/TwoSum.java
@@ -3,9 +3,14 @@ package com.anilt.leetcode;
 
 public class TwoSum {
     public static void main(String[] args) {
-
+        int nums[] = {2,7,11,15};
+        int target = 9;
+        int result[] = twoSum(nums, target);
     }
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
+        for(int i=0; i<nums.length; i++){
+            System.out.println(nums[i]+nums[i+1]);
+        }
         return null;
     }
 }
