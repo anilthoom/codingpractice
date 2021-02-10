d8ee1e2cc56a2ef78af3eae60e992f667d9a0aa0
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-02-10 15:17:07 +0530

    Two sum solution

--- JavaCoding/src/com/anilt/leetcode/TwoSum.java
+++ JavaCoding/src/com/anilt/leetcode/TwoSum.java
@@ -3,13 +3,21 @@ package com.anilt.leetcode;
 
 public class TwoSum {
     public static void main(String[] args) {
-        int nums[] = {2,7,11,15};
+        int nums[] = {2,11,7, 15};
         int target = 9;
         int result[] = twoSum(nums, target);
+        System.out.println(result[0]+","+ result[1]);
     }
     public static int[] twoSum(int[] nums, int target) {
+        int res[] = new int[2];
         for(int i=0; i<nums.length; i++){
-            System.out.println(nums[i]+nums[i+1]);
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i]+nums[j] == target){
+                    res[0] = i;
+                    res[1] = j;
+                    return res;
+                }
+            }
         }
         return null;
     }
