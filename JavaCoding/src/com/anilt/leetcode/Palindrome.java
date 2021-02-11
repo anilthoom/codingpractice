208152dd09541d1aaa16198cf91cee08943d9c2d
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-02-11 14:20:30 +0530

    Palindrome

--- JavaCoding/src/com/anilt/leetcode/Palindrome.java
+++ JavaCoding/src/com/anilt/leetcode/Palindrome.java
@@ -2,9 +2,9 @@ package com.anilt.leetcode;
 
 public class Palindrome {
     public static void main(String[] args) {
-        
+        System.out.println(isPalindrome(121));
     }
-    public boolean isPalindrome(int x) {
+    public static boolean isPalindrome(int x) {
         return false;
     }
 }
