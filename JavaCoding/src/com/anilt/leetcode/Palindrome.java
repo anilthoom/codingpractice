572b76a2197d5d61ef7940d2faccbbe8f98deb7e
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-02-11 14:53:17 +0530

    Palindrome

--- JavaCoding/src/com/anilt/leetcode/Palindrome.java
+++ JavaCoding/src/com/anilt/leetcode/Palindrome.java
@@ -2,17 +2,17 @@ package com.anilt.leetcode;
 
 public class Palindrome {
     public static void main(String[] args) {
-        System.out.println(isPalindrome(123));
+        System.out.println(isPalindrome(12321));
     }
     public static boolean isPalindrome(int x) {
         if(x<0)
             return false;
-        StringBuffer sb = new StringBuffer(x);
+        StringBuffer sb = new StringBuffer(""+x);
         sb.reverse();
         System.out.println(sb.toString());
-       /* int reversedInt = Integer.parseInt(reverStr);
+        int reversedInt = Integer.parseInt(sb.toString());
         if (x == reversedInt)
-            return true;*/
+            return true;
         return false;
     }
 }
