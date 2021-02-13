1c439a936741d46fadfe7f018a95192d93a2a63f
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-02-13 17:38:46 +0530

    Shuffle String

--- JavaCoding/src/com/anilt/leetcode/ShuffleString.java
+++ JavaCoding/src/com/anilt/leetcode/ShuffleString.java
@@ -3,12 +3,15 @@ package com.anilt.leetcode;
 
 public class ShuffleString {
     public static void main(String[] args) {
-        int ar[] = {5, 6, 7, 8, 0, 1, 2, 3, 4};
-        System.out.println(restoreString("KumarAnil", ar));
+        int ar[] = {4, 5, 6, 7, 0, 2, 1, 3};
+        System.out.println(restoreString("CodeLeet", ar));
     }
     public static String restoreString(String s, int[] indices) {
         String result = "";
+        char resultCharArr[] = new char[indices.length];
         for (int i = 0; i< indices.length; i++){
+            System.out.println(indices[i]);
+
             result += s.charAt(indices[i]);
         }
         return result;
