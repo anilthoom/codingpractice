05f2b3e70aed21593f1f3efe379b752f987a4181
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-02-16 12:22:07 +0530

    Longest Common Prefix

--- JavaCoding/src/com/anilt/leetcode/LongestCommonPrefix.java
+++ JavaCoding/src/com/anilt/leetcode/LongestCommonPrefix.java
@@ -3,7 +3,7 @@ package com.anilt.leetcode;
 
 public class LongestCommonPrefix {
     public static void main(String[] args) {
-        
+        LongestCommonPrefix lcp = new LongestCommonPrefix();
     }
     public String longestCommonPrefix(String[] strs) {
         return "";
