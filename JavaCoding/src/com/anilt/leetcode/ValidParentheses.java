9e1cf69d7fd51deff222064bebf327bc71dd2676
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-02 13:59:47 +0530

    Valid parentheses

--- JavaCoding/src/com/anilt/leetcode/ValidParentheses.java
+++ JavaCoding/src/com/anilt/leetcode/ValidParentheses.java
@@ -4,6 +4,7 @@ public class ValidParentheses {
     public static void main(String[] args) {
         ValidParentheses vp = new ValidParentheses();
         String s = "()[]{}";
+        System.out.println(vp.isValid(s));
     }
     public boolean isValid(String s) {
         return false;
