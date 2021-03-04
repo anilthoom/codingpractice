049efa688ff645ead728daecd2556eb8858560b1
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-04 12:47:36 +0530

    Roman number to Integer

--- JavaCoding/src/com/anilt/leetcode/RomanToInteger.java
+++ JavaCoding/src/com/anilt/leetcode/RomanToInteger.java
@@ -20,7 +20,7 @@ import java.util.HashMap;
 public class RomanToInteger {
     public static void main(String[] args) {
         RomanToInteger rti = new RomanToInteger();
-        System.out.println(rti.romanToInt("I"));
+        System.out.println(rti.romanToInt("VI"));
     }
     public int romanToInt(String s) {
         HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
