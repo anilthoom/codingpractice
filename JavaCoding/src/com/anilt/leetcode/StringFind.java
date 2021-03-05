75c52041c011c8a24a28905e64d59dacdc2b9307
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-05 13:15:44 +0530

    String find.

--- JavaCoding/src/com/anilt/leetcode/StringFind.java
+++ JavaCoding/src/com/anilt/leetcode/StringFind.java
@@ -4,4 +4,7 @@ public class StringFind {
     public static void main(String[] args) {
         StringFind sf = new StringFind();
     }
+    public int strStr(String haystack, String needle) {
+
+    }
 }
