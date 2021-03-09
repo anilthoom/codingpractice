e74585cdffba5ab27acc9d6300387258e80965ce
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-09 15:45:58 +0530

    Kids with candies

--- JavaCoding/src/com/anilt/leetcode/KidsWithCandies.java
+++ JavaCoding/src/com/anilt/leetcode/KidsWithCandies.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class KidsWithCandies {
     public static void main(String[] args) {
-
+        KidsWithCandies kidsWithCandies = new KidsWithCandies();
     }
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         return null;
