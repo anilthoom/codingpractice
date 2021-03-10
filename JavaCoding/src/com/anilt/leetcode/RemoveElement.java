9afa88db4c898f86b65fb4ab2fedecb7ee6a7b3d
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-10 15:43:34 +0530

    Remove element

--- JavaCoding/src/com/anilt/leetcode/RemoveElement.java
+++ JavaCoding/src/com/anilt/leetcode/RemoveElement.java
@@ -3,6 +3,7 @@ package com.anilt.leetcode;
 public class RemoveElement {
     public static void main(String[] args) {
         RemoveElement removeElement = new RemoveElement();
+        int nums[] = {3, 2, 1, 2, 3, 2};
     }
     public int removeElement(int[] nums, int val) {
         return 0;
