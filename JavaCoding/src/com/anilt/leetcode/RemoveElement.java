3c95ed2abb2964a5d669ae843f65bc0c5128963e
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-11 19:43:22 +0530

    We Commit a Message

--- JavaCoding/src/com/anilt/leetcode/RemoveElement.java
+++ JavaCoding/src/com/anilt/leetcode/RemoveElement.java
@@ -10,7 +10,7 @@ public class RemoveElement {
         int[] resultNums = new int[nums.length];
         for (int i=0; i<nums.length; i++){
             if(nums[i] != val){
-                resultNums[i] = val;
+                resultNums[i] = nums[i];
             }
         }
         System.out.println(resultNums);
