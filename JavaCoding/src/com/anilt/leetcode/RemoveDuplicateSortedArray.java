1e65aac4134b071157f0dcd2321c4ed163659593
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-12 12:17:14 +0530

    Remove duplicate from sorted array

--- JavaCoding/src/com/anilt/leetcode/RemoveDuplicateSortedArray.java
+++ JavaCoding/src/com/anilt/leetcode/RemoveDuplicateSortedArray.java
@@ -2,7 +2,7 @@ package com.anilt.leetcode;
 
 public class RemoveDuplicateSortedArray {
     public static void main(String[] args) {
-        
+        RemoveDuplicateSortedArray rDSA = new RemoveDuplicateSortedArray();
     }
     public int removeDuplicates(int[] nums) {
         return 0;
