b4a5e6a701b2f68c61e93dbf98da974559afdd62
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-12 14:24:03 +0530

    Square Root Implementation

--- JavaCoding/src/com/anilt/leetcode/SquareRoot.java
+++ JavaCoding/src/com/anilt/leetcode/SquareRoot.java
@@ -3,10 +3,11 @@ package com.anilt.leetcode;
 public class SquareRoot {
     public static void main(String[] args) {
         SquareRoot sR = new SquareRoot();
-        System.out.println(sR.mySqrt(4));
+        System.out.println(sR.mySqrt(8));
     }
     public int mySqrt(int x) {
         double resultD = Math.sqrt(x);
-        return Integer.parseInt(""+resultD);
+        int result = (int)Math.floor(resultD);
+        return result;
     }
 }
