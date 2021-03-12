70761c10df2b03b1afc50df032891bdd1bbfaaf8
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-12 14:00:50 +0530

    Search Insert Index

--- JavaCoding/src/com/anilt/leetcode/SearchIInsertIndex.java
+++ JavaCoding/src/com/anilt/leetcode/SearchIInsertIndex.java
@@ -3,6 +3,7 @@ package com.anilt.leetcode;
 public class SearchIInsertIndex {
     public static void main(String[] args) {
         SearchIInsertIndex sII = new SearchIInsertIndex();
+        System.out.println();
     }
     public int searchInsert(int[] nums, int target) {
         return 0;
