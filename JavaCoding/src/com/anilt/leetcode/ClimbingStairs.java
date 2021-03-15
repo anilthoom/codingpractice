7c6769bfc0cf5f595c4b49ce38ab915464c3ceff
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-15 13:35:40 +0530

    Climbing Stairs Solution

--- JavaCoding/src/com/anilt/leetcode/ClimbingStairs.java
+++ JavaCoding/src/com/anilt/leetcode/ClimbingStairs.java
@@ -2,7 +2,7 @@ package com.anilt.leetcode;
 
 public class ClimbingStairs {
     public static void main(String[] args) {
-
+        ClimbingStairs climbingStairs = new ClimbingStairs();
     }
     public int climbStairs(int n) {
         return 0;
