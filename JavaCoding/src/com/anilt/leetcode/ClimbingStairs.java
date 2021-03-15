88475f16bc0b2d44304395b9c709a3504ac7a46c
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-15 14:38:31 +0530

    Climbing Stairs

--- JavaCoding/src/com/anilt/leetcode/ClimbingStairs.java
+++ JavaCoding/src/com/anilt/leetcode/ClimbingStairs.java
@@ -3,11 +3,20 @@ package com.anilt.leetcode;
 public class ClimbingStairs {
     public static void main(String[] args) {
         ClimbingStairs climbingStairs = new ClimbingStairs();
-        System.out.println(climbingStairs.climbStairs(10));
+        System.out.println(climbingStairs.climbStairs(5));
     }
     public int climbStairs(int n) {
         //2 3 4 5 6  7
+        //0 1 1 2 3 5 8 13 21 34 55 89 144 233 377
         //2 3 5 8 12 17
-        return 0;
+        int x=0;
+        int y=1;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans=x+y;
+            x=y;
+            y=ans;
+        }
+        return ans;
     }
 }
