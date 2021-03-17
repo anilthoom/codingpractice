9a43a366f852955b7ba964f97d10de8f25dd44d5
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-17 15:29:20 +0530

    Web socket demo for chat application

--- SpringBoot/WebSocketDemo/src/main/java/com/anilt/websocket/controller/WebSocketEventListener.java
+++ SpringBoot/WebSocketDemo/src/main/java/com/anilt/websocket/controller/WebSocketEventListener.java
@@ -1,8 +1,17 @@
 package com.anilt.websocket.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WebSocketEventListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+
+    @Autowired
+    private SimpMessageSendingOperations messageSendingOperations;
 
 }
