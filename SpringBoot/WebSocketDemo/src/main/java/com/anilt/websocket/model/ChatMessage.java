c8cf6fb670f26434481f61e60fc7e983bc2305b9
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-17 15:13:55 +0530

    Web socket demo for chat application

--- SpringBoot/WebSocketDemo/src/main/java/com/anilt/websocket/model/ChatMessage.java
+++ SpringBoot/WebSocketDemo/src/main/java/com/anilt/websocket/model/ChatMessage.java
@@ -1,4 +1,13 @@
 package com.anilt.websocket.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.messaging.simp.SimpMessageType;
+
+@Getter
+@Setter
 public class ChatMessage {
+    private SimpMessageType type;
+    private String content;
+    private String sender;
 }
