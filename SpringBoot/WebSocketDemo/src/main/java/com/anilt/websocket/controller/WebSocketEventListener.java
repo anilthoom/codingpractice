947d9fac59aa56f01e5f76e53834f5c9e5b517e6
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-17 15:32:12 +0530

    Web socket demo for chat application

--- SpringBoot/WebSocketDemo/src/main/java/com/anilt/websocket/controller/WebSocketEventListener.java
+++ SpringBoot/WebSocketDemo/src/main/java/com/anilt/websocket/controller/WebSocketEventListener.java
@@ -3,8 +3,10 @@ package com.anilt.websocket.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
 
 @Component
 public class WebSocketEventListener {
@@ -13,5 +15,10 @@ public class WebSocketEventListener {
 
     @Autowired
     private SimpMessageSendingOperations messageSendingOperations;
+
+    @EventListener
+    public void handleWebSocketConnectionListener(SessionConnectedEvent event){
+        logger.info("Received a new web socket connection");
+    }
 
 }
