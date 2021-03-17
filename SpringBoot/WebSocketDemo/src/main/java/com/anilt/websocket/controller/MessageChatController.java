832c7fc930a46d16c4145a9bf0cc2e0b85cc7a1b
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-17 15:19:12 +0530

    Web socket demo for chat application

--- SpringBoot/WebSocketDemo/src/main/java/com/anilt/websocket/controller/MessageChatController.java
+++ SpringBoot/WebSocketDemo/src/main/java/com/anilt/websocket/controller/MessageChatController.java
@@ -1,4 +1,19 @@
 package com.anilt.websocket.controller;
 
+import com.anilt.websocket.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MessageChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage){
+        return chatMessage;
+    }
+
+
 }
