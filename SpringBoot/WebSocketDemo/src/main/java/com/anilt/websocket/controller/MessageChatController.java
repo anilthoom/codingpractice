5927808eb4d47740ad442ae0e685f9e3dc301c29
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-17 15:25:30 +0530

    Web socket demo for chat application

--- SpringBoot/WebSocketDemo/src/main/java/com/anilt/websocket/controller/MessageChatController.java
+++ SpringBoot/WebSocketDemo/src/main/java/com/anilt/websocket/controller/MessageChatController.java
@@ -16,6 +16,8 @@ public class MessageChatController {
         return chatMessage;
     }
 
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
     public ChatMessage addUser(@Payload ChatMessage chatMessage,
                                SimpMessageHeaderAccessor headerAccessor){
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
