dc1f07db8e235559e0ce42f455cc08cee1d4143d
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-17 15:05:57 +0530

    Web socket demo for chat application

--- SpringBoot/WebSocketDemo/src/main/java/com/anilt/websocket/config/WebSocketConfig.java
+++ SpringBoot/WebSocketDemo/src/main/java/com/anilt/websocket/config/WebSocketConfig.java
@@ -1,4 +1,16 @@
 package com.anilt.websocket.config;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+
+    }
 }
