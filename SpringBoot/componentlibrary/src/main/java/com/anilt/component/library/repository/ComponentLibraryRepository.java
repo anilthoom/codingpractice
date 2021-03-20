3b73759bb8b83a224c886bebecfef41c1fae1e33
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-20 18:23:43 +0530

    Moved to different location

--- SpringBoot/componentlibrary/src/main/java/com/anilt/component/library/repository/ComponentLibraryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anilt.component.library.repository;
-
-import com.anilt.component.library.model.ComponentLibrary;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ComponentLibraryRepository extends JpaRepository<ComponentLibrary, Long> {
-}
