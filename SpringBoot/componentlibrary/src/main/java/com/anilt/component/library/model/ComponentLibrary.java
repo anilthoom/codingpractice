213077eb5cd5a3d3a4e1f2f7d6ea0d3a76c64051
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-20 15:21:45 +0530

    Component Library Model

---
+++ SpringBoot/componentlibrary/src/main/java/com/anilt/component/library/model/ComponentLibrary.java
@@ -0,0 +1,9 @@
+package com.anilt.component.library.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "components_library")
+public class ComponentLibrary {
+}
