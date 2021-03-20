2d8561610a6e7f87fcf7c8910a5ef1d4e4bae16e
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-20 15:48:59 +0530

    Component Library Controller

--- SpringBoot/componentlibrary/src/main/java/com/anilt/component/library/controller/ComponentLibController.java
+++ SpringBoot/componentlibrary/src/main/java/com/anilt/component/library/controller/ComponentLibController.java
@@ -3,10 +3,8 @@ package com.anilt.component.library.controller;
 import com.anilt.component.library.model.ComponentLibrary;
 import com.anilt.component.library.repository.ComponentLibraryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +19,9 @@ public class ComponentLibController {
     @GetMapping("/components")
     public List<ComponentLibrary> getAllComponentLibraries(){
         return componentLibraryRepository.findAll();
+    }
+
+    public ResponseEntity<ComponentLibrary> getComponentLibraryById(@PathVariable(value = "id") Long componentId){
+        return null;
     }
 }
