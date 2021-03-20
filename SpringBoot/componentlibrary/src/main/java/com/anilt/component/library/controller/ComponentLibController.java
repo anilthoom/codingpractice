180912a45f4a8cd8df5cab91607dde3da0f0b4bc
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-20 15:40:13 +0530

    Component Library Controller

--- SpringBoot/componentlibrary/src/main/java/com/anilt/component/library/controller/ComponentLibController.java
+++ SpringBoot/componentlibrary/src/main/java/com/anilt/component/library/controller/ComponentLibController.java
@@ -1,5 +1,7 @@
 package com.anilt.component.library.controller;
 
+import com.anilt.component.library.repository.ComponentLibraryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/v1")
 public class ComponentLibController {
+
+    @Autowired
+    private ComponentLibraryRepository componentLibraryRepository;
 }
