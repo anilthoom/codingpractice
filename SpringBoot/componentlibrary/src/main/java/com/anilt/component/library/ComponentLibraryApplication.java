335a2827585c4e336ec96b7b495065a690d96a1d
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-20 16:25:26 +0530

    Component Library Custom Exception handling

--- SpringBoot/componentlibrary/src/main/java/com/anilt/component/library/ComponentLibraryApplication.java
+++ SpringBoot/componentlibrary/src/main/java/com/anilt/component/library/ComponentLibraryApplication.java
@@ -9,5 +9,9 @@ public class ComponentLibraryApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ComponentLibraryApplication.class, args);
 	}
-
+	// http://localhost:8080/cl/api/v1/components GET all
+	// http://localhost:8080/cl/api/v1/components/{componentId} GET
+	// http://localhost:8080/cl/api/v1/components Create
+	// http://localhost:8080/cl/api/v1/components/{componentId} Update
+	// http://localhost:8080/cl/api/v1/components/{componentId} Delete
 }
