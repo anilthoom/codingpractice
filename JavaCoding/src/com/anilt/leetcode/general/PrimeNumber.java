2e24dd525f6f9d7b24d4e5b70cb1f977902d09dc
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-03-21 17:45:59 +0530

    Prime number

--- JavaCoding/src/com/anilt/leetcode/general/PrimeNumber.java
+++ JavaCoding/src/com/anilt/leetcode/general/PrimeNumber.java
@@ -3,5 +3,18 @@ package com.anilt.leetcode.general;
 public class PrimeNumber {
     public static void main(String[] args) {
         PrimeNumber pn = new PrimeNumber();
+        
+    }
+    static boolean isPrime(int n)
+    {
+        // Corner case
+        if (n <= 1)
+            return false;
+
+        // Check from 2 to n-1
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+        return true;
     }
 }
