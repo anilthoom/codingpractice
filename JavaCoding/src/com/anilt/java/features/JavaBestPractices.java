64b77b63d4539efe91d26223fe025adeabe04a54
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-05-04 13:00:23 +0530

    Java best practices

--- JavaCoding/src/com/anilt/java/features/JavaBestPractices.java
+++ JavaCoding/src/com/anilt/java/features/JavaBestPractices.java
@@ -2,4 +2,7 @@ package com.anilt.java.features;
 
 // Reference https://dev.to/deepsource/guidelines-for-java-code-reviews-3096
 public class JavaBestPractices {
+    public static void main(String[] args) {
+
+    }
 }
