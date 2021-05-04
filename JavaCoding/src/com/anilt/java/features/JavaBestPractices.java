a3af3c1b6dbd9f148c47af6a126dc56bbb2532c4
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-05-04 13:10:08 +0530

    Java best practices

--- JavaCoding/src/com/anilt/java/features/JavaBestPractices.java
+++ JavaCoding/src/com/anilt/java/features/JavaBestPractices.java
@@ -1,5 +1,9 @@
 package com.anilt.java.features;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 // Reference https://dev.to/deepsource/guidelines-for-java-code-reviews-3096
 public class JavaBestPractices {
     public static void main(String[] args) {
@@ -7,6 +11,15 @@ public class JavaBestPractices {
 
     }
     void lambdasUse(){
-        
+        /* Traditional imperative way */
+        List<Integer> oddNumbers = new ArrayList<Integer>();
+        for (Integer number: Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)){
+            if (number % 2 != 0){
+                oddNumbers.add(number);
+            }
+        }
+        for (Integer n : oddNumbers){
+            System.out.println(n);
+        }
     }
 }
