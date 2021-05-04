4f0f41f222c087a5b672b6f4c8374833df09b98e
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-05-04 14:38:35 +0530

    Java best practices Interface

--- JavaCoding/src/com/anilt/java/features/BestUsageOfInterface.java
+++ JavaCoding/src/com/anilt/java/features/BestUsageOfInterface.java
@@ -6,3 +6,17 @@ public class BestUsageOfInterface {
 interface Shape {
     Double perimeter();
 }
+
+class Rectangle implements Shape {
+    //data members and constructors
+    private Double length;
+    private Double breadth;
+    Rectangle(Double length, Double breadth){
+        this.length = length;
+        this.breadth = breadth;
+    }
+    @Override
+    public Double perimeter() {
+        return 2 * (length + breadth);
+    }
+}
