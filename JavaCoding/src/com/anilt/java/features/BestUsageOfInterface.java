04055afa9e8214f44fe4e34298a6eeea2505286a
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-05-07 12:36:49 +0530

    Best case

--- JavaCoding/src/com/anilt/java/features/BestUsageOfInterface.java
+++ JavaCoding/src/com/anilt/java/features/BestUsageOfInterface.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class BestUsageOfInterface {
     public static void main(String[] args) {
         BestUsageOfInterface bestUsageOfInterface = new BestUsageOfInterface();
+
     }
     public double totalPerimeter(List<Shape> shapes) {
         return shapes.stream()
