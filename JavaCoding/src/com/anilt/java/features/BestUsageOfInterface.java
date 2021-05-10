046a1b473eb47a592c21002c2201c75924552a9c
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-05-10 10:28:37 +0530

    Generic API

--- JavaCoding/src/com/anilt/java/features/BestUsageOfInterface.java
+++ JavaCoding/src/com/anilt/java/features/BestUsageOfInterface.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class BestUsageOfInterface {
     public static void main(String[] args) {
         BestUsageOfInterface bestUsageOfInterface = new BestUsageOfInterface();
+        Shape rec = new Rectangle(10.0,5.0);
 //        bestUsageOfInterface.totalPerimeter();
     }
     public double totalPerimeter(List<Shape> shapes) {
