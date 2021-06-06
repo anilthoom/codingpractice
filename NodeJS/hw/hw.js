a4e3da73ddccebdeb2169e5c1e82a4b3ccf3f778
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-06-06 12:52:52 +0530

    request params

--- NodeJS/hw/hw.js
+++ NodeJS/hw/hw.js
@@ -4,4 +4,8 @@ var app = express();
 var things = require('./things.js');
 app.use('/things', things);
 
+app.get('/:id', function(req, res){
+    res.send('The id is '+req.params.id);
+});
+
 app.listen(3000);
