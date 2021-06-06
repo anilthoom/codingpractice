abc25fe6c90b69f2f7794bda4d31df6d66e878d0
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-06-06 12:56:06 +0530

    middleware functions

--- NodeJS/hw/hw.js
+++ NodeJS/hw/hw.js
@@ -8,4 +8,8 @@ app.get('/:id', function(req, res){
     res.send('The id is '+req.params.id);
 });
 
+app.use(function(req, res, next){
+    console.log('New request recieved at '+ Date.now());
+    next();
+});
 app.listen(3000);
