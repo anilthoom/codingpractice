d7f197ff4521d9ed8667770999bc59a645b347b7
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-06-06 12:57:05 +0530

    Middleware function

--- NodeJS/hw/hw.js
+++ NodeJS/hw/hw.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var app = express();
-
 var things = require('./things.js');
+
+//Middleware function
+app.use(function(req, res, next){
+    console.log('New request recieved at '+ Date.now());
+    next();
+});
+
 app.use('/things', things);
 
 app.get('/:id', function(req, res){
     res.send('The id is '+req.params.id);
 });
 
-app.use(function(req, res, next){
-    console.log('New request recieved at '+ Date.now());
-    next();
-});
 app.listen(3000);
