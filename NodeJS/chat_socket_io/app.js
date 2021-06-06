928ac6fc21a007048dd7380dabb9b536fefd0150
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-06-06 16:03:39 +0530

    Chat application

---
+++ NodeJS/chat_socket_io/app.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.sendFile('index.html', {root: __dirname});
+});
+
+users = [];
+io.on('connection', function(socket){
+    console.log('User connected!');
+    socket.on('setUserName', function(data){
+        if(users.indexOf(data) > -1){
+            users.push(data);
+            socket.emit('userSet', {username: data});
+        }
+        else{
+            socket.emit('userExists', data + ' username is taken! Try some other name.');
+        }
+    })
+});
+
+http.listen(3000, function(){
+    console.log('Listening on 3000');
+});
