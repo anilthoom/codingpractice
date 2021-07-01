785ead2c3251448fcd48e80b1e8026d52daefa59
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-01 15:50:03 +0530

    new routes

--- VueJS/vue-framework-intro/src/main.js
+++ VueJS/vue-framework-intro/src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import User from './components/User.vue'
+import Blog from './components/Blog.vue'
+import Account from './components/Account.vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter);
+
+const routes = [
+  { path: '/blog', component: Blog},
+  { path: '/account', component: Account},
+  { path: '/', component: User}
+]
 
 //Global use
 Vue.component('app-user', User);
