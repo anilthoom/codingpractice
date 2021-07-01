c8ff79044a7751e842af0e607787490134a0f49a
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-01 15:09:11 +0530

    Local component usage

--- VueJS/vue-framework-intro/src/components/User.vue
+++ VueJS/vue-framework-intro/src/components/User.vue
@@ -2,10 +2,12 @@
     <div id="user">
         <h3>The User</h3>
         <p>My username is {{username}}</p>
+        <app-info></app-info>
     </div>
 </template>
 
 <script>
+import Info from './Info.vue';
 export default {
     data(){
         return{
@@ -13,7 +15,7 @@ export default {
         }
     },
     components: {
-        
+        'app-info': Info
     }
 }
 </script>
