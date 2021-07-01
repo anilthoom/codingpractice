7418c77b1070eacbabac592d52cdb935b79d9c9a
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-01 12:48:28 +0530

    User vue

--- VueJS/vue-framework-intro/src/components/User.vue
+++ VueJS/vue-framework-intro/src/components/User.vue
@@ -1,15 +1,22 @@
 <template>
     <div id="user">
         <h3>The User</h3>
+        <p>My username is {{username}}</p>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data(){
+        return{
+            username: 'Anil'
+        }
+    }
 }
 </script>
 
 <style>
-
+    p {
+        font-style: italic;
+    }
 </style>
