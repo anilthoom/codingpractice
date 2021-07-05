862b1d210e181a28f4187af3b1d33a3c0638b229
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-05 14:56:23 +0530

    Info component change

--- ReactJS/react-framework-intro/src/App.js
+++ ReactJS/react-framework-intro/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Info from './components/Info';
+
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
     <Router>
-      
+
     </Router>
   );
 }
--- ReactJS/react-framework-intro/src/components/User.js
+++ ReactJS/react-framework-intro/src/components/User.js
@@ -1,8 +1,11 @@
+import Info from "./Info";
+
 export default function(props){
     return(
         <div>
             <h1>The User!</h1>
             <p>This is User details page</p>
+            <Info />
         </div>
     );
 }
