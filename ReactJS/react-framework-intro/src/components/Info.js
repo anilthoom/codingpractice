b2b9b32afe2113db2ab22395d3eff6616032ab77
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-05 14:16:27 +0530

    Info component

---
+++ ReactJS/react-framework-intro/src/components/Info.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Info extends Component{
+    render(){
+        return(
+            <div>
+                <h1>The Info</h1>
+                <p>This is just for your information!</p>
+            </div>
+        );
+    }
+}
+
+export default Info;
