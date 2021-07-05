b13a0f60332acf42ce55eae5ff4e3f055b5eaba3
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-05 14:58:26 +0530

    Component import

--- ReactJS/react-framework-intro/src/App.js
+++ ReactJS/react-framework-intro/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
-
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import User from './components/User';
+import Account from './components/Account';
+import Blog from './components/Blog';
 
 function App() {
   return (
