4ab95bca4aafeaa7ddda0e36f1171cfce236b74c
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-08 20:24:11 +0530

    properties

--- Angular/angular-framework-intro/src/app/app.component.ts
+++ Angular/angular-framework-intro/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Anil Thoom';
+  firstname = 'Anil';
+  lastname = 'Thoom';
 }
