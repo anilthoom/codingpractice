05100f9aa9d5703f27ecf452651a5853111cebe9
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-09 14:41:48 +0530

    passing data to component

--- Angular/angular-framework-intro/src/app/username/username.component.ts
+++ Angular/angular-framework-intro/src/app/username/username.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-username',
@@ -11,5 +11,7 @@ export class UsernameComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @Input() username: string;
 
 }
