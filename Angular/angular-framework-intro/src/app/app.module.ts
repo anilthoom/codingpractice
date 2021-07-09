70950a0de457826dfc59cc924d1261e040434786
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-09 14:57:35 +0530

    Two way binding

--- Angular/angular-framework-intro/src/app/app.module.ts
+++ Angular/angular-framework-intro/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UsernameComponent } from './username/username.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { UsernameComponent } from './username/username.component';
     UsernameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
