0800c2addf68dce4bb281cb4060c41d352327ee4
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-19 17:11:06 +0530

    New project

---
+++ SpringBootFeatures/Springboot-Maven/src/test/java/com/anil/example/demo/SpringbootMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anil.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
