4ff83ed465a8e2a5479a0ef36581458117136245
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-19 17:15:01 +0530

    Bug fix

--- SpringBootFeatures/Springboot-Maven/src/main/java/com/anil/example/demo/SpringbootMavenApplication.java
+++ SpringBootFeatures/Springboot-Maven/src/main/java/com/anil/example/demo/SpringbootMavenApplication.java
@@ -2,12 +2,19 @@ package com.anil.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootMavenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMavenApplication.class, args);
 	}
 
+	@RequestMapping(value = "/")
+	public String hello() {
+		return "Hello Anil1";
+	}
 }
