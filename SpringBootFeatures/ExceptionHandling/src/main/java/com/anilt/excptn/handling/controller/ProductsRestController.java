574858048b2cdb2cecc85e48c887fcd0c05c65e8
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-20 17:24:59 +0530

    New APIs

---
+++ SpringBootFeatures/ExceptionHandling/src/main/java/com/anilt/excptn/handling/controller/ProductsRestController.java
@@ -0,0 +1,55 @@
+package com.anilt.excptn.handling.controller;
+
+import com.anilt.excptn.handling.model.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ProductsRestController {
+    public static Map<String, Product> productRepo = new HashMap<>();
+    static {
+        Product lens = new Product();
+        lens.setId("1");
+        lens.setName("Canon");
+
+        Product tripod = new Product();
+        tripod.setId("2");
+        tripod.setName("Manfrotto");
+
+        productRepo.put(lens.getId(), lens);
+        productRepo.put(tripod.getId(), tripod);
+    }
+
+    @RequestMapping(value = "/products")
+    public ResponseEntity<Object> getAllProducts(){
+        return new ResponseEntity<>(productRepo.values(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/products/{id}")
+    public ResponseEntity<Object> getProductById(@PathVariable("id") String id){
+        return new ResponseEntity<>(productRepo.get(id), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/products", method = RequestMethod.POST)
+    public ResponseEntity<Object> createProduct(@RequestBody Product product){
+        productRepo.put(product.getId(), product);
+        return new ResponseEntity<>("Product created successfully", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product){
+        productRepo.remove(id);
+        productRepo.put(product.getId(), product);
+        return new ResponseEntity<>("Product updated successfully", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> deleteProduct(@PathVariable("id") String id){
+        productRepo.remove(id);
+        return new ResponseEntity<>("Product deleted successfully", HttpStatus.OK);
+    }
+}
