f7cbbf52fdf391c968f7e99114ba475e9b6f5c23
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-20 13:58:44 +0530

    Employee rest apis

--- SpringBootFeatures/Springboot-Maven/src/main/java/com/anil/example/demo/controller/EmployeeController.java
+++ SpringBootFeatures/Springboot-Maven/src/main/java/com/anil/example/demo/controller/EmployeeController.java
@@ -1,4 +1,14 @@
 package com.anil.example.demo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
+
+    @RequestMapping(value = "/emp-name")
+    public String getEmpName(){
+        return "My name is anil";
+    }
+
 }
