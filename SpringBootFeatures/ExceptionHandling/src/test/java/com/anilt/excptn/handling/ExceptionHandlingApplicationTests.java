d5c9dc07b22bf6958bc5771f638d680d6e0f7d3d
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-20 17:02:58 +0530

    Exception handling practice

---
+++ SpringBootFeatures/ExceptionHandling/src/test/java/com/anilt/excptn/handling/ExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anilt.excptn.handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
