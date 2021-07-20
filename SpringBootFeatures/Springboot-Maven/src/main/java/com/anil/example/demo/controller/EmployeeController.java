236f3e4ec949577947e2de60ab7a0bc376331f4f
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-20 14:16:34 +0530

    Path

--- SpringBootFeatures/Springboot-Maven/src/main/java/com/anil/example/demo/controller/EmployeeController.java
+++ SpringBootFeatures/Springboot-Maven/src/main/java/com/anil/example/demo/controller/EmployeeController.java
@@ -1,14 +1,21 @@
 package com.anil.example.demo.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
-    @RequestMapping(value = "/emp-name")
-    public String getEmpName(){
-        return "My name is Anil";
+    @RequestMapping(value = "/emp-name/{id}")
+    public String getEmpName(@PathVariable("id") String id){
+        String prefix = "Employee name is ";
+        if(id == "1")
+            return prefix+"Anil";
+        else if(id == "2")
+            return prefix+"Shrihan";
+        else
+            return prefix+"Thoom";
     }
 
 }
