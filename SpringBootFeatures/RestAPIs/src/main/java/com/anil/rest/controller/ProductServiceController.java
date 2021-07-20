ab57d4d541ea0ab427d2bb76cecdf0ea5307a8cf
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-20 14:52:20 +0530

    REST APIs

--- SpringBootFeatures/RestAPIs/src/main/java/com/anil/rest/controller/ProductServiceController.java
+++ SpringBootFeatures/RestAPIs/src/main/java/com/anil/rest/controller/ProductServiceController.java
@@ -1,11 +1,22 @@
 package com.anil.rest.controller;
 
+import com.anil.rest.model.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class ProductServiceController {
     private static final Logger logger = LoggerFactory.getLogger(ProductServiceController.class);
+    private static Map<String, Product> productRepo = new HashMap<>();
+    static {
+        Product lens = new Product();
+        lens.setId("1");
+        lens.setName("Canon");
+        productRepo.put(lens.getId(), lens);
+    }
 
 }
