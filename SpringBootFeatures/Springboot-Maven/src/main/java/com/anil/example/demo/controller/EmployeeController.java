cedda57246d3304250002ba74d377eaa3a617b55
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-20 14:18:46 +0530

    Logger

--- SpringBootFeatures/Springboot-Maven/src/main/java/com/anil/example/demo/controller/EmployeeController.java
+++ SpringBootFeatures/Springboot-Maven/src/main/java/com/anil/example/demo/controller/EmployeeController.java
@@ -1,14 +1,18 @@
 package com.anil.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
+    private static final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
     @RequestMapping(value = "/emp-name/{id}")
     public String getEmpName(@PathVariable("id") String id){
+        logger.info("Employee request with id : "+id);
         String prefix = "Employee name is ";
         if(id == "1")
             return prefix+"Anil";
