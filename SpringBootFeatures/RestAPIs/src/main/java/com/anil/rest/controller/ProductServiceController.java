2c271d32d9e254fc61aceb6cff8be350f88659f0
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-20 14:36:08 +0530

    REST endpoints

--- SpringBootFeatures/RestAPIs/src/main/java/com/anil/rest/controller/ProductServiceController.java
+++ SpringBootFeatures/RestAPIs/src/main/java/com/anil/rest/controller/ProductServiceController.java
@@ -1,7 +1,11 @@
 package com.anil.rest.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductServiceController {
+    private static final Logger logger = LoggerFactory.getLogger(ProductServiceController.class);
+
 }
