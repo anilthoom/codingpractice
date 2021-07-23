4e85e9275bc5a0d05ff1b93e6359ee20be9913c2
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-23 18:38:42 +0530

    Product Interceptors

--- SpringBootFeatures/Interceptor/src/main/java/com/anilt/interceptor/interceptors/ProductServiceInterceptors.java
+++ SpringBootFeatures/Interceptor/src/main/java/com/anilt/interceptor/interceptors/ProductServiceInterceptors.java
@@ -11,12 +11,16 @@ import javax.servlet.http.HttpServletResponse;
 public class ProductServiceInterceptors implements HandlerInterceptor {
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return false;
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws Exception {
+        return true;
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response,
+                           Object handler, ModelAndView modelAndView) throws Exception {
 
     }
 
