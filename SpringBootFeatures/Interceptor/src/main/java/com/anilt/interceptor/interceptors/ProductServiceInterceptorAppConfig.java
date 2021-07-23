5db4042ea47d4ff7a683f81e430376a20c880640
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-23 19:34:25 +0530

    Product Interceptors WebMvcConfigureAdapter

--- SpringBootFeatures/Interceptor/src/main/java/com/anilt/interceptor/interceptors/ProductServiceInterceptorAppConfig.java
+++ SpringBootFeatures/Interceptor/src/main/java/com/anilt/interceptor/interceptors/ProductServiceInterceptorAppConfig.java
@@ -2,10 +2,9 @@ package com.anilt.interceptor.interceptors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-public class ProductServiceInterceptorAppConfig implements WebMvcConfigurer {
+public class ProductServiceInterceptorAppConfig extends WebMvcConfigurerAdapter {
 
     @Autowired
     ProductServiceInterceptor productServiceInterceptor;
