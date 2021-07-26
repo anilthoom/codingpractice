e6dccca01575299e5e51eae25416295b6cf985ca
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-26 16:53:53 +0530

    Rest Templates

--- SpringBootFeatures/RestTemplate/src/main/java/com/anilt/rest/template/controller/ConsumeWebServiceController.java
+++ SpringBootFeatures/RestTemplate/src/main/java/com/anilt/rest/template/controller/ConsumeWebServiceController.java
@@ -1,8 +1,30 @@
 package com.anilt.rest.template.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @Controller
 public class ConsumeWebServiceController {
     public static String baseUrl = "http://localhost:8080/products/";
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping(value = "/template/products")
+    public String getProductList(){
+        System.out.println("Called......");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
+        return restTemplate.exchange("http://localhost:8080/products/",
+                HttpMethod.GET, entity, String.class).getBody();
+    }
 }
