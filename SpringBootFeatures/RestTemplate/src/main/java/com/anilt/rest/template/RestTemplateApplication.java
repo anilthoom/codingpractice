6287cabe658168b6f4901ed0ce50e58a1ab48dde
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-26 16:34:27 +0530

    Rest Templates

--- SpringBootFeatures/RestTemplate/src/main/java/com/anilt/rest/template/RestTemplateApplication.java
+++ SpringBootFeatures/RestTemplate/src/main/java/com/anilt/rest/template/RestTemplateApplication.java
@@ -2,6 +2,8 @@ package com.anilt.rest.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.http.HttpHeaders;
 
@@ -11,5 +13,10 @@ public class RestTemplateApplication {
 	public static String baseUrl = "http://localhost:8080/products/";
 	public static void main(String[] args) {
 		SpringApplication.run(RestTemplateApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
 	}
 }
