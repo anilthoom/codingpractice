a14efb71a423efdd0717efa8b99c64ccaa9091c2
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-26 15:18:01 +0530

    Servlet filters

--- SpringBootFeatures/ServletFilter/src/main/java/com/anilt/servlet/filter/SimpleFilter.java
+++ SpringBootFeatures/ServletFilter/src/main/java/com/anilt/servlet/filter/SimpleFilter.java
@@ -14,7 +14,9 @@ public class SimpleFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
+        System.out.println("Remote Host:"+servletRequest.getRemoteHost());
+        System.out.println("Remote Address: "+ servletRequest.getRemoteAddr());
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
