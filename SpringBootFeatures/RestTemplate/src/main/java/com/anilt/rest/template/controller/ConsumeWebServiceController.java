ce8b7c18d188309ab760ca6669f66ce35ac7fac2
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-26 18:25:07 +0530

    Rest Templates

--- SpringBootFeatures/RestTemplate/src/main/java/com/anilt/rest/template/controller/ConsumeWebServiceController.java
+++ SpringBootFeatures/RestTemplate/src/main/java/com/anilt/rest/template/controller/ConsumeWebServiceController.java
@@ -1,11 +1,14 @@
 package com.anilt.rest.template.controller;
 
+import com.anilt.rest.template.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -26,5 +29,13 @@ public class ConsumeWebServiceController {
         HttpEntity<String> entity = new HttpEntity<String>(headers);
         return restTemplate.exchange(baseUrl,
                 HttpMethod.GET, entity, String.class).getBody();
+    }
+
+    @RequestMapping(value = "/template/products", method = RequestMethod.POST)
+    public String createProduct(@RequestBody Product product){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<Product> entity = new HttpEntity<Product>(product, headers);
+        return restTemplate.exchange(baseUrl, HttpMethod.POST, entity, String.class).getBody();
     }
 }
