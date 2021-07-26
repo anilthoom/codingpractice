2f4b7d8e52bd203cd7bd97f6a776a2d2b49587ed
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-26 15:13:55 +0530

    Servlet filters

--- SpringBootFeatures/ServletFilter/src/main/java/com/anilt/servlet/filter/SimpleFilter.java
+++ SpringBootFeatures/ServletFilter/src/main/java/com/anilt/servlet/filter/SimpleFilter.java
@@ -1,4 +1,21 @@
 package com.anilt.servlet.filter;
 
-public class SimpleFilter {
+import javax.servlet.*;
+import java.io.IOException;
+
+public class SimpleFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
