fe8f52d1c94b60b8c9749fad5783100bf06911fc
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-28 18:37:51 +0530

    New problems

--- JavaCoding/src/com/anilt/problems/arrays/FrequentWordInArray.java
+++ JavaCoding/src/com/anilt/problems/arrays/FrequentWordInArray.java
@@ -2,4 +2,12 @@ package com.anilt.problems.arrays;
 
 // https://practice.geeksforgeeks.org/problems/most-frequent-word-in-an-array-of-strings3528/1
 public class FrequentWordInArray {
+    public static void main(String[] args) {
+
+    }
+    public String mostFrequentWord(String arr[],int n)
+    {
+        String result = "";
+        return result;
+    }
 }
