2e7514ae2579a1703379176885c98c44c41266b6
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-28 18:41:08 +0530

    New problems

--- JavaCoding/src/com/anilt/problems/arrays/FrequentWordInArray.java
+++ JavaCoding/src/com/anilt/problems/arrays/FrequentWordInArray.java
@@ -1,12 +1,18 @@
 package com.anilt.problems.arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // https://practice.geeksforgeeks.org/problems/most-frequent-word-in-an-array-of-strings3528/1
 public class FrequentWordInArray {
     public static void main(String[] args) {
-
+        String array[] = {"Anil", "Kumar", "Anil", "Shri"};
+        System.out.println(mostFrequentWord(array, 4));
     }
-    public String mostFrequentWord(String arr[],int n)
+    public static String mostFrequentWord(String arr[],int n)
     {
+        Map<String, Integer> wordsCountMap = new HashMap<>();
+
         String result = "";
         return result;
     }
