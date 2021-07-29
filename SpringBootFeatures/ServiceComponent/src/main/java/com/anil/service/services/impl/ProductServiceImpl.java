72e335e27c31403fa6c4ac8625b825bc83596857
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-29 15:14:30 +0530

    Service Implementation

--- SpringBootFeatures/ServiceComponent/src/main/java/com/anil/service/services/impl/ProductServiceImpl.java
+++ SpringBootFeatures/ServiceComponent/src/main/java/com/anil/service/services/impl/ProductServiceImpl.java
@@ -5,9 +5,23 @@ import com.anil.service.services.ProductService;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+    private static Map<String, Product> productRepo = new HashMap<>();
+    static {
+        Product honey = new Product();
+        honey.setId("1");
+        honey.setName("Canon");
+        productRepo.put(honey.getId(), honey);
+
+        Product almond = new Product();
+        almond.setId("2");
+        almond.setName("Nikon");
+        productRepo.put(almond.getId(), almond);
+    }
     @Override
     public void createProduct(Product product) {
 
