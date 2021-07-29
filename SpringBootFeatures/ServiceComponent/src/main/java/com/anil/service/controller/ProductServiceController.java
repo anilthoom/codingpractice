9e04f2fd281ae84e8957ae8bcaf764da0869246d
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-29 15:20:49 +0530

    Service Controller

--- SpringBootFeatures/ServiceComponent/src/main/java/com/anil/service/controller/ProductServiceController.java
+++ SpringBootFeatures/ServiceComponent/src/main/java/com/anil/service/controller/ProductServiceController.java
@@ -1,4 +1,12 @@
 package com.anil.service.controller;
 
+import com.anil.service.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductServiceController {
+
+    @Autowired
+    ProductService productService;
 }
