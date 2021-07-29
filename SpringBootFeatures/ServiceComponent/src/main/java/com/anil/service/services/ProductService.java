2304dacc439557f945bac8ad37cd303631ce3c76
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-29 15:09:14 +0530

    Model class

--- SpringBootFeatures/ServiceComponent/src/main/java/com/anil/service/services/ProductService.java
+++ SpringBootFeatures/ServiceComponent/src/main/java/com/anil/service/services/ProductService.java
@@ -1,5 +1,8 @@
 package com.anil.service.services;
 
+import com.anil.service.model.Product;
+
 public interface ProductService {
-    public abstract void createProduct();
+    public abstract void createProduct(Product product);
+
 }
