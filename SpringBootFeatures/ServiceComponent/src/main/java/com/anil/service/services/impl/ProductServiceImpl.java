9c9e9b35d99f27857b29b8d286a39076b32a69d9
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-29 15:12:23 +0530

    Service Implementation

---
+++ SpringBootFeatures/ServiceComponent/src/main/java/com/anil/service/services/impl/ProductServiceImpl.java
@@ -0,0 +1,28 @@
+package com.anil.service.services.impl;
+
+import com.anil.service.model.Product;
+import com.anil.service.services.ProductService;
+
+import java.util.Collection;
+
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public void createProduct(Product product) {
+
+    }
+
+    @Override
+    public Collection<Product> getProducts() {
+        return null;
+    }
+
+    @Override
+    public void updateProduct(String id, Product product) {
+
+    }
+
+    @Override
+    public void deleteProduct(String id) {
+
+    }
+}
