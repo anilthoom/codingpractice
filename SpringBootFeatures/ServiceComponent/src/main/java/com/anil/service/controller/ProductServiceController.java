d2fdf09e976f8ab0803e2c1fa4e222633ee41290
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-29 15:22:37 +0530

    Service Controller

--- SpringBootFeatures/ServiceComponent/src/main/java/com/anil/service/controller/ProductServiceController.java
+++ SpringBootFeatures/ServiceComponent/src/main/java/com/anil/service/controller/ProductServiceController.java
@@ -2,6 +2,9 @@ package com.anil.service.controller;
 
 import com.anil.service.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +12,9 @@ public class ProductServiceController {
 
     @Autowired
     ProductService productService;
+
+    @RequestMapping(value = "/products")
+    public ResponseEntity<Object> getProducts(){
+        return new ResponseEntity<>(productService.getProducts(), HttpStatus.OK);
+    }
 }
