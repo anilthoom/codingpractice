76531faa60606b48fe04d27d3dd6dc40c1af157c
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-29 15:11:00 +0530

    Service Interface

--- SpringBootFeatures/ServiceComponent/src/main/java/com/anil/service/services/ProductService.java
+++ SpringBootFeatures/ServiceComponent/src/main/java/com/anil/service/services/ProductService.java
@@ -2,7 +2,11 @@ package com.anil.service.services;
 
 import com.anil.service.model.Product;
 
+import java.util.Collection;
+
 public interface ProductService {
     public abstract void createProduct(Product product);
-
+    public abstract Collection<Product> getProducts();
+    public abstract void updateProduct(String id, Product product);
+    public abstract void deleteProduct(String id);
 }
