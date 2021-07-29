3af3008cac419a59a407a48ef6cfca88496cabe5
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-29 15:03:23 +0530

    New service component project

---
+++ SpringBootFeatures/ServiceComponent/src/test/java/com/anil/service/ServiceComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anil.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceComponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
