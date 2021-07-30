10c661c3cfbd417188da64ab19fc3ab3935c73b2
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-30 17:30:48 +0530

    Enabling HTTPs

--- SpringBootFeatures/Https/src/main/java/com/anilt/https/HttpsApplication.java
+++ SpringBootFeatures/Https/src/main/java/com/anilt/https/HttpsApplication.java
@@ -2,12 +2,19 @@ package com.anilt.https;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HttpsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HttpsApplication.class, args);
 	}
 
+	@RequestMapping(value = "/welcome")
+	public String welcome(){
+		return "Hello welcome to secured site!!!";
+	}
 }
