399c8929aa8be3201ac5caa9c4d275b1f635081d
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-30 17:13:59 +0530

    Scheduler

---
+++ SpringBootFeatures/Scheduler/src/main/java/com/anilt/scheduler/schedules/Scheduler.java
@@ -0,0 +1,25 @@
+package com.anilt.scheduler.schedules;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class Scheduler {
+
+    @Scheduled(cron = "0 * 17 * * ?") //Run between 5PM to 5:59 PM
+    public void cronJobSch(){
+        System.out.println("Scheduled Job : "+ getDate());
+    }
+
+    @Scheduled(fixedRate = 10000)
+    public void fixedRateSch(){
+        System.out.println("Fixed rate scheduler : "+getDate());
+    }
+
+    private String getDate(){
+        return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date());
+    }
+}
