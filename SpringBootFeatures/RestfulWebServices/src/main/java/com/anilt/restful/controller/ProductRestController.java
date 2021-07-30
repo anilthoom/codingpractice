fba7de0fadc9605bba5e2547d3902c9508d3d0b1
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-07-30 11:39:47 +0530

    Update product API Impl

--- SpringBootFeatures/RestfulWebServices/src/main/java/com/anilt/restful/controller/ProductRestController.java
+++ SpringBootFeatures/RestfulWebServices/src/main/java/com/anilt/restful/controller/ProductRestController.java
@@ -3,10 +3,7 @@ package com.anilt.restful.controller;
 import com.anilt.restful.model.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -36,5 +33,12 @@ public class ProductRestController {
     public ResponseEntity<Object> createProduct(@RequestBody Product product){
         productRepo.put(product.getId(), product);
         return new ResponseEntity<>("Product created successfully", HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Object> updateProduct(@PathVariable("id") String id, @RequestBody Product product){
+        productRepo.remove(id);
+        productRepo.put(id, product);
+        return new ResponseEntity<>("Product updated successfully", HttpStatus.OK);
     }
 }
