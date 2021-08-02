3cb87f580c3adac8eaa9d3acae5edfbcc5116f0e
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-02 10:20:54 +0530

    Zuul Gateway- Proxy server and routing

---
+++ SpringBootFeatures/ZuulServerGateway/src/main/java/com/anilt/zuul/ZuulServerGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.anilt.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulServerGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServerGatewayApplication.class, args);
+	}
+
+}
