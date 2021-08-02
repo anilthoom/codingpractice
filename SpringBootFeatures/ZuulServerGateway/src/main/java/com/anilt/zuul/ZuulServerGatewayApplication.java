2905aadc778e739098fad3b050b60681dc61d38a
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-02 11:48:28 +0530

    Zuul Gateway- Proxy server and routing

--- SpringBootFeatures/ZuulServerGateway/src/main/java/com/anilt/zuul/ZuulServerGatewayApplication.java
+++ SpringBootFeatures/ZuulServerGateway/src/main/java/com/anilt/zuul/ZuulServerGatewayApplication.java
@@ -2,8 +2,10 @@ package com.anilt.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulServerGatewayApplication {
 
 	public static void main(String[] args) {
