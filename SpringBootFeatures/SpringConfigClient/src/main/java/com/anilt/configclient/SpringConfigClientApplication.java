ce97386cd4e27c2dda3adb1428fa7f562eb9751e
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-02 13:56:01 +0530

    Spring config client

--- SpringBootFeatures/SpringConfigClient/src/main/java/com/anilt/configclient/SpringConfigClientApplication.java
+++ SpringBootFeatures/SpringConfigClient/src/main/java/com/anilt/configclient/SpringConfigClientApplication.java
@@ -1,13 +1,26 @@
 package com.anilt.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RefreshScope
+@RestController
 public class SpringConfigClientApplication {
+
+	@Value("${welcome.message}")
+	String welcomeText;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringConfigClientApplication.class, args);
 	}
 
+	@RequestMapping(value = "/hi")
+	public String welcomeText(){
+		return "HELLO";
+	}
 }
