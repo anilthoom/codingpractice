025e478ebd3747ed25908ca51a88883bdbc26f9e
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-03 12:42:35 +0530

    Controller class

--- SpringBootFeatures/SpringBootCache/src/main/java/com/anilt/cache/controller/StudentServiceController.java
+++ SpringBootFeatures/SpringBootCache/src/main/java/com/anilt/cache/controller/StudentServiceController.java
@@ -1,4 +1,20 @@
 package com.anilt.cache.controller;
 
+import com.anilt.cache.model.Student;
+import com.anilt.cache.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StudentServiceController {
+    @Autowired
+    StudentService studentService;
+
+    @RequestMapping(value = "/student/{id}")
+    public Student findStudentById(@PathVariable String id){
+        System.out.println("Search by Id: "+ id);
+        return studentService.getStudentByID(id);
+    }
 }
