f718a72f6259175eec824c03d93a7e47b1fd2d52
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-03 09:53:29 +0530

    Flyway Database Project

---
+++ SpringBootFeatures/FlywayDatabase/src/main/java/com/anilt/flyway/FlywayDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.anilt.flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlywayDatabaseApplication.class, args);
+	}
+
+}
