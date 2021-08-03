1dccc272b077d7b8570a2bc6e9ef991b3f07e843
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-03 15:46:47 +0530

    Product Service Implementation class

---
+++ SpringBootFeatures/SpringBootCacheAdvanced/src/main/java/com/anilt/cache/service/impl/ProductServiceImpl.java
@@ -0,0 +1,33 @@
+package com.anilt.cache.service.impl;
+
+import com.anilt.cache.model.Product;
+import com.anilt.cache.service.ProductService;
+
+import java.util.Collection;
+
+public class ProductServiceImpl implements ProductService {
+    @Override
+    public Collection<Product> getAllProducts() {
+        return null;
+    }
+
+    @Override
+    public Product getProductById(String id) {
+        return null;
+    }
+
+    @Override
+    public void createProduct(Product product) {
+
+    }
+
+    @Override
+    public void updateProduct(String id, Product product) {
+
+    }
+
+    @Override
+    public void deleteProduct(String id) {
+
+    }
+}
