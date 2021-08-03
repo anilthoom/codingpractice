0d5f0b3e3e2344a59fc3de3f95d1d92594121ab6
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-03 12:33:40 +0530

    Pojo class

---
+++ SpringBootFeatures/SpringBootCache/src/main/java/com/anilt/cache/model/Student.java
@@ -0,0 +1,14 @@
+package com.anilt.cache.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Student {
+    private String id;
+    private String name;
+    private String clz;
+}
