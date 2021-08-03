8d1a3d8e7349ab78b8a370970f7e5ef223c2def7
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-03 12:45:42 +0530

    Implemented cache for the service

--- SpringBootFeatures/SpringBootCache/src/main/java/com/anilt/cache/service/StudentService.java
+++ SpringBootFeatures/SpringBootCache/src/main/java/com/anilt/cache/service/StudentService.java
@@ -1,10 +1,13 @@
 package com.anilt.cache.service;
 
 import com.anilt.cache.model.Student;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+
+    @Cacheable("student")
     public Student getStudentByID(String id){
         try {
             System.out.println("In sleep for 5 seconds, to simulate the backend call.");
