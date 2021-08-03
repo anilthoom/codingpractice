43ef8d35d8b3eb8e90b5376d6ebbd89632267e78
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-03 15:53:19 +0530

    Product Service Implementation class

--- SpringBootFeatures/SpringBootCacheAdvanced/src/main/java/com/anilt/cache/service/impl/ProductServiceImpl.java
+++ SpringBootFeatures/SpringBootCacheAdvanced/src/main/java/com/anilt/cache/service/impl/ProductServiceImpl.java
@@ -3,31 +3,53 @@ package com.anilt.cache.service.impl;
 import com.anilt.cache.model.Product;
 import com.anilt.cache.service.ProductService;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProductServiceImpl implements ProductService {
+    private static Map<String, Product> productMap = new HashMap<>();
+    static {
+        Product p1 = new Product();
+        p1.setId("1");
+        p1.setName("Canon");
+
+        Product p2 = new Product();
+        p2.setId("2");
+        p2.setName("Nikon");
+
+        Product p3 = new Product();
+        p3.setId("3");
+        p3.setName("Sony");
+
+        productMap.put(p1.getId(), p1);
+        productMap.put(p2.getId(), p2);
+        productMap.put(p3.getId(), p3);
+    }
     @Override
     public Collection<Product> getAllProducts() {
-        return null;
+        return productMap.values();
     }
 
     @Override
     public Product getProductById(String id) {
-        return null;
+        return productMap.get(id);
     }
 
     @Override
     public void createProduct(Product product) {
-
+        productMap.put(product.getId(), product);
     }
 
     @Override
     public void updateProduct(String id, Product product) {
-
+        productMap.remove(id);
+        productMap.put(id, product);
     }
 
     @Override
     public void deleteProduct(String id) {
-
+        productMap.remove(id);
     }
 }
