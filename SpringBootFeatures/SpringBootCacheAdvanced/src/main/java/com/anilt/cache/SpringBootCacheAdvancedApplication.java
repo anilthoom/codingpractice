38a8f0f51c7dc14ed2e5baf6cc402ce7554c0a35
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-06 10:22:53 +0530

    Caching implementation

--- SpringBootFeatures/SpringBootCacheAdvanced/src/main/java/com/anilt/cache/SpringBootCacheAdvancedApplication.java
+++ SpringBootFeatures/SpringBootCacheAdvanced/src/main/java/com/anilt/cache/SpringBootCacheAdvancedApplication.java
@@ -9,19 +9,7 @@ import org.springframework.cache.annotation.EnableCaching;
 public class SpringBootCacheAdvancedApplication {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(SpringBootCacheAdvancedApplication.class, args);
-		System.out.println(test("contributor", null));
+		SpringApplication.run(SpringBootCacheAdvancedApplication.class, args);
 	}
 
-	public static String test(String type, String template){
-		if ((type.equalsIgnoreCase("contributor") && template == null)
-				|| (type.equalsIgnoreCase("reviewer") && template == null)
-				|| (type.equalsIgnoreCase("rejection") && template == null)
-				|| (type.equalsIgnoreCase("approval") && template == null)
-				|| (type.equalsIgnoreCase("review-participant") && template == null)
-				|| (type.equalsIgnoreCase("active") && template == null)) {
-			return null;
-		}
-		return "OK!!!";
-	}
 }
