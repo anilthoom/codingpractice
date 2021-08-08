ce1165f2c403151264db7943bcd462135195bfcc
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-08 15:50:32 +0530

    Unit testing Mockito

---
+++ SpringBootFeatures/UnitTestMockito/src/main/java/com/anilt/mockito/UnitTestMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.anilt.mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitTestMockitoApplication.class, args);
+	}
+
+}
