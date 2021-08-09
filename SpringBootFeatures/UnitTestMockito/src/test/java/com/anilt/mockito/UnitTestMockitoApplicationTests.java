a48c6ca8d87ab73c326860bdad9be5661bed9cd4
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-09 12:30:25 +0530

    Unit testing Mockito

--- SpringBootFeatures/UnitTestMockito/src/test/java/com/anilt/mockito/UnitTestMockitoApplicationTests.java
+++ SpringBootFeatures/UnitTestMockito/src/test/java/com/anilt/mockito/UnitTestMockitoApplicationTests.java
@@ -1,6 +1,12 @@
 package com.anilt.mockito;
 
+import com.anilt.mockito.service.OrderService;
+import com.anilt.mockito.service.ProductService;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -9,11 +15,19 @@ import javax.annotation.security.RunAs;
 
 @SpringBootTest
 @ActiveProfiles("test")
-
+@RunWith(SpringJUnit4ClassRunner.class)
 class UnitTestMockitoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private OrderService orderService;
 
+	@Autowired
+	private ProductService productService;
+
+	@Test
+	public void whenUserIdIsProvided_thenRetrievedNameIsCorrect(){
+		Mockito.when(productService.getProductName()).thenReturn("Mock Product Name");
+		String testName = orderService.getProductName();
+		Assert.assertEquals("Mock Product Name", testName);
+	}
 }
