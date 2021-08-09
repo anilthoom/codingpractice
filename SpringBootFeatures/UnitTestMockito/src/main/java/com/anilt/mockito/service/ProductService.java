80c37e4010dfab094454d7afaab1777fa7df120e
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-09 11:57:25 +0530

    Unit testing Mockito

---
+++ SpringBootFeatures/UnitTestMockito/src/main/java/com/anilt/mockito/service/ProductService.java
@@ -0,0 +1,10 @@
+package com.anilt.mockito.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    public String getProductName(){
+        return "Sony";
+    }
+}
