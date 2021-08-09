6a054953c3fb80ceefcf6e9cd2a75e25cf72ac31
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-09 11:58:38 +0530

    Unit testing Mockito

--- SpringBootFeatures/UnitTestMockito/src/test/java/com/anilt/mockito/UnitTestMockitoApplicationTests.java
+++ SpringBootFeatures/UnitTestMockito/src/test/java/com/anilt/mockito/UnitTestMockitoApplicationTests.java
@@ -2,8 +2,14 @@ package com.anilt.mockito;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.security.RunAs;
 
 @SpringBootTest
+@ActiveProfiles("test")
+
 class UnitTestMockitoApplicationTests {
 
 	@Test
