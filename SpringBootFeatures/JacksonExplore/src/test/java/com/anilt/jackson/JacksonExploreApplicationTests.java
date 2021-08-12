fe81fda54e050f0a4623840373b0e18abc935158
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-12 13:47:57 +0530

    Jackson parser explore

---
+++ SpringBootFeatures/JacksonExplore/src/test/java/com/anilt/jackson/JacksonExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anilt.jackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacksonExploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
