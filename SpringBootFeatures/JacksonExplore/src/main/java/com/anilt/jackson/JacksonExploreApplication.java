811f60b254731a10d28ce9c4df13a02c9766f6c1
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-12 13:47:49 +0530

    Jackson parser explore

---
+++ SpringBootFeatures/JacksonExplore/src/main/java/com/anilt/jackson/JacksonExploreApplication.java
@@ -0,0 +1,13 @@
+package com.anilt.jackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonExploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacksonExploreApplication.class, args);
+	}
+
+}
