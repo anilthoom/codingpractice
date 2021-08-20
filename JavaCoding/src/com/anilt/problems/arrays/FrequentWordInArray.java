0ad0065bf9af23e642cf7a566c7322f5cf8887df
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-20 15:32:59 +0530

    Frequent word from array

--- JavaCoding/src/com/anilt/problems/arrays/FrequentWordInArray.java
+++ JavaCoding/src/com/anilt/problems/arrays/FrequentWordInArray.java
@@ -14,14 +14,24 @@ public class FrequentWordInArray {
         Map<String, Integer> wordsCountMap = new HashMap<>();
         String word = "";
         Integer counter;
+        String heroWord = "";
+        Integer heroCounter = -1;
         for(int i=0; i<n; i++){
             word = arr[i];
             if(wordsCountMap.containsKey(word)){
                counter = wordsCountMap.get(word)+1;
                wordsCountMap.put(word, counter);
+               if(counter > heroCounter){
+                   heroWord = word;
+                   heroCounter = counter;
+               }
             }
             else {
                 wordsCountMap.put(word, 1);
+                if(heroCounter == -1){
+                    heroWord = word;
+                    heroCounter = 1;
+                }
             }
         }
         for (Map.Entry<String, Integer> entry: wordsCountMap.entrySet()) {
