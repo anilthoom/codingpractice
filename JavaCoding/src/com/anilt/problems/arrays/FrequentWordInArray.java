683f333e38169b5a5fd1705f58b4b6bc1e655056
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-20 17:23:22 +0530

    Frequent word from array

--- JavaCoding/src/com/anilt/problems/arrays/FrequentWordInArray.java
+++ JavaCoding/src/com/anilt/problems/arrays/FrequentWordInArray.java
@@ -6,10 +6,14 @@ import java.util.Map;
 // https://practice.geeksforgeeks.org/problems/most-frequent-word-in-an-array-of-strings3528/1
 public class FrequentWordInArray {
     public static void main(String[] args) {
-        String array[] = {"xejdcj", "xejdcj", "lvjpb", "tmyuiu", "lvjpb", "tmyuiu", "ovoba", "lvjpb", "lvjpb", "fqhyu", "fqhyu", "tmyuiu", "xejdcj", "tmyuiu", "fqhyu", "ovoba", "xejdcj"};
+        String array[] = {"elgefh", "elgefh", "vuxct", "elgefh", "kmrht", "elgefh", "kmrht", "kmrht", "hjfd", "hjfd", "kmrht", "vuxct", "elgefh", "mzwlcq", "kmrht"};
         System.out.println(mostFrequentWord(array, array.length));
     }
-    public static String mostFrequentWord(String arr[],int n)
+    public static String mostFrequentWord(String arr[],int n){
+
+        return "";
+    }
+    public static String mostFrequentWordX(String arr[],int n)
     {
         Map<String, Integer> wordsCountMap = new HashMap<>();
         String word = "";
@@ -21,21 +25,17 @@ public class FrequentWordInArray {
             if(wordsCountMap.containsKey(word)){
                counter = wordsCountMap.get(word)+1;
                wordsCountMap.put(word, counter);
-               if(counter >= heroCounter){
-                   heroWord = word;
-                   heroCounter = counter;
-               }
             }
             else {
                 wordsCountMap.put(word, 1);
-                if(heroCounter == -1){
-                    heroWord = word;
-                    heroCounter = 1;
-                }
             }
         }
         for (Map.Entry<String, Integer> entry: wordsCountMap.entrySet()) {
             System.out.println(entry.getKey() +" - "+ entry.getValue());
+            if(heroCounter <= entry.getValue()){
+                heroWord = entry.getKey();
+                heroCounter = entry.getValue();
+            }
         }
         return heroWord;
     }
