629531c902d7c9a3e339a1262f4704bda472c491
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-08-20 18:46:35 +0530

    Find triplets with zero

--- JavaCoding/src/com/anilt/google/FindTripletsWithZero.java
+++ JavaCoding/src/com/anilt/google/FindTripletsWithZero.java
@@ -5,18 +5,20 @@ import java.util.Arrays;
 public class FindTripletsWithZero {
     public static void main(String[] args) {
         FindTripletsWithZero findTripletsWithZero = new FindTripletsWithZero();
-        int arr[] = {0, -1, 2, -3, 1};
-        findTripletsWithZero.findTriplets(arr, arr.length);
+        int arr[] = {0, -1, 2, -3, 3};
+        System.out.println(findTripletsWithZero.findTriplets(arr, arr.length));
+
     }
     public boolean findTriplets(int arr[] , int n)
     {
         Arrays.sort(arr);
         for(int i=0; i<n; i++){
+
             if(arr[i] + arr[i+1] + arr[i+2] == 0)
             {
                 return true;
             }
-            System.out.print(arr[i] + " ");
+            System.out.println(arr[i] + " ");
         }
         return false;
     }
