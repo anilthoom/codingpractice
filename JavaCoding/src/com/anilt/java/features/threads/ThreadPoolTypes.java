717dad05ff122214a768cc7893984f1bbb078348
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-03 14:37:57 +0530

    Practice

---
+++ JavaCoding/src/com/anilt/java/features/threads/ThreadPoolTypes.java
@@ -0,0 +1,17 @@
+package com.anilt.java.features.threads;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolTypes {
+    public static void main(String[] args) {
+        // Fixed thread pool
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(10);
+
+        // Cached thread pool
+
+        // Scheduled thread pool
+
+        // Single threaded executor
+    }
+}
