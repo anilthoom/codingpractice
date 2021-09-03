e4970a7eae6be818c7b12924222eaefe67ed17df
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-03 10:58:27 +0530

    Practice

---
+++ JavaCoding/src/com/anilt/java/features/threads/ExecutorServiceExploration.java
@@ -0,0 +1,14 @@
+package com.anilt.java.features.threads;
+
+public class ExecutorServiceExploration {
+    public static void main(String[] args) {
+
+    }
+}
+class Task implements Runnable{
+
+    @Override
+    public void run() {
+
+    }
+}
