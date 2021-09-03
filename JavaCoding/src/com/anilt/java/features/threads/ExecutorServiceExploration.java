b669f1b6ac0f0ac32d84ea30e443a0754acff83c
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-03 13:33:03 +0530

    Practice

--- JavaCoding/src/com/anilt/java/features/threads/ExecutorServiceExploration.java
+++ JavaCoding/src/com/anilt/java/features/threads/ExecutorServiceExploration.java
@@ -1,14 +1,17 @@
 package com.anilt.java.features.threads;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class ExecutorServiceExploration {
     public static void main(String[] args) {
-
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        service.execute(new Task());
     }
 }
 class Task implements Runnable{
-
     @Override
     public void run() {
-
+        System.out.println("Run... run...");
     }
 }
