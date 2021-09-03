5218b42405585fd8a4b5e2f35e70f301e7d3d5ec
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-03 13:56:05 +0530

    Practice

--- JavaCoding/src/com/anilt/java/features/threads/ThreadUsage.java
+++ JavaCoding/src/com/anilt/java/features/threads/ThreadUsage.java
@@ -2,7 +2,7 @@ package com.anilt.java.features.threads;
 
 public class ThreadUsage {
     public static void main(String[] args) {
-        for (int i =0; i<10; i++){
+        for (int i =0; i<1000; i++){
             new Thread(new TaskT()).start();
             new Thread(new TaskR()).start();
         }
@@ -11,12 +11,12 @@ public class ThreadUsage {
 class TaskT extends Thread{
     @Override
     public void run() {
-        System.out.println("-- Thread class thread ---");
+        System.out.println("-- Thread class thread ---" + Thread.currentThread().getName());
     }
 }
 class TaskR implements Runnable{
     @Override
     public void run() {
-        System.out.println("-- Runnable class thread ---");
+        System.out.println("-- Runnable class thread ---"+ Thread.currentThread().getName());
     }
 }
