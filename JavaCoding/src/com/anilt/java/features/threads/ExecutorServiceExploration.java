81a9eb4503e61d9f4afed53a8c6e1f926868e629
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-03 14:17:45 +0530

    Practice

--- JavaCoding/src/com/anilt/java/features/threads/ExecutorServiceExploration.java
+++ JavaCoding/src/com/anilt/java/features/threads/ExecutorServiceExploration.java
@@ -7,7 +7,8 @@ public class ExecutorServiceExploration {
     public static void main(String[] args) {
         int coreCount = Runtime.getRuntime().availableProcessors();
         System.out.println("Total Processors : "+ coreCount);
-        //If the task execution is of type CPU intensive, then this approach is useful
+        // If the task execution is of type CPU intensive, then this approach is useful
+        // For IO intensive task (or waiting kind, ex: HTTP request) having higher count is more effective. Ex: Executors.newFixedThreadPool(100);
         ExecutorService service = Executors.newFixedThreadPool(coreCount);
         long startTime = System.currentTimeMillis();
         for (int i=0; i< 1000; i++){
