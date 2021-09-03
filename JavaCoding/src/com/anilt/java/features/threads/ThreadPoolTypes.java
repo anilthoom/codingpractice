1230f9904cead543379369f870d09c9ffba11ee7
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-03 14:47:46 +0530

    Practice

--- JavaCoding/src/com/anilt/java/features/threads/ThreadPoolTypes.java
+++ JavaCoding/src/com/anilt/java/features/threads/ThreadPoolTypes.java
@@ -9,9 +9,18 @@ public class ThreadPoolTypes {
         ExecutorService fixedThreadPool = Executors.newFixedThreadPool(10);
 
         // Cached thread pool
-
+        ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+        for (int i = 0; i<100; i++){
+            cachedThreadPool.execute(new TaskHandler());
+        }
         // Scheduled thread pool
 
         // Single threaded executor
+    }
+}
+class TaskHandler implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Task executed : "+ Thread.currentThread().getName());
     }
 }
