edb5d19d31c488f37683db3c3e87c422175d6651
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-04 15:38:52 +0530

    Practice

--- JavaCoding/src/com/anilt/faang/arrays/TwoSum.java
+++ JavaCoding/src/com/anilt/faang/arrays/TwoSum.java
@@ -1,17 +1,17 @@
 package com.anilt.faang.arrays;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 //https://leetcode.com/problems/two-sum/
 public class TwoSum {
     public static void main(String[] args) {
         TwoSum twoSum = new TwoSum();
-        int[] arr = {1, 5, 11, 4, 9};
-        int target = 20;
-        twoSum.twoSumFast(arr, target);
-        int result[] = twoSum.twoSum(arr, target);
+        int[] arr = {3, 2, 4};
+        int target = 6;
+        int result[] = twoSum.twoSumFast(arr, target);
+        //int result[] = twoSum.twoSum(arr, target);
         if(result != null){
             for(int i=0; i<result.length; i++){
                 System.out.print(result[i]+ " ");
@@ -21,17 +21,23 @@ public class TwoSum {
             System.out.println("No target found or invalid input");
         }
     }
-    //Optimal solution
+    //Optimal solution O(n)
     public int[] twoSumFast(int[] nums, int target){
-        List listNums = Arrays.asList(nums);
+        Map<Integer, Integer> targetMap = new HashMap<Integer, Integer>();
         int findNum;
+        int ans[] = new int[2];
         for(int i=0; i< nums.length; i++){
             findNum = target - nums[i];
-            System.out.println(listNums.indexOf(findNum));
+            if(targetMap.containsKey(nums[i])){
+                ans[1] = i;
+                ans[0] = targetMap.get(nums[i]);
+                return ans;
+            }
+            targetMap.put(findNum, i);
         }
         return null;
     }
-    //Brut force approach
+    //Brut force approach O(n^2)
     public int[] twoSum(int[] nums, int target) {
        int answer[] = new int[2];
        for (int i=0; i<nums.length-1; i++){
