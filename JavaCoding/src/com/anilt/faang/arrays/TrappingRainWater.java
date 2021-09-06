e6168b6f9b0d67bc75b7fbef859c11f8edc7e25b
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-06 19:55:37 +0530

    Trapping rain water problem

--- JavaCoding/src/com/anilt/faang/arrays/TrappingRainWater.java
+++ JavaCoding/src/com/anilt/faang/arrays/TrappingRainWater.java
@@ -1,12 +1,14 @@
 package com.anilt.faang.arrays;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 //https://leetcode.com/problems/trapping-rain-water/
 public class TrappingRainWater {
     public static void main(String[] args) {
         TrappingRainWater trappingRainWater = new TrappingRainWater();
-        int arr[] = {0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2};
+        int arr[] = {0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 3};
         System.out.println("\n"+trappingRainWater.trap(arr));
     }
     public int trap(int[] height) {
@@ -31,12 +33,15 @@ public class TrappingRainWater {
                 while(p1 >= p2){
                     waterBucket += p1 - p2;
                     i = i+1;
+                    if(i >= height.length)
+                        break;
                     System.out.print(p2 + "  - ");
                     p2 = height[i];
                     System.out.println(p2);
                 }
                 p1 = p2;
-                p2 = height[i+1];
+                if(i < height.length)
+                    p2 = height[i+1];
             }
         }
 
