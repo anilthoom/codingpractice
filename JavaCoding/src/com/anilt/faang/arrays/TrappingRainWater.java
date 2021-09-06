8b1fe4bb8885060929e9858552ce5fd177885d48
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-06 17:19:59 +0530

    Trapping rain water problem

---
+++ JavaCoding/src/com/anilt/faang/arrays/TrappingRainWater.java
@@ -0,0 +1,16 @@
+package com.anilt.faang.arrays;
+//https://leetcode.com/problems/trapping-rain-water/
+public class TrappingRainWater {
+    public static void main(String[] args) {
+        TrappingRainWater trappingRainWater = new TrappingRainWater();
+        int arr[] = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trappingRainWater.trap(arr));
+    }
+    public int trap(int[] height) {
+        for (int i =0; i< height.length; i++){
+
+        }
+        return 0;
+    }
+
+}
