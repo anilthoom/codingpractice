bb22c0e5df348035ee25c57fb56cbbe18d225131
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-06 20:27:37 +0530

    Trapping rain water problem

--- JavaCoding/src/com/anilt/faang/arrays/TrappingRainWater.java
+++ JavaCoding/src/com/anilt/faang/arrays/TrappingRainWater.java
@@ -8,17 +8,14 @@ import java.util.List;
 public class TrappingRainWater {
     public static void main(String[] args) {
         TrappingRainWater trappingRainWater = new TrappingRainWater();
-        int arr[] = {0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 3};
+        int arr[] = {0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2};
         System.out.println("\n"+trappingRainWater.trap(arr));
     }
     public int trap(int[] height) {
         int p1 = -1;
         int p2 = -1;
         int waterBucket = 0;
-        int initValue = height[0];
-        // Need to do ltrim rtrim of 0
-
-
+        ArrayList<ArrayList> bucketTuples = new ArrayList<>();
         boolean beginShow = false;
         for (int i = 0; i< height.length; i++){
             if(!beginShow && height[i] > 0 && height[i] > height[i+1] ){
@@ -29,22 +26,34 @@ public class TrappingRainWater {
                 System.out.println("STARTS HERE... "+ p1 + " At Index "+ i);
             }
             if(beginShow){
-                System.out.print(p1+" - ");
+                ArrayList tuples = new ArrayList();
+                System.out.print(p1);
+                tuples.add(p1);
                 while(p1 >= p2){
+                    System.out.print(" - "+p2);
+                    tuples.add(p2);
                     waterBucket += p1 - p2;
                     i = i+1;
-                    if(i >= height.length)
+                    if(i >= height.length){
+                        bucketTuples.add(tuples);
                         break;
-                    System.out.print(p2 + "  - ");
+                    }
+
                     p2 = height[i];
-                    System.out.println(p2);
                 }
                 p1 = p2;
-                if(i < height.length)
+                if(i < height.length){
+                    System.out.println(" - "+p2);
+                    tuples.add(p2);
+                    bucketTuples.add(tuples);
                     p2 = height[i+1];
+                }
             }
         }
 
+        for(int i = 0; i< bucketTuples.size(); i++){
+            ArrayList tupleArr = bucketTuples.get(i);
+        }
         return waterBucket;
     }
 
