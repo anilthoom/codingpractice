e1681c98f674725dd6010b0279ce66815f69b2ce
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-07 09:40:09 +0530

    Trapping rain water problem

--- JavaCoding/src/com/anilt/faang/arrays/TrappingRainWater.java
+++ JavaCoding/src/com/anilt/faang/arrays/TrappingRainWater.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class TrappingRainWater {
     public static void main(String[] args) {
         TrappingRainWater trappingRainWater = new TrappingRainWater();
-        int arr[] = {0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2};
+        int arr[] = {0,1,0,2,1,0,1,3,2,1,2,1};
         System.out.println("\n"+trappingRainWater.trap(arr));
     }
     public int trap(int[] height) {
@@ -32,7 +32,6 @@ public class TrappingRainWater {
                 while(p1 >= p2){
                     System.out.print(" - "+p2);
                     tuples.add(p2);
-                    waterBucket += p1 - p2;
                     i = i+1;
                     if(i >= height.length){
                         bucketTuples.add(tuples);
@@ -50,9 +49,27 @@ public class TrappingRainWater {
                 }
             }
         }
-
+        System.out.println();
         for(int i = 0; i< bucketTuples.size(); i++){
             ArrayList tupleArr = bucketTuples.get(i);
+            int firstVal = (int) tupleArr.get(0);
+            int lastVal =  (int) tupleArr.get(tupleArr.size()-1);
+            if(firstVal<=lastVal){
+                for (int j=0; j< tupleArr.size()-1; j++){
+                    System.out.print(" "+tupleArr.get(j));
+                    int current = (int)tupleArr.get(j);
+                    waterBucket += firstVal-current;
+                }
+            }
+            else {
+                firstVal = (int) tupleArr.get(tupleArr.size()-1);
+                for (int j=tupleArr.size()-1; j> 0; j--){
+                    System.out.print(" "+tupleArr.get(j));
+                    int current = (int)tupleArr.get(j);
+                    waterBucket += firstVal-current;
+                }
+            }
+            System.out.println();
         }
         return waterBucket;
     }
