32d6ccc85c4a7a6b54e507a4b52900c03e16a99f
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-14 16:29:17 +0530

    Dynamic

--- ReactJS/react-course/src/App.js
+++ ReactJS/react-course/src/App.js
@@ -3,9 +3,9 @@ import Todo from "./components/Todo";
 function App() {
   return (<div>
     <h1>My Todos</h1>
-    <Todo />
-    <Todo />
-    <Todo />
+    <Todo text='Learn React'/>
+    <Todo text='Learn Kafka'/>
+    <Todo text='Learn Docker'/>
   </div>);
 }
 
