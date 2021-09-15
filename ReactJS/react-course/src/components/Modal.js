bfb02397d4025ebdaf64e8786c339ff2edb0c3b6
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-15 19:28:42 +0530

    onclick event impl

--- ReactJS/react-course/src/components/Modal.js
+++ ReactJS/react-course/src/components/Modal.js
@@ -1,14 +1,14 @@
-function Modal(props){
-    function cancelHandler(){
+function Modal(props) {
+    function cancelHandler() {
         props.onCancel();
     }
-    function confirmHandler(){
+    function confirmHandler() {
         props.onConfirm();
     }
-    return(
+    return (
         <div className="modal">
             <p>Are you sure?</p>
-            <button className='btn btn--alt' onClick={cancelHandler}>Cancel</button>
+            <button className='btn btn--alt' onClick={props.onCancel}>Cancel</button>
             <button className='btn' onClick={confirmHandler}>Confirm</button>
         </div>
     );
