6d669d2a7c7c1cb0ddae7c7332ff95fbab9e3e5d
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-16 12:49:30 +0530

    Meetups custom components

---
+++ ReactJS/meet-up/src/components/meetups/MeetupItem.js
@@ -0,0 +1,19 @@
+function MeetupItem(){
+    return(
+        <li>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <h3> Title </h3>
+                <address> address </address>
+                <p>description</p>
+            </div>
+            <div>
+                <button> To Favourites</button>
+            </div>
+        </li>
+    );
+}
+
+export default MeetupItem;
