a68c54bedce519c8f80ddd6b916b5b0932d6c01e
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-16 11:09:59 +0530

    Router

--- ReactJS/meet-up/src/App.js
+++ ReactJS/meet-up/src/App.js
@@ -1,5 +1,22 @@
+import { Route } from "react-router";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavrouitePage from "./pages/Favourite";
+import NewMeetupPage from "./pages/NewMeetup";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Route path='/'>
+        <AllMeetupsPage></AllMeetupsPage>
+      </Route>
+      <Route path='/new-meetup'>
+        <NewMeetupPage></NewMeetupPage>
+      </Route>
+      <Route path='/favourites'>
+        <FavrouitePage></FavrouitePage>
+      </Route>
+    </div>
+    );
 }
 
 export default App;
