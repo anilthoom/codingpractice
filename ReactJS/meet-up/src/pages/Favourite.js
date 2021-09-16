0a1b75faa235b1470514d51e9eb8f9891df35947
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-16 20:24:57 +0530

    Badge style

--- ReactJS/meet-up/src/pages/Favourite.js
+++ ReactJS/meet-up/src/pages/Favourite.js
@@ -1,5 +1,21 @@
+import { useContext } from "react";
+import MeetupList from "../components/meetups/MeetupList";
+import FavouritesContext from "../store/favourite-context";
+
 function FavrouitePage(){
-    return <div>Favourite meetups page</div>;
+    const favouritesCtx = useContext(FavouritesContext);
+    let content;
+    if(favouritesCtx.totalFavourites === 0){
+        content = <p>You got no favourites yet. Start add some?</p>
+    } else{
+        content = <MeetupList meetups={favouritesCtx.favourites} />
+    }
+    return (
+        <section>
+            <h1>My Favourites</h1>
+            {content}
+        </section>
+    );
 }
 
 export default FavrouitePage;
