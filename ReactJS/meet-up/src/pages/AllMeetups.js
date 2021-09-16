c5937c59c1b7035b401df9d83664d60a44ef0aa7
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-16 18:52:38 +0530

    Fetch from firebase

--- ReactJS/meet-up/src/pages/AllMeetups.js
+++ ReactJS/meet-up/src/pages/AllMeetups.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 const DUMMY_DATA = [
@@ -18,10 +19,40 @@ const DUMMY_DATA = [
 ];
 
 function AllMeetupsPage(){
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedMeetups, setLoadedMetups] = useState([]);
+
+    useEffect(() => {
+            setIsLoading(true);
+            fetch(
+            'https://react-meetup-c32f2-default-rtdb.firebaseio.com/meetups.json'
+            ).then(response => {
+                return response.json();
+            }).then(data => {
+                const meetups = [];
+                for(const key in data){
+                    const meetup = {
+                        id: key,
+                        ...data[key]
+                    };
+                    meetups.push(meetup);
+                }
+                setIsLoading(false);
+                setLoadedMetups(meetups);
+            });
+    }, []);
+    
+    if(isLoading){
+        return(
+            <section>
+                <p>Loading...</p>
+            </section>
+        );
+    }
     return (
         <section>
             <h1>All Meetups</h1>
-            <MeetupList meetups={DUMMY_DATA}/>
+            <MeetupList meetups={loadedMeetups}/>
         </section>
     );
 }
