3abd12cafdd7482da2f0e6a44ba9b0bc0d923b22
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-22 17:20:06 +0530

    Todo Item

--- ReactJS/todo-app/src/App.js
+++ ReactJS/todo-app/src/App.js
@@ -1,9 +1,12 @@
+import TodoItem from "./components/TodoItem";
+
 function App() {
   return (
     <div>
       <h1>My Todo</h1>
       <input type="text"></input>
       <button>Add</button>
+      <TodoItem />
     </div>
   );
 }
---
+++ ReactJS/todo-app/src/components/TodoItem.js
@@ -0,0 +1,10 @@
+function TodoItem(){
+    return(
+        <div>
+            <h2>Complete reading</h2>
+            <div>31 December 2021</div>
+        </div>
+    );
+}
+
+export default TodoItem;
