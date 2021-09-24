4ab9b47a552b70c910837cb62f184ad32f754983
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-24 15:24:50 +0530

    Trapping rain water problem

--- JavaCoding/src/com/anilt/faang/arrays/TrappingRainWater.java
+++ JavaCoding/src/com/anilt/faang/arrays/TrappingRainWater.java
@@ -8,79 +8,45 @@ import java.util.List;
 public class TrappingRainWater {
     public static void main(String[] args) {
         TrappingRainWater trappingRainWater = new TrappingRainWater();
-//        int arr[] = {0,1,0,2,1,0,1,3,2,1,2,1};
-        int arr[] = {4,2,0,3,2,5};
+        int arr[] = {0,1,0,2,1,0,1,3,2,1,2,1};
+//        int arr[] = {4,2,0,3,2,5};
         System.out.println("\n"+trappingRainWater.trap(arr));
     }
-    public int trap(int[] height) {
+    public int trap_opt(int[] height) {
         // currentWater = min(maxL, maxR) - currentHeight
+        int left=0, right=height.length-1, total=0;
+        int maxLeft=0, maxRight=0;
+        while (left < right){
+            if(height[left] <= height[right]){
+
+
+            }
+        }
         return 0;
     }
-    public int trap_bad(int[] height) {
-        int p1 = -1;
-        int p2 = -1;
-        int waterBucket = 0;
-        ArrayList<ArrayList> bucketTuples = new ArrayList<>();
-        boolean beginShow = false;
-        for (int i = 0; i< height.length; i++){
-            if(!beginShow && height[i] > 0 && height[i] > height[i+1] ){
-                beginShow = true;
-                p1 = height[i];
-                i = i+1;
-                p2 = height[i];
-                System.out.println("STARTS HERE... "+ p1 + " At Index "+ i);
+    public int trap(int[] height) {
+        int totalWater = 0;
+        int leftP, rightP, maxLeft, maxRight, currentWater;
+        for (int p = 0; p< height.length; p++){
+            leftP = p;
+            rightP = p;
+            maxLeft =0;
+            maxRight = 0;
+
+            while (leftP >= 0){
+                maxLeft = Math.max(maxLeft, height[leftP]);
+                leftP--;
             }
-            if(beginShow){
-                ArrayList tuples = new ArrayList();
-                System.out.print(p1);
-                tuples.add(p1);
-                while(p1 >= p2){
-                    System.out.print(" - "+p2);
-                    tuples.add(p2);
-                    i = i+1;
-                    if(i >= height.length){
-                        bucketTuples.add(tuples);
-                        break;
-                    }
-                    p2 = height[i];
-                }
-                p1 = p2;
-                if(i < height.length-1){
-                    System.out.println(" - "+p2);
-                    tuples.add(p2);
-                    bucketTuples.add(tuples);
-                    p2 = height[i+1];
-                }
+            while (rightP < height.length){
+                maxRight = Math.max(maxRight, height[rightP]);
+                rightP++;
+            }
+            currentWater = Math.min(maxLeft, maxRight) - height[p];
+            if(currentWater >0){
+                totalWater += currentWater;
             }
         }
-        System.out.println();
-        for(int i = 0; i< bucketTuples.size(); i++){
-            ArrayList tupleArr = bucketTuples.get(i);
-            int firstVal = (int) tupleArr.get(0);
-            int lastVal =  (int) tupleArr.get(tupleArr.size()-1);
-            if(firstVal<=lastVal){
-                for (int j=0; j< tupleArr.size()-1; j++){
-                    System.out.print(" "+tupleArr.get(j));
-                    int current = (int)tupleArr.get(j);
-                    waterBucket += firstVal-current;
-                }
-            }
-            else {
-                firstVal = (int) tupleArr.get(tupleArr.size()-1);
-                for (int j=tupleArr.size()-1; j> 0; j--){
-                    System.out.print(" "+tupleArr.get(j));
-                    int current = (int)tupleArr.get(j);
-                    if(current >= firstVal){
-                        firstVal = current;
-                    }
-                    else {
-                        waterBucket += firstVal - current;
-                    }
-                }
-            }
-            System.out.println();
-        }
-        return waterBucket;
+        return totalWater;
     }
 
 }
