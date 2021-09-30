2b37096a0259fdc3c930bcee5ceabefa5cfd93c9
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-09-30 19:03:44 +0530

    Spring JPA Docker

---
+++ Docker/SpringJPADocker/src/main/java/com/anilt/jpa/docker/SpringJpaDockerApplication.java
@@ -0,0 +1,13 @@
+package com.anilt.jpa.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaDockerApplication.class, args);
+	}
+
+}
---
+++ Docker/SpringJPADocker/src/test/java/com/anilt/jpa/docker/SpringJpaDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anilt.jpa.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
