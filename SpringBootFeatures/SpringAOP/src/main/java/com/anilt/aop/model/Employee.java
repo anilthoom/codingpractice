839bebf14f64860a5961caa9a332a7eee15393fc
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-10-12 13:15:45 +0530

    Aspect oriented programming

---
+++ SpringBootFeatures/SpringAOP/src/main/java/com/anilt/aop/model/Employee.java
@@ -0,0 +1,14 @@
+package com.anilt.aop.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class Employee {
+    private String empId;
+    private String firstName;
+    private String lastName;
+}
