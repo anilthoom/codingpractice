ae3b7bce67d079b95cee351840da1808f9a9bf1e
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-10-13 15:43:58 +0530

    Aspect oriented programming

--- SpringBootFeatures/SpringAOP/src/main/java/com/anilt/aop/aspect/EmployeeServiceAspect.java
+++ SpringBootFeatures/SpringAOP/src/main/java/com/anilt/aop/aspect/EmployeeServiceAspect.java
@@ -1,10 +1,18 @@
 package com.anilt.aop.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+import com.anilt.aop.service.EmployeeService;
 
 @Aspect
 @Component
 public class EmployeeServiceAspect {
 
+    @Before(value = "execution(* EmployeeService.*(..)) and args(empId, fName, lName)")
+    public void beforeAdvice(JoinPoint joinPoint, String empId, String fName, String lName){
+        System.out.println("Before Method:" + joinPoint.getSignature());
+        System.out.println("Creating employee with : first name: "+fName+" and last name: "+lName);
+    }
 }
