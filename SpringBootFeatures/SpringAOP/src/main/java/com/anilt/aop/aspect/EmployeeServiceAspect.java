69839540601331611eea92cc25dc111a80863dd1
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-10-13 16:05:03 +0530

    Aspect oriented programming

--- SpringBootFeatures/SpringAOP/src/main/java/com/anilt/aop/aspect/EmployeeServiceAspect.java
+++ SpringBootFeatures/SpringAOP/src/main/java/com/anilt/aop/aspect/EmployeeServiceAspect.java
@@ -1,6 +1,7 @@
 package com.anilt.aop.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -13,5 +14,17 @@ public class EmployeeServiceAspect {
     public void beforeAdvice(JoinPoint joinPoint, String empId, String fName, String lName){
         System.out.println("Before Method:" + joinPoint.getSignature());
         System.out.println("Creating employee with : first name: "+fName+" and last name: "+lName);
+    }
+
+    @After(value = "execution(* com.anilt.aop.service.EmployeeService.*(..)) and args(empId, fName, lName)")
+    public void afterAdvice(JoinPoint joinPoint, String empId, String fName, String lName) {
+        System.out.println("After method:" + joinPoint.getSignature());
+        System.out.println("Creating Employee with first name - " + fName + ", second name - " + lName + " and id - " + empId);
+    }
+
+    @After(value = "execution(* com.anilt.aop.service.EmployeeService.*(..)) and args(empId)")
+    public void afterAdvice(JoinPoint joinPoint, String empId) {
+        System.out.println("After method:" + joinPoint.getSignature());
+        System.out.println("Deleted Employee with id - " + empId);
     }
 }
