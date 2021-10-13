3ec9a1d2fb2a9e8d5e1a503777f91fd8c56bfeba
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-10-13 16:43:41 +0530

    Aspect oriented programming

--- SpringBootFeatures/SpringAOP/src/main/java/com/anilt/aop/aspect/EmployeeServiceAspect.java
+++ SpringBootFeatures/SpringAOP/src/main/java/com/anilt/aop/aspect/EmployeeServiceAspect.java
@@ -1,7 +1,9 @@
 package com.anilt.aop.aspect;
 
+import com.anilt.aop.model.Employee;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -20,6 +22,12 @@ public class EmployeeServiceAspect {
     public void afterAdvice(JoinPoint joinPoint, String empId, String fName, String lName) {
         System.out.println("After method:" + joinPoint.getSignature());
         System.out.println("Creating Employee with first name - " + fName + ", second name - " + lName + " and id - " + empId);
+    }
+
+    @AfterReturning(value = "execution(* com.anilt.aop.service.EmployeeService.*(..))", returning = "employee")
+    public void afterReturningAdvice(JoinPoint joinPoint, Employee employee){
+        System.out.println("After successful execution: "+joinPoint.getSignature());
+        System.out.println(employee);
     }
 
     @After(value = "execution(* com.anilt.aop.service.EmployeeService.*(..)) and args(empId)")
