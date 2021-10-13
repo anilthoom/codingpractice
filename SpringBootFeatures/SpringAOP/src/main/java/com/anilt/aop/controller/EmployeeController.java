e99e80497d945a3910e2bd04c5704bbcfe4e56e1
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-10-13 15:51:07 +0530

    Aspect oriented programming

--- SpringBootFeatures/SpringAOP/src/main/java/com/anilt/aop/controller/EmployeeController.java
+++ SpringBootFeatures/SpringAOP/src/main/java/com/anilt/aop/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package com.anilt.aop.controller;
 import com.anilt.aop.model.Employee;
 import com.anilt.aop.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +13,7 @@ public class EmployeeController {
     @Autowired
     EmployeeService employeeService;
 
+    @RequestMapping(value = "/add/employee", method = RequestMethod.GET)
     public Employee addEmployee(@RequestParam("empId") String empId, @RequestParam("firstName") String fName, @RequestParam("lastName") String lName){
         return employeeService.createEmployee(empId, fName, lName);
     }
