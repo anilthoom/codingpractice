2a7232e9c6f79b57e49813a7305391585852349d
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-10-28 16:44:46 +0530

    AOP

--- SpringBootFeatures/RestAPIs/src/main/java/com/anil/rest/RestApIsApplication.java
+++ SpringBootFeatures/RestAPIs/src/main/java/com/anil/rest/RestApIsApplication.java
@@ -2,8 +2,10 @@ package com.anil.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class RestApIsApplication {
 
 	public static void main(String[] args) {
