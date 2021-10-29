1b00c7116f64c93025e95c2e593dad37385f27b4
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-10-29 15:39:34 +0530

    AOP

--- SpringBootFeatures/RestAPIs/src/main/java/com/anil/rest/aop/ProductServiceAspect.java
+++ SpringBootFeatures/RestAPIs/src/main/java/com/anil/rest/aop/ProductServiceAspect.java
@@ -2,6 +2,7 @@ package com.anil.rest.aop;
 
 import com.anil.rest.model.Product;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -19,5 +20,10 @@ public class ProductServiceAspect {
     @AfterReturning(value = "execution(* com.anil.rest.controller.ProductServiceController.*(..)) and args(product)")
     public void afterReturningAdviceProduct(JoinPoint joinPoint, Product product){
         System.out.println("AOP : "+ product.getName());
+    }
+
+    @After(value = "execution(* com.anil.rest.controller.ProductServiceController.*(..)) and args(product)")
+    public void afterAdvice(JoinPoint joinPoint, Product product){
+        System.out.println("After Method:" + joinPoint.getSignature());
     }
 }
