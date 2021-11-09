7ddbbd6680e1b919e5214e8992b4ed5edc9c544d
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-09 12:52:45 +0530

    Logger

--- SpringBootFeatures/SpringLogger/src/main/java/com/anilt/log/controller/LombokLoggingController.java
+++ SpringBootFeatures/SpringLogger/src/main/java/com/anilt/log/controller/LombokLoggingController.java
@@ -1,11 +1,13 @@
 package com.anilt.log.controller;
 
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+//@Log4j2
 public class LombokLoggingController {
     @RequestMapping("/lombok")
     public String index() {
