259db90885a751f989d0b080ecacbca142c0dc00
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-10 13:48:15 +0530

    Leet Code Practice

--- LeetCodePractice/src/com/anil/binarysearch/BinarySearch.java
+++ LeetCodePractice/src/com/anil/binarysearch/BinarySearch.java
@@ -4,14 +4,25 @@ public class BinarySearch {
     public static void main(String[] args) {
         BinarySearch binarySearch = new BinarySearch();
         int nums[] = {-1,0,3,5,9,12};
-        int target = 9;
+        int target = 1;
         System.out.println(binarySearch.search(nums, target));
     }
     public int search(int[] nums, int target) {
-        // get the mid pointer
-        int pointer = nums.length/2;
-        System.out.println(pointer);
+       int startIndex =0;
+       int endIndex = nums.length-1;
+       while(startIndex <= endIndex){
+           int midIndex = startIndex +(endIndex-startIndex)/2;
+           if(nums[midIndex] == target){
+               return midIndex;
+           }
 
-        return -1;
+           if(nums[midIndex] < target){
+               startIndex = midIndex+1;
+           }
+           else if(nums[midIndex] > target){
+               endIndex = midIndex-1;
+           }
+       }
+       return -1;
     }
 }
