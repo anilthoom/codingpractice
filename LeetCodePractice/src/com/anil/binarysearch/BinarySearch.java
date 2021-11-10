b14ff644d650b79006a1cf0a2ba63659d5afe279
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-10 14:10:42 +0530

    Binary Search Ways

--- LeetCodePractice/src/com/anil/binarysearch/BinarySearch.java
+++ LeetCodePractice/src/com/anil/binarysearch/BinarySearch.java
@@ -4,9 +4,11 @@ public class BinarySearch {
     public static void main(String[] args) {
         BinarySearch binarySearch = new BinarySearch();
         int nums[] = {-1,0,3,5,9,12};
-        int target = 1;
+        int target = 0;
         System.out.println(binarySearch.search(nums, target));
+        System.out.println(binarySearch.search(nums, target, 0, nums.length-1));
     }
+    //Iterative
     public int search(int[] nums, int target) {
        int startIndex =0;
        int endIndex = nums.length-1;
@@ -24,5 +26,19 @@ public class BinarySearch {
            }
        }
        return -1;
+    }
+
+    //Recursive
+    public int search(int[] nums, int target, int startIndex, int endIndex) {
+        while (startIndex <= endIndex){
+            int midIndex = startIndex + (endIndex-startIndex)/2;
+            if(nums[midIndex] == target)
+                return midIndex;
+            if (nums[midIndex] < target)
+                return search(nums, target, midIndex+1, endIndex);
+            else if(nums[midIndex] > target)
+                return search(nums, target, startIndex, midIndex-1);
+        }
+        return -1;
     }
 }
