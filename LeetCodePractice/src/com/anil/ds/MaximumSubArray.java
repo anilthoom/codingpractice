49219b8adde4a2f61eb48e737fe57d3564664c0d
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-10 18:09:45 +0530

    Binary Search Ways

--- LeetCodePractice/src/com/anil/ds/MaximumSubArray.java
+++ LeetCodePractice/src/com/anil/ds/MaximumSubArray.java
@@ -4,14 +4,14 @@ package com.anil.ds;
 public class MaximumSubArray {
     public static void main(String[] args) {
         MaximumSubArray maximumSubArray = new MaximumSubArray();
-        int nums[] = {-3,-2,-2,-3};
+        int nums[] = {-1, 2};
         //int nums[] = {-2,1,-3,4,-1,2,1,-5,4}; //Output: 6 Explanation: [4,-1,2,1] has the largest sum = 6.
         System.out.println(maximumSubArray.maxSubArray(nums));
     }
     public int maxSubArray(int[] nums) {
-        int maxSoFar = nums[0];
+        int maxSoFar = 0;
         int maxEndsHere = 0;
-        for (int i=1; i<nums.length; i++){
+        for (int i=0; i<nums.length; i++){
             maxEndsHere = maxEndsHere + nums[i];
             if(maxSoFar < maxEndsHere)
                 maxSoFar = maxEndsHere;
