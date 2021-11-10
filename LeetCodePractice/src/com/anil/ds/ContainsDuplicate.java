9ea949625c2987113a2e9ac7ed22183cb4f0b11f
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-10 16:26:30 +0530

    Binary Search Ways

---
+++ LeetCodePractice/src/com/anil/ds/ContainsDuplicate.java
@@ -0,0 +1,24 @@
+package com.anil.ds;
+
+import java.util.Arrays;
+import java.util.Hashtable;
+
+//https://leetcode.com/problems/contains-duplicate/
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        ContainsDuplicate containsDuplicate = new ContainsDuplicate();
+        int nums[] = {1,2,3,12};
+        System.out.println(containsDuplicate.containsDuplicate(nums));
+    }
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0; i< nums.length; i++){
+            if(i+1 >= nums.length)
+                return false;
+            else if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
