f98ac034f02dc73d9031f5967b67b70e199ba937
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-10 12:29:09 +0530

    Leet Code Practice

--- LeetCodePractice/src/com/anil/binarysearch/BinarySearch.java
+++ LeetCodePractice/src/com/anil/binarysearch/BinarySearch.java
@@ -1,4 +1,10 @@
 package com.anil.binarysearch;
-
+//https://leetcode.com/problems/binary-search/
 public class BinarySearch {
+    public static void main(String[] args) {
+
+    }
+    public int search(int[] nums, int target) {
+        return -1;
+    }
 }
