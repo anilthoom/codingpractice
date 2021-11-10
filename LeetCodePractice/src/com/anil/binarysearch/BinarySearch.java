5bd8a1e1e96f3760631a727f07b4f8a244d55005
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-10 12:32:26 +0530

    Leet Code Practice

--- LeetCodePractice/src/com/anil/binarysearch/BinarySearch.java
+++ LeetCodePractice/src/com/anil/binarysearch/BinarySearch.java
@@ -2,9 +2,16 @@ package com.anil.binarysearch;
 //https://leetcode.com/problems/binary-search/
 public class BinarySearch {
     public static void main(String[] args) {
-
+        BinarySearch binarySearch = new BinarySearch();
+        int nums[] = {-1,0,3,5,9,12};
+        int target = 9;
+        System.out.println(binarySearch.search(nums, target));
     }
     public int search(int[] nums, int target) {
+        // get the mid pointer
+        int pointer = nums.length/2;
+        System.out.println(pointer);
+
         return -1;
     }
 }
