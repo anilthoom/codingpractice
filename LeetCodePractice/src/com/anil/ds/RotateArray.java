06363742e53d6d76d63516d45d5e99ec10e712f6
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-11 13:38:22 +0530

    Rotate Array

--- LeetCodePractice/src/com/anil/ds/RotateArray.java
+++ LeetCodePractice/src/com/anil/ds/RotateArray.java
@@ -1,13 +1,16 @@
 package com.anil.ds;
 
+import java.util.Arrays;
+
 public class RotateArray {
     public static void main(String[] args) {
         RotateArray rotateArray = new RotateArray();
         int nums[] = {1,2,3,4,5,6,7};
-        int k = 3;
+        int k = 3; // Output: [5,6,7,1,2,3,4]
         rotateArray.rotate(nums, k);
     }
     public void rotate(int[] nums, int k) {
-
+        int subArr[] = Arrays.copyOfRange(nums, nums.length-3, nums.length);
+        System.out.println();
     }
 }
