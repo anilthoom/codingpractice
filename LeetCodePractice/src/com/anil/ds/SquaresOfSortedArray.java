f33100dfbf7a4aaf9989490feaa845123994004b
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-11 13:27:16 +0530

    Squares of a Sorted Array

--- LeetCodePractice/src/com/anil/ds/SquaresOfSortedArray.java
+++ LeetCodePractice/src/com/anil/ds/SquaresOfSortedArray.java
@@ -1,5 +1,8 @@
 package com.anil.ds;
 
+import java.util.Arrays;
+
+//https://leetcode.com/problems/squares-of-a-sorted-array/
 public class SquaresOfSortedArray {
     public static void main(String[] args) {
         SquaresOfSortedArray squaresOfSortedArray = new SquaresOfSortedArray();
@@ -7,6 +10,9 @@ public class SquaresOfSortedArray {
         squaresOfSortedArray.sortedSquares(nums);
     }
     public int[] sortedSquares(int[] nums) {
+        for(int i =0; i<nums.length; i++){
+
+        }
         return null;
     }
 }
