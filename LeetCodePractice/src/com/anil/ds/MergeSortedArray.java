ef138eb71acd51d76e660c7f66c69e1e4b3920ec
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-11 13:15:09 +0530

    Merge Sorted Array

---
+++ LeetCodePractice/src/com/anil/ds/MergeSortedArray.java
@@ -0,0 +1,16 @@
+package com.anil.ds;
+
+//https://leetcode.com/problems/merge-sorted-array/
+public class MergeSortedArray {
+    public static void main(String[] args) {
+        MergeSortedArray mergeSortedArray = new MergeSortedArray();
+        int nums1[] = {1,2,3,0,0,0};
+        int m = 3;
+        int nums2[] = {2,5,6};
+        int n = 3;
+        System.out.println(mergeSortedArray);
+    }
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+
+    }
+}
