b2a1b37fd1922e12615666ef793ebe081d3e477b
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-11 14:07:03 +0530

    Rotate Array

--- LeetCodePractice/src/com/anil/ds/RotateArray.java
+++ LeetCodePractice/src/com/anil/ds/RotateArray.java
@@ -5,20 +5,22 @@ import java.util.Arrays;
 public class RotateArray {
     public static void main(String[] args) {
         RotateArray rotateArray = new RotateArray();
-        int nums[] = {1,2,3,4,5,6,7};
-        int k = 3; // Output: [5,6,7,1,2,3,4]
+        int nums[] = {-1,-100,3,99};
+        int k = 2; // Output: [5,6,7,1,2,3,4]3     [-1,-100,3,99] 2
         rotateArray.rotate(nums, k);
     }
     public void rotate(int[] nums, int k) {
-        int rightArr[] = Arrays.copyOfRange(nums, nums.length-3, nums.length);
-        int leftArr[] = Arrays.copyOfRange(nums, 0,nums.length-3);
-        for(int i=0; i<rightArr.length; i++){
-            nums[i] = rightArr[i];
-        }
-        int index = rightArr.length;
-        for(int i=0; i<leftArr.length; i++){
-            nums[index] = leftArr[i];
-            index++;
+        if(nums.length > k) {
+            int rightArr[] = Arrays.copyOfRange(nums, nums.length-k, nums.length);
+            int leftArr[] = Arrays.copyOfRange(nums, 0,nums.length-k);
+            for(int i=0; i<rightArr.length; i++){
+                nums[i] = rightArr[i];
+            }
+            int index = rightArr.length;
+            for(int i=0; i<leftArr.length; i++){
+                nums[index] = leftArr[i];
+                index++;
+            }
         }
     }
 }
