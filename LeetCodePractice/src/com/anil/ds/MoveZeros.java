f427cbacefa39c3018031cd42ee52c7fa05edfab
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-11 14:29:19 +0530

    Move Zeros

---
+++ LeetCodePractice/src/com/anil/ds/MoveZeros.java
@@ -0,0 +1,13 @@
+package com.anil.ds;
+
+//https://leetcode.com/problems/move-zeroes/
+public class MoveZeros {
+    public static void main(String[] args) {
+        MoveZeros moveZeros = new MoveZeros();
+        int nums[] = {0,1,0,3,12}; //Output: [1,3,12,0,0]
+        moveZeros.moveZeroes(nums);
+    }
+    public void moveZeroes(int[] nums) {
+
+    }
+}
