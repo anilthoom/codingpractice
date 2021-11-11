705d0249edb705ae7632c580ab841fac0ea760fc
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-11 15:56:25 +0530

    Move Zeros

--- LeetCodePractice/src/com/anil/ds/MoveZeros.java
+++ LeetCodePractice/src/com/anil/ds/MoveZeros.java
@@ -8,26 +8,11 @@ public class MoveZeros {
         MoveZeros moveZeros = new MoveZeros();
         int nums[] = {0,1,0,3,12}; //Output: [1,3,12,0,0]
         moveZeros.moveZeroes(nums);
-        System.out.println();
     }
+    //Note that you must do this in-place without making a copy of the array.
     public void moveZeroes(int[] nums) {
-        int ans[] =  new int[nums.length];
-        int index = 0;
-        int zeroCounter = 0;
         for(int i=0; i<nums.length; i++){
-            if(nums[i] != 0){
-                ans[index] = nums[i];
-                index++;
-            }
-            else {
-                zeroCounter++;
-            }
+
         }
-        int ansCounter = ans.length;
-        for(int i=0; i<zeroCounter; i++){
-            ansCounter = ansCounter-1;
-            ans[ansCounter] = 0;
-        }
-        nums = Arrays.copyOf(ans, ans.length);
     }
 }
