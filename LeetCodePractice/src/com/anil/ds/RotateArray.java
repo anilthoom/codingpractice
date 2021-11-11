f77959d2e6c07611e40df21b71f35ac526072204
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-11 13:33:07 +0530

    Squares of a Sorted Array

---
+++ LeetCodePractice/src/com/anil/ds/RotateArray.java
@@ -0,0 +1,13 @@
+package com.anil.ds;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        RotateArray rotateArray = new RotateArray();
+        int nums[] = {1,2,3,4,5,6,7};
+        int k = 3;
+        rotateArray.rotate(nums, k);
+    }
+    public void rotate(int[] nums, int k) {
+
+    }
+}
