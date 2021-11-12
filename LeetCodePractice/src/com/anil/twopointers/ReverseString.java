64d36c2be81c5abd27377b86e1697fba221189f7
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-12 14:09:44 +0530

    Reverse String

--- LeetCodePractice/src/com/anil/twopointers/ReverseString.java
+++ LeetCodePractice/src/com/anil/twopointers/ReverseString.java
@@ -7,6 +7,16 @@ public class ReverseString {
         reverseString.reverseString(s);
     }
     public void reverseString(char[] s) {
-
+        int startPointer = 0;
+        int endPointer = s.length-1;
+        char temp;
+        while (startPointer < endPointer){
+            //Swap and change the pointers
+            temp = s[startPointer];
+            s[startPointer] = s[endPointer];
+            s[endPointer] = temp;
+            startPointer++;
+            endPointer--;
+        }
     }
 }
