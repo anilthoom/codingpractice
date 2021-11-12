2cac833dbf6228bda6d1a72edbcf0ede18c7391b
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-12 18:48:41 +0530

    Middle of the Linked List

--- LeetCodePractice/src/com/anil/twopointers/MiddleOfLinkedList.java
+++ LeetCodePractice/src/com/anil/twopointers/MiddleOfLinkedList.java
@@ -3,9 +3,38 @@ package com.anil.twopointers;
 //https://leetcode.com/problems/middle-of-the-linked-list/
 public class MiddleOfLinkedList {
     public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode two = new ListNode(2);
+        head.next = two;
+        ListNode three = new ListNode(3);
+        two.next = three;
+        ListNode four = new ListNode(4);
+        three.next = four;
+        ListNode five = new ListNode(5);
+        four.next = five;
+        ListNode six = new ListNode(5);
+        five.next = six;
+
         MiddleOfLinkedList middleOfLinkedList = new MiddleOfLinkedList();
+        middleOfLinkedList.middleNode(head);
     }
     public ListNode middleNode(ListNode head) {
+        int listSize = 1;
+        ListNode tempHead = head;
+        while (tempHead.next != null){
+            tempHead = tempHead.next;
+            listSize++;
+        }
+        int midOfList = listSize/2;
+        int counter = -1;
+        System.out.println(listSize);
+        while (head.next != null){
+            counter++;
+            if(counter == midOfList){
+                return head;
+            }
+            head = head.next;
+        }
         return head;
     }
 }
