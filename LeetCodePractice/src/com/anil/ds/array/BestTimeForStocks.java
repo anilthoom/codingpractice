bd5a1394ceedb7f9b4c5982c6a05446a090b523e
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-12 10:45:56 +0530

    Best Time to Buy and Sell Stock

---
+++ LeetCodePractice/src/com/anil/ds/array/BestTimeForStocks.java
@@ -0,0 +1,12 @@
+package com.anil.ds.array;
+
+public class BestTimeForStocks {
+    public static void main(String[] args) {
+        BestTimeForStocks bestTimeForStocks = new BestTimeForStocks();
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println(bestTimeForStocks.maxProfit(prices));
+    }
+    public int maxProfit(int[] prices) {
+        return -1;
+    }
+}
