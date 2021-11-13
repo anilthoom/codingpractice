647b7e56688342cc62889910ec3b91a027773e83
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-13 15:39:51 +0530

    Is Valid Sudoku

--- LeetCodePractice/src/com/anil/ds/array/ValidSudoku.java
+++ LeetCodePractice/src/com/anil/ds/array/ValidSudoku.java
@@ -1,5 +1,10 @@
 package com.anil.ds.array;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+//https://leetcode.com/problems/valid-sudoku/
 public class ValidSudoku {
     public static void main(String[] args) {
         ValidSudoku validSudoku = new ValidSudoku();
@@ -16,7 +21,13 @@ public class ValidSudoku {
         System.out.println(validSudoku.isValidSudoku(board));
     }
     public boolean isValidSudoku(char[][] board) {
-
+        List<Character> rowList = new ArrayList<>();
+        for(int i=0; i<board.length; i++){
+            for (int j=0; j<board[i].length; j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
         return false;
     }
 }
