52ccdb25a951138eb242fbde379e3eee2436fc16
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-13 15:33:57 +0530

    Reverse words optimal solution

--- LeetCodePractice/src/com/anil/twopointers/ReverseString.java
+++ LeetCodePractice/src/com/anil/twopointers/ReverseString.java
@@ -1,17 +1,29 @@
 package com.anil.twopointers;
 
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 //https://leetcode.com/problems/reverse-string/
 public class ReverseString {
     public static void main(String[] args) {
         ReverseString reverseString = new ReverseString();
         char s[] = {'h','e','l','l','o'};
-        reverseString.reverseCharString(s);
+        //reverseString.reverseCharString(s);
         String str = "Let's take LeetCode contest";
-        System.out.println(reverseString.reverseWords(str));
+        //System.out.println(reverseString.reverseWords(str));
+        System.out.println(reverseString.reverseWordsOptimal(str));
     }
     //https://leetcode.com/problems/reverse-words-in-a-string-iii/
+    public String reverseWordsOptimal(String s) {
+        StringTokenizer stringTokenizer = new StringTokenizer(s, " ");
+        StringBuilder stringBuilder = new StringBuilder();
+        while(stringTokenizer.hasMoreTokens()){
+            StringBuilder temp = new StringBuilder(stringTokenizer.nextToken());
+            temp.reverse();
+            stringBuilder.append(temp+" ");
+        }
+        return stringBuilder.substring(0, s.length());
+    }
     public String reverseWords(String s) {
         String ans = "";
         String strs[] = s.split(" ");
