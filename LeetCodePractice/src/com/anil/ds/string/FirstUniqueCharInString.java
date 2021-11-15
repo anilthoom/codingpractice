7a4f5bd743cc6282038439bfa4e13c6d90ae7127
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-15 13:04:57 +0530

    First Unique Character in a String

--- LeetCodePractice/src/com/anil/ds/string/FirstUniqueCharInString.java
+++ LeetCodePractice/src/com/anil/ds/string/FirstUniqueCharInString.java
@@ -3,9 +3,15 @@ package com.anil.ds.string;
 public class FirstUniqueCharInString {
     public static void main(String[] args) {
         FirstUniqueCharInString firstUniqueCharInString = new FirstUniqueCharInString();
-        System.out.println(firstUniqueCharInString.firstUniqChar("loveleetcode"));
+        System.out.println(firstUniqueCharInString.firstUniqChar("aabb"));
     }
     public int firstUniqChar(String s) {
+        String charToSearch = "";
+        for(int i=0; i<s.length(); i++){
+            charToSearch = s.substring(i, i+1);
+            if(!s.substring(i+1, s.length()-1).contains(charToSearch))
+                return i;
+        }
         return -1;
     }
 }
