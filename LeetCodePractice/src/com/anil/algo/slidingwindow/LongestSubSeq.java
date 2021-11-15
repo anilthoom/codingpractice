c92e9d43a7d240b657d0c43254984ad626f4f7ee
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-15 15:29:58 +0530

    Longest Substring Without Repeating Characters

---
+++ LeetCodePractice/src/com/anil/algo/slidingwindow/LongestSubSeq.java
@@ -0,0 +1,12 @@
+package com.anil.algo.slidingwindow;
+
+public class LongestSubSeq {
+
+    public static void main(String[] args) {
+        LongestSubSeq longestSubSeq = new LongestSubSeq();
+        System.out.println(longestSubSeq.lengthOfLongestSubstring("pwwkew"));
+    }
+    public int lengthOfLongestSubstring(String s) {
+        return -1;
+    }
+}
