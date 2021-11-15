9a8cafd262db93865372841d31d4774f7dad8939
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-15 13:25:56 +0530

    First Unique Character in a String

--- LeetCodePractice/src/com/anil/ds/string/FirstUniqueCharInString.java
+++ LeetCodePractice/src/com/anil/ds/string/FirstUniqueCharInString.java
@@ -3,31 +3,41 @@ package com.anil.ds.string;
 public class FirstUniqueCharInString {
     public static void main(String[] args) {
         FirstUniqueCharInString firstUniqueCharInString = new FirstUniqueCharInString();
-        System.out.println(firstUniqueCharInString.firstUniqChar("leetcode"));
+        System.out.println(firstUniqueCharInString.firstUniqCharOptimal("leetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeleetcodeXleetcode"));
     }
     public int firstUniqCharOptimal(String s) {
         int[] chars = new int[128];
+        int counter = 0;
         for(int i = 0; i < s.length(); i++) {
             char c  = s.charAt(i);
             chars[c]++;
+            counter++;
         }
 
         for(int i = 0; i < s.length(); i++) {
+            counter++;
             if(chars[s.charAt(i)] == 1) {
+                System.out.println("COUNTER : "+counter);
                 return i;
             }
         }
-
+        System.out.println("COUNTER : "+counter);
         return -1;
     }
     public int firstUniqChar(String s) {
         String charToSearch = "", remainingSubStr = "";
+        int counter = 0;
         for(int i=0; i<s.length(); i++){
+            counter++;
             charToSearch = s.substring(i, i+1);
             remainingSubStr = s.substring(0, i) + s.substring(i+1, s.length());
-            if(!remainingSubStr.contains(charToSearch))
+            if(!remainingSubStr.contains(charToSearch)){
+                System.out.println("COUNTER : "+counter);
                 return i;
+            }
+
         }
+        System.out.println("COUNTER : "+counter);
         return -1;
     }
 }
