53536715b09537402b177ec37dffc5c9ce0edf02
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-15 12:33:57 +0530

    Search 2D Matrix

---
+++ LeetCodePractice/src/com/anil/ds/array/Search2DMatrix.java
@@ -0,0 +1,14 @@
+package com.anil.ds.array;
+
+//https://leetcode.com/problems/search-a-2d-matrix/
+public class Search2DMatrix {
+    public static void main(String[] args) {
+        Search2DMatrix search2DMatrix = new Search2DMatrix();
+        int matrix[][] = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        int target = 34;
+        System.out.println(search2DMatrix.searchMatrix(matrix, target));
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        return false;
+    }
+}
