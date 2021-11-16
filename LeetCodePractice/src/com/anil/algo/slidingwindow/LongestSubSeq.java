96e61647352314704245f1690b6ec29c4fc07e01
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-16 13:58:35 +0530

    Longest Substring Without Repeating Characters

--- LeetCodePractice/src/com/anil/algo/slidingwindow/LongestSubSeq.java
+++ LeetCodePractice/src/com/anil/algo/slidingwindow/LongestSubSeq.java
@@ -1,14 +1,36 @@
 package com.anil.algo.slidingwindow;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LongestSubSeq {
     public static void main(String[] args) {
         LongestSubSeq longestSubSeq = new LongestSubSeq();
-        System.out.println(longestSubSeq.lengthOfLongestSubstring("  "));
+        System.out.println(longestSubSeq.lengthOfLongestSubstring("abcabcbb"));
     }
     public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) {return 0;}
+        int max = 0;
+        Set<Character> set = new HashSet<>();
+        int i =0 , j=0;
+        char[] c = s.toCharArray();
+        while(j < c.length) {
+
+            if (!set.contains(c[j])) {
+                set.add(c[j]);
+                j++;
+                max = Math.max(set.size(), max);
+            }else
+            {
+                set.remove(c[i]);
+                i++;
+            }
+        }
+        return max;
+    }
+    public int lengthOfLongestSubstringBF(String s) {
         int maxLength = 0;
         for(int i=0; i<s.length(); i++){
             List<Character> charList = new ArrayList<>();
