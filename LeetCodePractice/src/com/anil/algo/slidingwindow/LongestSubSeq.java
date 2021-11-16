06900f742a11bfe758a2c5f1d8db34fc5bdaab52
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-16 13:48:19 +0530

    Longest Substring Without Repeating Characters

--- LeetCodePractice/src/com/anil/algo/slidingwindow/LongestSubSeq.java
+++ LeetCodePractice/src/com/anil/algo/slidingwindow/LongestSubSeq.java
@@ -1,29 +1,31 @@
 package com.anil.algo.slidingwindow;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LongestSubSeq {
     public static void main(String[] args) {
         LongestSubSeq longestSubSeq = new LongestSubSeq();
-        System.out.println(longestSubSeq.lengthOfLongestSubstring("abcabca"));
+        System.out.println(longestSubSeq.lengthOfLongestSubstring("  "));
     }
     public int lengthOfLongestSubstring(String s) {
         int maxLength = 0;
-        int p1=0, p2=1;
-        while (p1 < p2){
-            char c = s.charAt(p1);
-            String remainingSubString = s.substring(p1+1, s.length());
-            if(remainingSubString.contains(c+"")){
-                //There is a repeating char in the remaining
-                int curLength = (c+remainingSubString.substring(0, remainingSubString.indexOf(c))).length();
-                if(curLength > maxLength)
-                    maxLength = curLength;
-                p1 = p1+1;
-                p2 = p1+1;
+        for(int i=0; i<s.length(); i++){
+            List<Character> charList = new ArrayList<>();
+            String remainingStr = s.substring(i, s.length());
+            for(int j=0; j<remainingStr.length(); j++){
+                char c = remainingStr.charAt(j);
+                if(charList.contains(c)){
+                    break;
+                }
+                else {
+                    charList.add(c);
+                }
             }
-            else {
-                if(remainingSubString.length() > maxLength)
-                    maxLength = remainingSubString.length();
+            if(maxLength < charList.size())
+                maxLength = charList.size();
+            if(charList.size() == remainingStr.length())
                 break;
-            }
         }
         return maxLength;
     }
