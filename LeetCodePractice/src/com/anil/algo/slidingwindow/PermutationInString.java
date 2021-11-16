d070c24964be8967496b9917f76489c6e1622810
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-16 20:22:04 +0530

    Permutation in String

--- LeetCodePractice/src/com/anil/algo/slidingwindow/PermutationInString.java
+++ LeetCodePractice/src/com/anil/algo/slidingwindow/PermutationInString.java
@@ -1,4 +1,7 @@
 package com.anil.algo.slidingwindow;
+
+import java.util.Arrays;
+
 //https://leetcode.com/problems/permutation-in-string/
 public class PermutationInString {
     public static void main(String[] args) {
@@ -6,6 +9,31 @@ public class PermutationInString {
         System.out.println(permutationInString.checkInclusion("ab", "eidbaooo"));
     }
     public boolean checkInclusion(String s1, String s2) {
+        char[] s1Chars = new char[26];
+        for(int i=0; i<s1.length(); i++){
+            s1Chars[s1.charAt(i)-'a']++;
+        }
+        for(int i =0; i<s2.length(); i++){
+            if(s1Chars[s2.charAt(i)-'a'] > 0){
+                String str = s2.substring(i, i+s1.length());
+                boolean found = false;
+                for(int j=0; j<str.length(); j++){
+                    char[] strChars = new char[26];
+                    for(int k=0; k<str.length();k++){
+                        strChars[str.charAt(k)-'a']++;
+                    }
+                    if(s1Chars[str.charAt(j)-'a'] == strChars[str.charAt(j)-'a']){
+                        found =true;
+                    }
+                    else {
+                        found = false;
+                        break;
+                    }
+                }
+                if(found)
+                    return true;
+            }
+        }
         return false;
     }
 }
