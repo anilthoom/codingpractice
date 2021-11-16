d039d37235aecab5311ce955620d7b48ddab9aed
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-16 21:40:02 +0530

    Permutation in String

--- LeetCodePractice/src/com/anil/algo/slidingwindow/PermutationInString.java
+++ LeetCodePractice/src/com/anil/algo/slidingwindow/PermutationInString.java
@@ -6,10 +6,32 @@ import java.util.Arrays;
 public class PermutationInString {
     public static void main(String[] args) {
         PermutationInString permutationInString = new PermutationInString();
-        System.out.println(permutationInString.checkInclusion("hello", "ooolleoooleh"));
+        System.out.println(permutationInString.checkInclusion("hello", "ooolleooolehlo"));
     }
     public boolean checkInclusion(String s1, String s2) {
+        char[] s1Chars = new char[26];
+        char[] s2Chars = new char[26];
+        for(int i=0; i<s1.length(); i++){
+            s1Chars[s1.charAt(i)-'a']++;
+            s2Chars[s2.charAt(i)-'a']++;
+        }
+        if(isMatch(s1Chars, s2Chars))
+            return true;
+        int s1Length = s1.length();
+        for(int i=1; i<=s2.length()-s1Length; i++){
+            s2Chars[s2.charAt(i-1)-'a']--;
+            s2Chars[s2.charAt(i+s1Length-1)-'a']++;
+            if(isMatch(s1Chars, s2Chars))
+                return true;
+        }
         return false;
+    }
+    private boolean isMatch(char[] s1Chars, char[] s2Chars){
+        for(int i=0; i<26; i++){
+            if(s1Chars[i] != s2Chars[i])
+                return false;
+        }
+        return true;
     }
     public boolean checkInclusionBF(String s1, String s2) {
         char[] s1Chars = new char[26];
