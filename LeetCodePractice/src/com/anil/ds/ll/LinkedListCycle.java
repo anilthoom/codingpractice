6d495f8411e0ae3e38a357fe0a59f2d90a6e3129
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-17 14:31:13 +0530

    Linked list cycle

---
+++ LeetCodePractice/src/com/anil/ds/ll/LinkedListCycle.java
@@ -0,0 +1,20 @@
+package com.anil.ds.ll;
+
+public class LinkedListCycle {
+    public static void main(String[] args) {
+
+    }
+
+}
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int val){
+        this.val = val;
+    }
+    ListNode(int val, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
