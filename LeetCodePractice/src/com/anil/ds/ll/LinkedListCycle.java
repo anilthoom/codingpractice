fcbcb1adfd5ba9a5e7172c5afe4871a0852ce95b
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-17 14:36:03 +0530

    Linked list cycle

--- LeetCodePractice/src/com/anil/ds/ll/LinkedListCycle.java
+++ LeetCodePractice/src/com/anil/ds/ll/LinkedListCycle.java
@@ -2,7 +2,24 @@ package com.anil.ds.ll;
 
 public class LinkedListCycle {
     public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode two = new ListNode(2);
+        head.next = two;
+        ListNode three = new ListNode(3);
+        two.next = three;
+        ListNode four = new ListNode(4);
+        three.next = four;
+        ListNode five = new ListNode(5);
+        four.next = five;
+        ListNode six = new ListNode(6);
+        five.next = six;
 
+        LinkedListCycle linkedListCycle = new LinkedListCycle();
+        System.out.println(linkedListCycle.hasCycle(head));
+    }
+    public boolean hasCycle(ListNode head) {
+
+        return false;
     }
 
 }
