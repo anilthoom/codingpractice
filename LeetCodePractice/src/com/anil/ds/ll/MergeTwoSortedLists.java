84e01b0ca04cc37d68c3665a64280f22bb9fa86d
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-18 14:48:00 +0530

    Merge Two Sorted Lists

--- LeetCodePractice/src/com/anil/ds/ll/MergeTwoSortedLists.java
+++ LeetCodePractice/src/com/anil/ds/ll/MergeTwoSortedLists.java
@@ -16,9 +16,24 @@ public class MergeTwoSortedLists {
         l2.next = l2i;
         ListNode l2j = new ListNode(4);
         l2i.next = l2j;
-        mergeTwoSortedLists.mergeTwoLists(l1, l2);
+        ListNode sortedList = mergeTwoSortedLists.mergeTwoLists(l1, l2);
     }
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode remainingL1 =l1, remainingL2 = l2;
+        while (remainingL1.next !=null && remainingL2.next!=null){
+            int v1 = remainingL1.val;
+            int v2 = remainingL2.val;
+            if(v1<v2){
+                // Attach l2 to l1
+                remainingL2 = remainingL2.next;
+                l2.next =null;
+                remainingL1 = remainingL1.next;
+                l1.next = l2;
+            }
+            else if(v2<v1){
+                //Attach l1 to l2
+            }
+        }
         return null;
     }
 }
