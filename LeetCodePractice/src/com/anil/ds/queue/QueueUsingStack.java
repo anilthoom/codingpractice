9aa28b0b8c4a4fe40228a8fbe8a343d88e124e5b
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-22 13:31:50 +0530

    Implement Queue using Stacks

---
+++ LeetCodePractice/src/com/anil/ds/queue/QueueUsingStack.java
@@ -0,0 +1,28 @@
+package com.anil.ds.queue;
+
+public class QueueUsingStack {
+    public static void main(String[] args) {
+
+    }
+}
+class MyQueue{
+    public MyQueue() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public int pop() {
+        return -1;
+    }
+
+    public int peek() {
+        return -1;
+    }
+
+    public boolean empty() {
+        return false;
+    }
+}
