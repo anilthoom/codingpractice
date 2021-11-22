92838d4f4f11925a19a4fcd5a015856272af88de
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-22 21:05:35 +0530

    Binary Tree Preorder Traversal

---
+++ LeetCodePractice/src/com/anil/ds/tree/BinaryTreePreOrder.java
@@ -0,0 +1,24 @@
+package com.anil.ds.tree;
+
+import java.util.List;
+
+public class BinaryTreePreOrder {
+    public static void main(String[] args) {
+
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        return null;
+    }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+     }
+}
