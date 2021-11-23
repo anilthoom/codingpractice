9fc5cf0febfa129ac1564262bef9c8cd830492ca
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-23 11:28:41 +0530

    Level order

--- LeetCodePractice/src/com/anil/ds/tree/BinaryTreePreOrder.java
+++ LeetCodePractice/src/com/anil/ds/tree/BinaryTreePreOrder.java
@@ -1,7 +1,11 @@
 package com.anil.ds.tree;
 
+import com.sun.source.tree.Tree;
+
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 //https://leetcode.com/problems/binary-tree-preorder-traversal/
 public class BinaryTreePreOrder {
@@ -40,6 +44,37 @@ public class BinaryTreePreOrder {
             nodesList.add(root.val);
         }
         return nodesList;
+    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> levelOrderList = new ArrayList<>();
+        parseLevelOrder(root, levelOrderList);
+        return levelOrderList;
+    }
+    public void parseLevelOrder(TreeNode root, List<List<Integer>> list){
+
+    }
+    public static void printLevelOrder(TreeNode root)
+    {
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty())
+        {
+            /* poll() removes the present head.
+            For more information on poll() visit
+            http://www.tutorialspoint.com/java/util/linkedlist_poll.htm */
+            TreeNode tempNode = queue.poll();
+            System.out.print(tempNode.val + " ");
+
+            /*Enqueue left child */
+            if (tempNode.left != null) {
+                queue.add(tempNode.left);
+            }
+
+            /*Enqueue right child */
+            if (tempNode.right != null) {
+                queue.add(tempNode.right);
+            }
+        }
     }
 }
 class TreeNode {
