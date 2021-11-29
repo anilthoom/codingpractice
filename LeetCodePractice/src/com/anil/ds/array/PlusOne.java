6eb273e2cbd6b04e2426aab023d294ae27ba481a
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-29 15:08:06 +0530

    Plus one in array

--- LeetCodePractice/src/com/anil/ds/array/PlusOne.java
+++ LeetCodePractice/src/com/anil/ds/array/PlusOne.java
@@ -1,11 +1,13 @@
 package com.anil.ds.array;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 
 public class PlusOne {
     public static void main(String[] args) {
         PlusOne plusOne = new PlusOne();
-        int digits[] = {5,6,2,0,0,4,6,2,4,9};
+        int digits[] = {5,2,2,6,5,7,1,9,0,3,8,6,8,6,5,2,1,8,7,9,8,3,8,4,7,2,5,8,9};
+//        int digits[] = {5,2,2,6,5,7,1,9};
         plusOne.plusOne(digits);
         System.out.println();
     }
@@ -20,9 +22,9 @@ public class PlusOne {
             for(int i =0; i<digits.length; i++){
                 intStr +=""+digits[i];
             }
-            long result = Long.parseLong(intStr);
-            result++;
-            intStr = ""+result;
+            BigInteger bigInteger = new BigInteger(intStr);
+            bigInteger = bigInteger.add(new BigInteger("1"));
+            intStr = ""+bigInteger;
             int outArr[] = new int[intStr.length()];
             for(int i=0; i<intStr.length(); i++){
                 outArr[i] = Integer.parseInt(""+intStr.charAt(i));
