f101fe584f1e6af96c387e0f6ffa75825e150ac5
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-30 15:56:16 +0530

    Palindrome Linked List

--- LeetCodePractice/src/com/anil/ds/ll/PalindromeLinkedList.java
+++ LeetCodePractice/src/com/anil/ds/ll/PalindromeLinkedList.java
@@ -3,9 +3,23 @@ package com.anil.ds.ll;
 //https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/772/
 public class PalindromeLinkedList {
     public static void main(String[] args) {
-
+        PalindromeLinkedList palindromeLinkedList = new PalindromeLinkedList();
+        ListNode head = new ListNode(1);
+        ListNode two = new ListNode(2);
+        head.next = two;
+        ListNode three = new ListNode(2);
+        two.next = three;
+        ListNode one = new ListNode(1);
+        three.next = one;
+        System.out.println(palindromeLinkedList.isPalindrome(head));
     }
     public boolean isPalindrome(ListNode head) {
-        return false;
+        StringBuffer stringBuffer = new StringBuffer();
+        while (head!=null){
+            stringBuffer.append(head.val);
+            head = head.next;
+        }
+        String original = stringBuffer.toString();
+        return original.contentEquals(stringBuffer.reverse());
     }
 }
