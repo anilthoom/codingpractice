38dcac5dc38fd9091b7d5117d54b0c05d202fb06
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-30 15:32:12 +0530

    Palindrome Linked List

---
+++ LeetCodePractice/src/com/anil/ds/ll/PalindromeLinkedList.java
@@ -0,0 +1,11 @@
+package com.anil.ds.ll;
+
+//https://leetcode.com/explore/interview/card/top-interview-questions-easy/93/linked-list/772/
+public class PalindromeLinkedList {
+    public static void main(String[] args) {
+
+    }
+    public boolean isPalindrome(ListNode head) {
+        return false;
+    }
+}
