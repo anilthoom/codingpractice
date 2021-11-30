ac9799d1d71d36631dfd940912dcf5a582e4f9ae
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-30 14:56:52 +0530

    Fizz buzz

--- LeetCodePractice/src/com/anil/math/FizzBuzz.java
+++ LeetCodePractice/src/com/anil/math/FizzBuzz.java
@@ -11,6 +11,16 @@ public class FizzBuzz {
     }
     public List<String> fizzBuzz(int n) {
         List<String> resultList = new ArrayList<>();
+        for(int i=1; i<=n; i++){
+            if(i%3 == 0 && i%5==0)
+                resultList.add("FizzBuzz");
+            else if(i%3 == 0)
+                resultList.add("Fizz");
+            else if(i%5 ==0)
+                resultList.add("Buzz");
+            else
+                resultList.add(""+i);
+        }
         return resultList;
     }
 }
