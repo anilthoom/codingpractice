16441315567ddc8a6d80eb1e6fa3831670c0550a
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-30 14:35:51 +0530

    Power of three

---
+++ LeetCodePractice/src/com/anil/math/PowerOfThree.java
@@ -0,0 +1,13 @@
+package com.anil.math;
+
+public class PowerOfThree {
+    public static void main(String[] args) {
+        PowerOfThree powerOfThree = new PowerOfThree();
+        System.out.println(powerOfThree.isPowerOfThree(729));
+    }
+    public boolean isPowerOfThree(int n) {
+        int result = 3;
+
+        return false;
+    }
+}
