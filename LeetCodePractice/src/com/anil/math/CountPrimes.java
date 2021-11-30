89659e22e5ac640d3fb8b0d3e9c17ee21b6c126c
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-30 12:53:29 +0530

    Counting Primes

--- LeetCodePractice/src/com/anil/math/CountPrimes.java
+++ LeetCodePractice/src/com/anil/math/CountPrimes.java
@@ -3,7 +3,9 @@ package com.anil.math;
 public class CountPrimes {
     public static void main(String[] args) {
         CountPrimes countPrimes = new CountPrimes();
-        System.out.println(countPrimes.countPrimes(3));
+
+        System.out.println(countPrimes.countPrimes(345694));
+        System.out.println(Math.sqrt(30));
     }
     public int countPrimes(int n) {
         int counter = 0;
@@ -13,12 +15,20 @@ public class CountPrimes {
         }
         return counter;
     }
-    public boolean isPrime(int num){
-        for(int i=2; i<=num/2; i++)
-        {
-            if(num%i == 0)
+    static boolean isPrime(int n)
+    {
+        // Corner cases
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+
+        // This is checked so that we can skip
+        // middle five numbers in below loop
+        if (n % 2 == 0 || n % 3 == 0) return false;
+
+        for (int i = 5; i * i <= n; i = i + 6)
+            if (n % i == 0 || n % (i + 2) == 0)
                 return false;
-        }
+
         return true;
     }
 }
