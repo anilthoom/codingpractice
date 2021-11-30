6e6627e0235bca47840c9f6ffb156f03a1e83085
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-11-30 14:52:57 +0530

    Fizz buzz

---
+++ LeetCodePractice/src/com/anil/math/FizzBuzz.java
@@ -0,0 +1,16 @@
+package com.anil.math;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//https://leetcode.com/explore/interview/card/top-interview-questions-easy/102/math/743/
+public class FizzBuzz {
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz =  new FizzBuzz();
+        fizzBuzz.fizzBuzz(15);
+    }
+    public List<String> fizzBuzz(int n) {
+        List<String> resultList = new ArrayList<>();
+        return resultList;
+    }
+}
