89ab6a6139ec01df821028d63721c31d3c1625b7
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-12-09 17:28:23 +0530

    Add two numbers - linked list

---
+++ LeetCodePractice/src/com/anil/ds/ll/AddTwoNums.java
@@ -0,0 +1,27 @@
+package com.anil.ds.ll;
+
+//https://leetcode.com/problems/add-two-numbers/
+public class AddTwoNums {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        ListNode two = new ListNode(2);
+        l1.next = two;
+        ListNode three = new ListNode(4);
+        two.next = three;
+
+        ListNode l2 = new ListNode(1);
+        ListNode l2i = new ListNode(3);
+        l2.next = l2i;
+        ListNode l2j = new ListNode(4);
+        l2i.next = l2j;
+        AddTwoNums addTwoNums = new AddTwoNums();
+        addTwoNums.addTwoNumbers(l1, l2);
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        while (l1.next != null){
+
+        }
+        ListNode mergedNode = new ListNode();
+        return mergedNode;
+    }
+}
