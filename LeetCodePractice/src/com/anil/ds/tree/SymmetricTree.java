1778467c6886a12235b25aaef0f24dbbbf18b2b7
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-12-09 15:21:05 +0530

    Symmetric Tree - Mirror reflection

--- LeetCodePractice/src/com/anil/ds/tree/SymmetricTree.java
+++ LeetCodePractice/src/com/anil/ds/tree/SymmetricTree.java
@@ -12,34 +12,51 @@ public class SymmetricTree {
         TreeNode root = new TreeNode(1);
         TreeNode two = new TreeNode(2);
         root.left = two;
-        TreeNode three = new TreeNode(3);
+        TreeNode three = new TreeNode(2);
         root.right = three;
         two.left = new TreeNode(4);
-        two.right = new TreeNode(5);
-        three.left = new TreeNode(6);
-        three.right = new TreeNode(7);
+        //two.right = new TreeNode(5);
+        //three.left = new TreeNode(5);
+        three.right = new TreeNode(4);
 
         SymmetricTree symmetricTree = new SymmetricTree();
         System.out.println(symmetricTree.isSymmetric(root));
     }
     public boolean isSymmetric(TreeNode root) {
-        List<Integer> nodeValues = new ArrayList<>();
+        String nodeValStr = "";
         LinkedList<TreeNode> nextNodes = new LinkedList<>();
         LinkedList<TreeNode> queue = new LinkedList<>();
         queue.add(root);
         while (!queue.isEmpty()){
             TreeNode tempNode = queue.poll();
-            if(tempNode.left!=null){
-                nextNodes.add(tempNode.left);
+            nodeValStr += ""+tempNode.val;
+            if(tempNode.left !=null || tempNode.right!=null){
+                if(tempNode.left == null){
+                    nodeValStr += "X";
+                }
+                else if(tempNode.left!=null){
+                    nextNodes.add(tempNode.left);
+                }
+                if(tempNode.right == null){
+                    nodeValStr += "X";
+                }
+                else if(tempNode.right != null){
+                    nextNodes.add(tempNode.right);
+                }
             }
-            if(tempNode.right != null){
-                nextNodes.add(tempNode.right);
-            }
-
             if(queue.isEmpty()){
-
+                if(!isPalindrome(nodeValStr))
+                    return false;
+                queue = nextNodes;
+                nextNodes = new LinkedList<>();
+                nodeValStr = "";
             }
         }
         return true;
+    }
+    public boolean isPalindrome(String str){
+        StringBuffer buffer = new StringBuffer(str);
+        buffer.reverse();
+        return str.contentEquals(buffer);
     }
 }
