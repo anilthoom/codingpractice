fe8258c44360abd06c15669314c1dd41d040aefb
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-12-23 15:47:45 +0530

    Thread pool exceeding example

---
+++ JavaCoding/src/com/anilt/java/features/DateTimeZone.java
@@ -0,0 +1,10 @@
+package com.anilt.java.features;
+
+import java.time.ZonedDateTime;
+
+public class DateTimeZone {
+    public static void main(String[] args) {
+        ZonedDateTime currentDateTime = ZonedDateTime.now();
+        System.out.println(currentDateTime.toLocalDate());
+    }
+}
