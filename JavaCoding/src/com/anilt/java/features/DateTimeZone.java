134a2070e406ebb3c46f1f699ea2f0707cd1e55f
anilkumar.thoom <anilkumar.thoom@gmail.com>
2021-12-23 16:33:35 +0530

    ZonedDateTime example

--- JavaCoding/src/com/anilt/java/features/DateTimeZone.java
+++ JavaCoding/src/com/anilt/java/features/DateTimeZone.java
@@ -1,11 +1,17 @@
 package com.anilt.java.features;
 
+import java.time.Instant;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class DateTimeZone {
     public static void main(String[] args) {
         ZonedDateTime currentDateTime = ZonedDateTime.now();
-
         System.out.println(currentDateTime.toLocalDateTime());
+        currentDateTime.truncatedTo(ChronoUnit.DAYS);
+        System.out.println(currentDateTime.toLocalDateTime());
+        Instant instant = Instant.parse(currentDateTime.toString());
+        System.out.println(instant.toString());
     }
 }
