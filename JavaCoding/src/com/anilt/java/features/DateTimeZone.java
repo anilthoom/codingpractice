e04779ddc48eed38b2ab2fca267e7a94807f08ff
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-01-03 18:36:13 +0530

    ZonedDateTime example

--- JavaCoding/src/com/anilt/java/features/DateTimeZone.java
+++ JavaCoding/src/com/anilt/java/features/DateTimeZone.java
@@ -16,5 +16,6 @@ public class DateTimeZone {
         ZonedDateTime currentDate = ZonedDateTime.now(ZoneOffset.UTC).with(LocalTime.of(0, 0));
         System.out.println(currentDate);
         ZonedDateTime cDate = ZonedDateTime.now().with(LocalTime.of(0, 0));
+        System.out.println(cDate);
     }
 }
