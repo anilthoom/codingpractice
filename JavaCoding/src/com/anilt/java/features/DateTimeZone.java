2ea30102f50fbe913ef28126aacbe204bf7a62bd
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-01-04 10:19:57 +0530

    ZonedDateTime example

--- JavaCoding/src/com/anilt/java/features/DateTimeZone.java
+++ JavaCoding/src/com/anilt/java/features/DateTimeZone.java
@@ -7,14 +7,14 @@ import java.util.Calendar;
 public class DateTimeZone {
     public static void main(String[] args) {
         ZonedDateTime currentDateTime = ZonedDateTime.now();
-        System.out.println(currentDateTime.toLocalDateTime());
+        //System.out.println(currentDateTime.toLocalDateTime());
         String instant = Instant.parse("2017-03-03T13:14:28.666Z").truncatedTo(ChronoUnit.DAYS).toString();
-        System.out.println(instant);
+       // System.out.println(instant);
 
         ZonedDateTime currentDate = ZonedDateTime.now(ZoneOffset.UTC).with(LocalTime.of(0, 0));
-        System.out.println(currentDate);
+        //System.out.println(currentDate);
         ZonedDateTime cDate = ZonedDateTime.now().with(LocalTime.of(0, 0));
-        System.out.println(cDate);
+        //System.out.println(cDate);
 
         ZonedDateTime startDate = ZonedDateTime.of(ZonedDateTime.now().getYear(),
                 ZonedDateTime.now().getMonthValue(), ZonedDateTime.now().getDayOfMonth(),
