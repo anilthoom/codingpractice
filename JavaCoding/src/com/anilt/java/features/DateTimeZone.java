40494f5418eaf76c0a9f572bb900b7a44a158d5a
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-01-07 14:10:37 +0530

    ZonedDateTime example

--- JavaCoding/src/com/anilt/java/features/DateTimeZone.java
+++ JavaCoding/src/com/anilt/java/features/DateTimeZone.java
@@ -4,15 +4,20 @@ import java.time.*;
 import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class DateTimeZone {
     public static void main(String[] args) {
         Calendar calendar = Calendar.getInstance();
+//        calendar.setTimeZone(TimeZone.getTimeZone("UTC"));
         calendar.set(Calendar.HOUR_OF_DAY, 0);
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
 
-        System.out.println(calendar.getTime());
+        System.out.println(calendar.toInstant());
+
+        LocalDate currentDate = LocalDate.now();
+        System.out.println(currentDate);
     }
 }
