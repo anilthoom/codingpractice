1e702a896ccd87b596275184b48f8ebe9a3b7ecd
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-01-30 19:23:39 +0530

    Length of Last Word

---
+++ LeetCodePractice/src/com/anil/ds/string/LengthOfLastWord.java
@@ -0,0 +1,12 @@
+package com.anil.ds.string;
+
+public class LengthOfLastWord {
+    public static void main(String[] args) {
+        LengthOfLastWord lengthOfLastWord = new LengthOfLastWord();
+        String word = "";
+        System.out.println(lengthOfLastWord.lengthOfLastWord(word));
+    }
+    public int lengthOfLastWord(String s) {
+        return -1;
+    }
+}
