8d19a7105c3c7b8537eb4b17f789b71901ea0bd6
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-01-30 19:38:14 +0530

    Same Tree

---
+++ LeetCodePractice/src/com/anil/ds/tree/SameTree.java
@@ -0,0 +1,30 @@
+package com.anil.ds.tree;
+
+public class SameTree {
+    public static void main(String[] args) {
+        SameTree sameTree = new SameTree();
+        TreeNode p = new TreeNode(1);
+        TreeNode two = new TreeNode(2);
+        p.left = two;
+        TreeNode three = new TreeNode(3);
+        p.right = three;
+
+        TreeNode q = new TreeNode(1);
+        TreeNode two1 = new TreeNode(2);
+        q.left = two1;
+        TreeNode three1 = new TreeNode(3);
+        q.right = three1;
+
+        System.out.println(sameTree.isSameTree(p, q));
+    }
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        while (p != null && q != null){
+            if(p.val != q.val){
+                return false;
+            }
+            p = p.left;
+            q = q.left;
+        }
+        return false;
+    }
+}
