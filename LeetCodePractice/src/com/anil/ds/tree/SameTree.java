b3e2a23dbcd3571f8303713e952cb1f801fd13b8
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-02-07 16:20:49 +0530

    Same Tree

--- LeetCodePractice/src/com/anil/ds/tree/SameTree.java
+++ LeetCodePractice/src/com/anil/ds/tree/SameTree.java
@@ -1,11 +1,5 @@
 package com.anil.ds.tree;
 
-import com.sun.source.tree.Tree;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-
 //https://leetcode.com/problems/same-tree/
 public class SameTree {
     public static void main(String[] args) {
