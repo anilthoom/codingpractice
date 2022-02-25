9ae8c81b4fe8afce083ff15e9ac380ab75366571
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-02-25 14:58:26 +0530

    Meta coding practice - Warmup

---
+++ Facebook/src/com/anilt/meta/warmup/AllWrong.java
@@ -0,0 +1,12 @@
+package com.anilt.meta.warmup;
+
+public class AllWrong {
+    public static void main(String[] args) {
+        AllWrong allWrong = new AllWrong();
+        String output = allWrong.getWrongAnswers(3, "ABABA");
+    }
+    public String getWrongAnswers(int N, String C){
+
+        return "";
+    }
+}
