a6b7d4d8d3468395431551795cd57ad7fd5d3878
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-02-25 14:46:43 +0530

    Meta coding practice - Warmup

--- Facebook/src/com/anilt/meta/warmup/ABCs.java
+++ Facebook/src/com/anilt/meta/warmup/ABCs.java
@@ -3,6 +3,11 @@ package com.anilt.meta.warmup;
 public class ABCs {
     public static void main(String[] args) {
         ABCs abCs = new ABCs();
+        int sum = abCs.getSum(100, 100, 100);
+        System.out.println(sum);
+    }
+    public int getSum(int A, int B, int C) {
+        return A+B+C;
     }
 
 }
