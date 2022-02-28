0cd31eacc18a9af2fecebbbcaf790a94ec064144
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-02-28 19:14:49 +0530

    Meta coding practice - BattleShip

--- Facebook/src/com/anilt/meta/warmup/BattleShip.java
+++ Facebook/src/com/anilt/meta/warmup/BattleShip.java
@@ -3,7 +3,8 @@ package com.anilt.meta.warmup;
 public class BattleShip {
     public static void main(String[] args) {
         BattleShip battleShip = new BattleShip();
-
+        int[][] G= {{0,0,1}, {1,0,1}};
+        System.out.println(battleShip.getHitProbability(2, 3, G));
     }
     public double getHitProbability(int R, int C, int[][] G){
         return 0.0;
