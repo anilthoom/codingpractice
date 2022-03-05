e19d55122cd5ec515735009dbc7589376b9e5584
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-03-05 20:28:59 +0530

    File Upload-download to DB

---
+++ SpringBootFeatures/FileUploadDownloadDB/src/main/java/com/anilt/file/FileUploadDownloadDbApplication.java
@@ -0,0 +1,13 @@
+package com.anilt.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadDownloadDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadDownloadDbApplication.class, args);
+	}
+
+}
---
+++ SpringBootFeatures/FileUploadDownloadDB/src/test/java/com/anilt/file/FileUploadDownloadDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anilt.file;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadDownloadDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
