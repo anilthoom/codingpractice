11175ee1a767a61a65a3c6440c43476dabbc2a33
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-03-05 20:38:01 +0530

    File Upload-download to DB

--- SpringBootFeatures/FileUploadDownloadDB/src/main/java/com/anilt/file/model/FileEntity.java
+++ SpringBootFeatures/FileUploadDownloadDB/src/main/java/com/anilt/file/model/FileEntity.java
@@ -1,17 +1,25 @@
 package com.anilt.file.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 @Table(name="FILES")
 public class FileEntity {
     @Id
     @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
+
+    private String name;
+    private String contentType;
+    private Long size;
+
+    @Lob
+    private byte[] data;
 }
