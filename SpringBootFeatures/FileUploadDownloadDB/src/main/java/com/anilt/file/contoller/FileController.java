aa39db09d728e58db86b66d574530b5528bea742
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-03-06 16:11:03 +0530

    File Upload-download to DB

--- SpringBootFeatures/FileUploadDownloadDB/src/main/java/com/anilt/file/contoller/FileController.java
+++ SpringBootFeatures/FileUploadDownloadDB/src/main/java/com/anilt/file/contoller/FileController.java
@@ -2,8 +2,13 @@ package com.anilt.file.contoller;
 
 import com.anilt.file.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("files")
@@ -13,5 +18,18 @@ public class FileController {
     @Autowired
     public FileController(FileService fileService){
         this.fileService = fileService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> upload(@RequestParam("file")MultipartFile file){
+        try {
+            fileService.save(file);
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(String.format("File uploaded successfully: %s", file.getOriginalFilename()));
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(String.format("Could not upload the file: %s!", file.getOriginalFilename()));
+        }
     }
 }
