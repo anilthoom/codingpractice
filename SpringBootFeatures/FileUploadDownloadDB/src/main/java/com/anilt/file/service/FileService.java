0c8d6ce2d699593ddfa56890748a55b4c894b6ea
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-03-06 16:02:03 +0530

    File Upload-download to DB

--- SpringBootFeatures/FileUploadDownloadDB/src/main/java/com/anilt/file/service/FileService.java
+++ SpringBootFeatures/FileUploadDownloadDB/src/main/java/com/anilt/file/service/FileService.java
@@ -1,7 +1,30 @@
 package com.anilt.file.service;
 
+import com.anilt.file.dao.FileRepository;
+import com.anilt.file.model.FileEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 public class FileService {
+    private final FileRepository fileRepository;
+
+    @Autowired
+    public FileService(FileRepository fileRepository){
+        this.fileRepository = fileRepository;
+    }
+
+    public void save(MultipartFile file) throws IOException{
+        FileEntity fileEntity = new FileEntity();
+        fileEntity.setName(StringUtils.cleanPath(file.getOriginalFilename()));
+        fileEntity.setContentType(file.getContentType());
+        fileEntity.setData(file.getBytes());
+        fileEntity.setSize(file.getSize());
+
+        fileRepository.save(fileEntity);
+    }
 }
