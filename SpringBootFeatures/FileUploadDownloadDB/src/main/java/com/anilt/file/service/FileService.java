4306abd99852b03ae715a8f0b6c7bf92e6ff6050
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-03-06 16:04:18 +0530

    File Upload-download to DB

--- SpringBootFeatures/FileUploadDownloadDB/src/main/java/com/anilt/file/service/FileService.java
+++ SpringBootFeatures/FileUploadDownloadDB/src/main/java/com/anilt/file/service/FileService.java
@@ -8,6 +8,8 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FileService {
@@ -26,5 +28,13 @@ public class FileService {
         fileEntity.setSize(file.getSize());
 
         fileRepository.save(fileEntity);
+    }
+
+    public Optional<FileEntity> getFile(String id){
+        return fileRepository.findById(id);
+    }
+
+    public List<FileEntity> getAllFiles(){
+        return fileRepository.findAll();
     }
 }
