11f3ba09ec4c1df3e8e9733e0cf9bf351b360c26
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-03-10 17:56:13 +0530

    Redis cache implementation

---
+++ SpringBootFeatures/RedisCache/src/main/java/com/anilt/redis/controller/InvoiceController.java
@@ -0,0 +1,27 @@
+package com.anilt.redis.controller;
+
+import com.anilt.redis.model.Invoice;
+import com.anilt.redis.service.InvoiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/")
+public class InvoiceController {
+    @Autowired
+    InvoiceService invoiceService;
+
+    @PostMapping("/invoice")
+    public Invoice saveInvoice(@RequestBody Invoice inv){
+        return invoiceService.saveInvoice(inv);
+    }
+
+    @GetMapping("/invoices")
+    public ResponseEntity<List<Invoice>> getAllInvoices(){
+        return ResponseEntity.ok(invoiceService.getAllInvoices());
+    }
+
+}
