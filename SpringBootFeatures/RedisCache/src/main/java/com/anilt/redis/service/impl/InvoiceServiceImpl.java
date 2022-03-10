68b86d70a0a75089009d7dac82beaee484ea520b
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-03-10 17:41:59 +0530

    Redis cache implementation

--- SpringBootFeatures/RedisCache/src/main/java/com/anilt/redis/service/impl/InvoiceServiceImpl.java
+++ SpringBootFeatures/RedisCache/src/main/java/com/anilt/redis/service/impl/InvoiceServiceImpl.java
@@ -1,19 +1,34 @@
 package com.anilt.redis.service.impl;
 
+import com.anilt.redis.exceptions.InvoiceNotFoundException;
 import com.anilt.redis.model.Invoice;
+import com.anilt.redis.repository.InvoiceRepository;
 import com.anilt.redis.service.InvoiceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class InvoiceServiceImpl implements InvoiceService {
+
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
     @Override
     public Invoice saveInvoice(Invoice inv) {
-        return null;
+        return invoiceRepository.save(inv);
     }
 
     @Override
+    @CachePut(value = "Invoice", key = "#invId")
     public Invoice updateInvoice(Invoice inv, Integer invId) {
-        return null;
+        Invoice invoice = invoiceRepository.findById(invId)
+                .orElseThrow(() -> new InvoiceNotFoundException("Invoice not found"));
+        invoice.setInvAmount(inv.getInvAmount());
+        invoice.setInvName(inv.getInvName());
+        return invoiceRepository.save(invoice);
     }
 
     @Override
