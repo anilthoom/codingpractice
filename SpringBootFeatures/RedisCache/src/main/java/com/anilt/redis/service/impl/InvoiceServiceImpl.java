4a8967cadad86ad01db58bb7fc920727cd8656a3
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-03-10 17:36:56 +0530

    Redis cache implementation

---
+++ SpringBootFeatures/RedisCache/src/main/java/com/anilt/redis/service/impl/InvoiceServiceImpl.java
@@ -0,0 +1,33 @@
+package com.anilt.redis.service.impl;
+
+import com.anilt.redis.model.Invoice;
+import com.anilt.redis.service.InvoiceService;
+
+import java.util.List;
+
+public class InvoiceServiceImpl implements InvoiceService {
+    @Override
+    public Invoice saveInvoice(Invoice inv) {
+        return null;
+    }
+
+    @Override
+    public Invoice updateInvoice(Invoice inv, Integer invId) {
+        return null;
+    }
+
+    @Override
+    public void deleteInvoice(Integer invId) {
+
+    }
+
+    @Override
+    public Invoice getOneInvoice(Integer invId) {
+        return null;
+    }
+
+    @Override
+    public List<Invoice> getAllInvoices() {
+        return null;
+    }
+}
