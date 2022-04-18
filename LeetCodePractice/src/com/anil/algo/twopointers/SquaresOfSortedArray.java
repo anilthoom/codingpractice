5415d2bd71930048443f2a6dca43435e7801db0f
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-04-18 16:54:17 +0530

    Rotate array

--- LeetCodePractice/src/com/anil/algo/twopointers/SquaresOfSortedArray.java
+++ LeetCodePractice/src/com/anil/algo/twopointers/SquaresOfSortedArray.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class SquaresOfSortedArray {
     public static void main(String[] args) {
         SquaresOfSortedArray squaresOfSortedArray = new SquaresOfSortedArray();
-        int nums[] = {4,-1,0,3,10, 11};
+        int nums[] = {4,-1,0,3,10, 14};
         squaresOfSortedArray.sortedSquares(nums);
     }
     public int[] sortedSquares(int[] nums) {
