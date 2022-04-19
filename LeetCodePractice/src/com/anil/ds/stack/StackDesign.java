f63a3492151f5703d7f011489dc708be1c723262
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-04-19 19:05:37 +0530

    Stack Design

--- LeetCodePractice/src/com/anil/ds/stack/StackDesign.java
+++ LeetCodePractice/src/com/anil/ds/stack/StackDesign.java
@@ -3,3 +3,25 @@ package com.anil.ds.stack;
 public class StackDesign {
 
 }
+class MinStack {
+
+    public MinStack() {
+
+    }
+
+    public void push(int val) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+        return -1;
+    }
+
+    public int getMin() {
+        return -1;
+    }
+}
