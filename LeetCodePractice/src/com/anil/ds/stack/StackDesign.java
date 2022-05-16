0526760fb4e49f124152b4d77765aaa56e0264d4
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-05-16 19:49:09 +0530

    Stack Design

--- LeetCodePractice/src/com/anil/ds/stack/StackDesign.java
+++ LeetCodePractice/src/com/anil/ds/stack/StackDesign.java
@@ -6,13 +6,10 @@ import java.util.*;
 public class StackDesign {
     public static void main(String[] args) {
         MinStack minStack = new MinStack();
-        minStack.push(-1);
-        minStack.push(-2);
-        minStack.push(-3);
-        minStack.push(7);
+        minStack.push(0);
         minStack.push(1);
-        minStack.push(5);
-        minStack.getMin(); // return -3
+        minStack.push(0);
+        minStack.getMin();
         minStack.pop();
         minStack.top();    // return 0
         System.out.println(minStack.getMin());
