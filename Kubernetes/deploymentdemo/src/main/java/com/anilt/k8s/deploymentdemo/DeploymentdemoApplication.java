44486e1323e74af684f622b640526377743ab604
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-07-14 15:35:04 +0530

    k8s deployment demo

---
+++ Kubernetes/deploymentdemo/src/main/java/com/anilt/k8s/deploymentdemo/DeploymentdemoApplication.java
@@ -0,0 +1,13 @@
+package com.anilt.k8s.deploymentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploymentdemoApplication.class, args);
+	}
+
+}
