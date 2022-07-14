c1ecc7b4777e640c60e7db9cc1d53c1027c4693a
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-07-14 16:24:27 +0530

    k8s deployment demo

---
+++ Kubernetes/k8sdeploymentdemo/src/test/java/com/anilt/k8s/K8sdeploymentdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anilt.k8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sdeploymentdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
