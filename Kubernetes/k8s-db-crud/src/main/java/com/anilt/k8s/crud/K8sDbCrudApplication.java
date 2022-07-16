ca550a2c04af38ce20ee75eef2b906ff7c2e1953
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-07-16 17:32:45 +0530

    K8S SpringBoot MySQL CRUD deployment

---
+++ Kubernetes/k8s-db-crud/src/main/java/com/anilt/k8s/crud/K8sDbCrudApplication.java
@@ -0,0 +1,13 @@
+package com.anilt.k8s.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sDbCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sDbCrudApplication.class, args);
+	}
+
+}
