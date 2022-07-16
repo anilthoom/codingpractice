653900943bca8867d7b2594c98a437aae26bbd13
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-07-16 18:44:06 +0530

    K8S SpringBoot MySQL CRUD deployment

--- Kubernetes/k8s-db-crud/src/main/java/com/anilt/k8s/crud/controller/OrderController.java
+++ Kubernetes/k8s-db-crud/src/main/java/com/anilt/k8s/crud/controller/OrderController.java
@@ -1,4 +1,13 @@
 package com.anilt.k8s.crud.controller;
 
+import com.anilt.k8s.crud.dao.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
 public class OrderController {
+    @Autowired
+    private OrderRepository orderRepository;
 }
