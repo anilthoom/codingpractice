91d970cc287d2ca33e52cfc8e1f75bd1d4552d5c
anilkumar.thoom <anilkumar.thoom@gmail.com>
2022-07-16 19:51:28 +0530

    K8S SpringBoot MySQL CRUD deployment

--- Kubernetes/k8s-db-crud/src/main/java/com/anilt/k8s/crud/service/impl/OrderServiceImpl.java
+++ Kubernetes/k8s-db-crud/src/main/java/com/anilt/k8s/crud/service/impl/OrderServiceImpl.java
@@ -1,30 +1,39 @@
 package com.anilt.k8s.crud.service.impl;
 
+import com.anilt.k8s.crud.dao.OrderRepository;
 import com.anilt.k8s.crud.entity.Order;
 import com.anilt.k8s.crud.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+
+    @Autowired
+    OrderRepository orderRepository;
+
     @Override
     public Order getOrderById(int id) {
-        return null;
+        return orderRepository.findById(id).get();
     }
 
     @Override
     public List<Order> getAllOrders() {
-        return null;
+        List<Order> orders = new ArrayList<Order>();
+        orderRepository.findAll().forEach(order -> orders.add(order));
+        return orders;
     }
 
     @Override
     public void saveOrUpdate(Order order) {
-
+        orderRepository.save(order);
     }
 
     @Override
     public void deleteOrderById(int id) {
-
+        orderRepository.deleteById(id);
     }
 }
